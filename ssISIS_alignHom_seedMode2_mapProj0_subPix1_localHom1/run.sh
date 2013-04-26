838fe9b6059a8e497492a327b427af03e2b73e34
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-04-26 14:32:47 -0700

    Rename/wipe some tests

--- ssISIS_alignHom_seedMode2_mapProj0_subPix1_localHom1/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. ~oalexan1/bin/isis_setup.sh
-d=../data
-dir=run
-rm -rfv $dir
-stereo $d/M0100115.cub $d/E0201461.cub $dir/$dir -s stereo.default --left-image-crop-win 0 1024 672 4864  --disparity-estimation-dem $d/ref-DEM.tif --disparity-estimation-dem-error 5 --alignment-method homography --corr-seed-mode 2 --subpixel-mode 1 --use-local-homography
-point2dem -r mars $dir/$dir-PC.tif --nodata-value -32767
