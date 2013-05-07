b0140b0c904407f8a20e643c2894b212b856a19f
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-05-07 11:36:59 -0700

    More tests and some updates

--- ssDG_alignNone_seedMode1_mapProj1_subPix2/run.sh
+++ ssDG_alignNone_seedMode1_mapProj1_subPix2/run.sh
@@ -4,6 +4,6 @@ d=../data
 dir=run
 rm -rfv $dir
 
-stereo $d/left.tif $d/right.tif $d/left.xml $d/right.xml $dir/$dir $d/krigged_dem_nsidc_ndv0_fill.tif -s stereo.default -t dg --alignment-method none --corr-seed-mode 1 --subpixel-mode 2
+stereo $d/left.tif $d/right.tif $d/left.xml $d/right.xml $dir/$dir $d/krigged_dem_nsidc_ndv0_fill.tif  --disable-fill-holes -s stereo.default -t dg --alignment-method none --corr-seed-mode 1 --subpixel-mode 2
 point2dem -r Earth $dir/$dir-PC.tif --nodata-value -32767
 
---
+++ ssDG_alignNone_seedMode1_mapProj1_subPix2_leastSq/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+d=../data
+dir=run
+rm -rfv $dir
+
+stereo $d/left.tif $d/right.tif $d/left.xml $d/right.xml $dir/$dir $d/krigged_dem_nsidc_ndv0_fill.tif -s stereo.default -t dg --left-image-crop-win 0 0 150 150 --alignment-method none --corr-seed-mode 1 --subpixel-mode 2 --use-least-squares
+point2dem -r Earth $dir/$dir-PC.tif --nodata-value -32767
+
