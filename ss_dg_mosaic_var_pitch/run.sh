0609fb7cc5304d7f32acd20f33441864a174f8fe
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-05-24 09:42:26 -0700

    dg_mosaic with variable pitch

---
+++ ss_dg_mosaic_var_pitch/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# In this testcase the various xml files have different pitches. We put a bugfix for that.
+
+set -x verbose
+d=../data
+dir=run
+rm -rfv $dir
+
+dg_mosaic $d/WV02_10JUN212134084_5pct.tif $d/WV02_10JUN212134096_5pct.tif $d/WV02_10JUN212134088_5pct.tif $d/WV02_10JUN212134100_5pct.tif --output-prefix run/run --reduce-percent 50 --preview
+
+
