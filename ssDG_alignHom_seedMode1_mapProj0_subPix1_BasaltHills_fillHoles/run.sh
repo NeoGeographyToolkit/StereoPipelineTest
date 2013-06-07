90ca775e74dbfbb4b3e2879dd6d0f71cfad7980f
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-06-07 09:32:02 -0700

    fill_holes

---
+++ ssDG_alignHom_seedMode1_mapProj0_subPix1_BasaltHills_fillHoles/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x verbose
+d=../data
+dir=run
+rm -rfv $dir
+
+stereo $d/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif $d/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.tif $d/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.xml $d/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.xml $dir/$dir --left-image-crop-win 1024 1024 2048 2048 --corr-max-levels 5 -t dg -s stereo.default --alignment-method homography --subpixel-mode 1 --threads 32 --subpixel-h-kernel 19 --subpixel-v-kernel 19 --corr-seed-mode 1
+
+point2dem -r Earth $dir/$dir-PC.tif --nodata-value -32767
