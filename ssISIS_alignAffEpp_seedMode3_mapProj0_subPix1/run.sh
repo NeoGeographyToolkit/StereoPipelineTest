990cc7f2bf06a8234be23aab63ec31df115404db
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-06-06 21:27:23 -0700

    One more sparse_disp test

--- ssISIS_alignAffEpp_seedMode3_mapProj0_subPix1/run.sh
+++ ssISIS_alignAffEpp_seedMode3_mapProj0_subPix1/run.sh
@@ -9,7 +9,7 @@ rm -rfv $dir
 export PYTHONPATH=$PYTHONPATH:$HOME/packages/lib64/python2.6/site-packages/
 export LD_LIBRARY_PATH=$HOME/projects/base_system/lib
 
-stereo $d/M0100115.cub $d/E0201461.cub $dir/$dir -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method affineepipolar --corr-seed-mode 3 --subpixel-mode 1
+stereo $d/M0100115.cub $d/E0201461.cub $dir/$dir -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method affineepipolar --corr-seed-mode 3 --subpixel-mode 1 --verbose
 point2dem -r mars $dir/$dir-PC.tif --nodata-value -32767
 
 
