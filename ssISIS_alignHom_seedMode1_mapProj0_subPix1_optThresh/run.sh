6c2079bfb1c4bc24e133b93c3718639ee2a1cb15
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-06-07 13:24:06 -0700

    optimal thresh testcase

---
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1_optThresh/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x verbose
+d=../data
+dir=run
+rm -rfv $dir
+
+stereo $d/M0100115.cub $d/E0201461.cub $dir/$dir -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1  --nodata-optimal-threshold-factor 0.5
+point2dem -r mars $dir/$dir-PC.tif --nodata-value -32767
+
+
+
