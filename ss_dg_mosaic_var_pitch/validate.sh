2767b7afaac28ac37e35a6af35f0aad0e6a02c14
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-08-11 21:14:55 -0700

    refine validation

--- ss_dg_mosaic_var_pitch/validate.sh
+++ ss_dg_mosaic_var_pitch/validate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-for g in gold/run.r50.tif gold/run.r50.xml gold/run.small.png; do
-  
+for g in gold/run.r50.xml gold/run.small.png; do
+
   if [ ! -e "$g" ]; then
       echo "ERROR: File $g does not exist."
       exit 1
@@ -16,7 +16,7 @@ for g in gold/run.r50.tif gold/run.r50.xml gold/run.small.png; do
 
   diff=$(cmp $f $g)
   echo diff is $diff
- 
+
   if [ "$diff" != "" ]; then
     echo Validation failed
     exit 1
@@ -24,6 +24,37 @@ for g in gold/run.r50.tif gold/run.r50.xml gold/run.small.png; do
 
 done
 
+file=run/run.r50.tif
+gold=gold/run.r50.tif
+
+if [ ! -e "$file" ]; then
+    echo "ERROR: File $file does not exist."
+    exit 1;
+fi
+
+if [ ! -e "$gold" ]; then
+    echo "ERROR: File $gold does not exist."
+    exit 1;
+fi
+
+# Remove cached xmls
+rm -fv "$file.aux.xml"
+rm -fv "$gold.aux.xml"
+
+cmp_images.sh x $file $gold
+gdalinfo -stats $file | grep -v Files | grep -v -i tif > run.txt
+gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold.txt
+
+diff=$(diff run.txt gold.txt)
+cat run.txt
+
+rm -f run.txt gold.txt
+
+echo diff is $diff
+if [ "$diff" != "" ]; then
+    echo Validation failed
+    exit 1
+fi
+
 echo Validation succeded
 exit 0
-
