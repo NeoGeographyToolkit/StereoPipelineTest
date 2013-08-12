8bd3575b4c63583ca3127a660c9c0c88bf076e69
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-08-12 08:41:24 -0700

    Tweak the code for doing stats

--- bin/cmp_images.sh
+++ bin/cmp_images.sh
@@ -1,38 +1,27 @@
 #!/bin/bash
 
-plainDiff=0
-if [ "$#" -eq 3 ]; then
-    shift
-    plainDiff=1
-fi
+# Compare the stats of two images by using gdalinfo.
 
+if [ "$#" -lt 2 ]; then echo Usage: $0 img1.tif img2.tif; exit 1; fi
+
+img1=$1
+img2=$2
+
+# Don't put these in /tmp, as we will run this script at the same time
+# for multiple data sets.
 out1=tmpimg1.txt
 out2=tmpimg2.txt
 
 # Remove cached xmls
-rm -fv "$1.aux.xml"
-rm -fv "$2.aux.xml"
+rm -fv "$img1.aux.xml"
+rm -fv "$img2.aux.xml"
 
-echo $1 > $out1
-echo $2 > $out2
+echo $img1 > $out1
+echo $img2 > $out2
 
-# Sanity checks, this is a bugfix for byss
-opts="-proj4"
-gdalinfo -stats $opts $1 > /dev/null
-status="$?"
-if [ $status -ne 0 ]; then
-    echo "gdalinfo does not support option: $opts"
-    opts=""
-fi
+gdalinfo -stats $img1 | grep STATISTICS_ | perl -pi -e "s#=# = #g" >> $out1
+gdalinfo -stats $img2 | grep STATISTICS_ | perl -pi -e "s#=# = #g" >> $out2
 
-gdalinfo -stats $opts $1 | grep STATISTICS_ | grep -v Min= | grep -v .tif | perl -pi -e "s#=# = #g" >> $out1
-gdalinfo -stats $opts $2 | grep STATISTICS_ | grep -v Min= | grep -v .tif | perl -pi -e "s#=# = #g" >> $out2
-
-if [ "$plainDiff" -eq 1 ]; then
-    diff $out1 $out2
-else
-    tkdiff $out1 $out2
-fi
-
-max_err.pl  $out1 $out2
+diff $out1 $out2
+max_err.pl $out1 $out2
 rm -f $out1 $out2
