55861094b30aa72d1ddb30424a3313a248c6a085
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-08-19 16:13:50 -0700

    pc_align testcase

---
+++ ss_pc_align_SanLuisResevoir/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x verbose
+d=../data
+dir=run
+rm -rfv $dir
+
+pc_align $d/zone10-CA_SanLuisResevoir-9m_sub5-PC.tif $d/zone10-CA_SanLuisResevoir-9m_sub5-PC_shift50_angle0.01.tif --save-transformed-source-points --output-prefix run/run
+
+
+
+
