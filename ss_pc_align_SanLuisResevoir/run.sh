b55c93169c2084b05462d2bee7272d1111c50d2c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-09-05 16:13:11 -0700

    pc_align: Minor interface change

--- ss_pc_align_SanLuisResevoir/run.sh
+++ ss_pc_align_SanLuisResevoir/run.sh
@@ -5,7 +5,8 @@ d=../data
 dir=run
 rm -rfv $dir
 
-pc_align $d/zone10-CA_SanLuisResevoir-9m_sub5-PC.tif $d/zone10-CA_SanLuisResevoir-9m_sub5-PC_shift50_angle0.01.tif --save-transformed-source-points --output-prefix run/run
+pc_align $d/zone10-CA_SanLuisResevoir-9m_sub5-PC.tif $d/zone10-CA_SanLuisResevoir-9m_sub5-PC_shift50_angle0.01.tif --save-transformed-source-points --output-prefix run/run --max-displacement -1
+
 
 
 
