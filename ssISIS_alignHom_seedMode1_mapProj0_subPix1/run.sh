d5f34f305d2b3faded8ba1f44ac1a22f7789779e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-09-12 13:33:44 -0700

    Add errorimage to regressions

--- ssISIS_alignHom_seedMode1_mapProj0_subPix1/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1/run.sh
@@ -4,7 +4,8 @@ set -x verbose
 rm -rfv run
 
 stereo ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1
-point2dem -r mars run/run-PC.tif --nodata-value -32767
+point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage
+
 
 
 
--- ssISIS_alignHom_seedMode1_mapProj0_subPix1_localHom/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1_localHom/run.sh
@@ -3,8 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --use-local-homography
-point2dem -r mars run/run-PC.tif --nodata-value -32767
-
+stereo ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --use-local-homography --compute-error-vector
+point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage
 
 
