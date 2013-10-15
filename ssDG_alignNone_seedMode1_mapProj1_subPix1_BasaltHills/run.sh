1d1898a4122f36a5a084cff7b4a4aff837902a47
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-10-15 10:11:28 -0700

    test for skip-image-normalization

--- ssDG_alignNone_seedMode1_mapProj1_subPix1_BasaltHills/run.sh
+++ ssDG_alignNone_seedMode1_mapProj1_subPix1_BasaltHills/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_rpc_5mpp.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_rpc_5mpp.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_rpc.xml ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_rpc.xml run/run ../data/zone10-CA_SanLuisResevoir-9m.tif --corr-max-levels 5 -t dg -s stereo.default --alignment-method none --subpixel-mode 1 --disable-fill-holes --threads 32 --subpixel-h-kernel 19 --subpixel-v-kernel 19 --corr-seed-mode 1 --left-image-crop-win 1024 1024 1024 2048
+stereo ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_rpc_5mpp.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_rpc_5mpp.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_rpc.xml ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_rpc.xml run/run ../data/zone10-CA_SanLuisResevoir-9m.tif --corr-max-levels 5 -t dg -s stereo.default --alignment-method none --subpixel-mode 1 --disable-fill-holes --threads 32 --subpixel-h-kernel 19 --subpixel-v-kernel 19 --corr-seed-mode 1 --left-image-crop-win 1024 1024 1024 2048 --skip-image-normalization
 
 point2dem -r Earth run/run-PC.tif --nodata-value -32767
 
