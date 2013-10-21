6626de05fe3f33efd6236e0bfab45262609e96d9
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-10-21 13:39:25 -0700

    point2mesh adjustmenets

--- ss_point2mesh/run.sh
+++ ss_point2mesh/run.sh
@@ -3,6 +3,6 @@
 set -x verbose
 rm -rfv run
 
-point2mesh ../data/zone10-CA_SanLuisResevoir-9m_sub5-PC.tif --output-prefix run/run
+point2mesh ../data/run-PC.tif ../data/run-L.tif -l --output-prefix run/run
 
 
