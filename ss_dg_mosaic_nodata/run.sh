4bffb2938ee518e59d73d720ea4b8498b6be10f1
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-10-29 14:07:48 -0700

    Regression for dg_mosaic with nodata

---
+++ ss_dg_mosaic_nodata/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+dg_mosaic --verbose ../data/WV01_11JAN131652222-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131652231-P1BS-10200100104A0300.r12.tif --reduce-percent 25 --preview --skip-rpc-gen --output-prefix run/run --nodata-value -100.125
+
+
+
