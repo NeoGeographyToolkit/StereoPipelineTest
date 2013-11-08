fcebcf7624f7d1f29d3c14e66bc29775033817df
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-11-07 18:10:29 -0800

    dg_mosaic: API change

--- ss_dg_mosaic_nodata/run.sh
+++ ss_dg_mosaic_nodata/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-dg_mosaic --verbose ../data/WV01_11JAN131652222-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131652231-P1BS-10200100104A0300.r12.tif --reduce-percent 25 --preview --skip-rpc-gen --output-prefix run/run --nodata-value -100.125
+dg_mosaic --verbose ../data/WV01_11JAN131652222-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131652231-P1BS-10200100104A0300.r12.tif --reduce-percent 25 --preview --skip-rpc-gen --output-prefix run/run --output-nodata-value -100.125
 
 
 
