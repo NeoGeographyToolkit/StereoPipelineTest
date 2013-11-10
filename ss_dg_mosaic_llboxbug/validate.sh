f3d4f34abfa164ec3cb697e42497b0036793d8d7
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-11-09 18:00:07 -0800

    Update tests for platforms

--- ss_dg_mosaic_llboxbug/validate.sh
+++ ss_dg_mosaic_llboxbug/validate.sh
@@ -14,6 +14,10 @@ for g in gold/run.r100.xml; do
       exit 1
   fi
 
+  cat $f | perl -pi -e "s#(\<|\>)# \$1 #g" > run.txt
+  cat $g | perl -pi -e "s#(\<|\>)# \$1 #g" > gold.txt
+  max_err.pl run.txt gold.txt
+  
   diff=$(cmp $f $g)
   echo diff is $diff
  
@@ -21,7 +25,7 @@ for g in gold/run.r100.xml; do
     echo Validation failed
     exit 1
   fi
-
+  
 done
 
 echo Validation succeded
