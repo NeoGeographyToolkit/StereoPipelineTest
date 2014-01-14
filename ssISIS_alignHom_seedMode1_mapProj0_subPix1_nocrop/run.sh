12b36771c7628b10b7202e2fdc8633310fe0c554
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-01-14 08:29:43 -0800

    Change option --min-valid-triangulation-error to reflect code

--- ssISIS_alignHom_seedMode1_mapProj0_subPix1_nocrop/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1_nocrop/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/M0100115_small.cub ../data/E0201461_small.cub run/run -s stereo.default  --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --min-valid-triangulation-error 2
+stereo ../data/M0100115_small.cub ../data/E0201461_small.cub run/run -s stereo.default  --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --max-valid-triangulation-error 2
 point2dem -r mars run/run-PC.tif --nodata-value -32767
 
 
