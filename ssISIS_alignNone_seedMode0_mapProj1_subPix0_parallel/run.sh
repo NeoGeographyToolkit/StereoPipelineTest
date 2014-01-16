92fde008315ffd4c2a1070e59dbb2ea50fb47819
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-01-16 09:43:26 -0800

    Increase timeout, otherwise get nonunique results

--- ssISIS_alignNone_seedMode0_mapProj1_subPix0_parallel/run.sh
+++ ssISIS_alignNone_seedMode0_mapProj1_subPix0_parallel/run.sh
@@ -5,7 +5,7 @@ set -x verbose
 rm -rfv run
 
 echo localhost > machines.txt
-par_opts="--processes 4 --threads-multi 2 --job-size-w 1024 --job-size-h 1024 --corr-timeout 600 --entry-point 0 --stop-point 6 --nodes-list machines.txt"
+par_opts="--processes 4 --threads-multi 2 --job-size-w 1024 --job-size-h 1024 --corr-timeout 6000 --entry-point 0 --stop-point 6 --nodes-list machines.txt"
 
 parallel_stereo $par_opts ../data/M0100115.map.cub ../data/E0201461.map.cub run/run -s stereo.default --corr-search -300 -300 -200 -200 --left-image-crop-win 0 1024 672 4864 --alignment-method none --corr-seed-mode 0 --subpixel-mode 0 --verbose --compute-error-vector
 point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage
