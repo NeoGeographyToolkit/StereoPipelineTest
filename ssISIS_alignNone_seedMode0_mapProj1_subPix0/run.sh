f8b6bb8a2a3d1c569abe6ddd2ff8e4ec3d0422ee
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-01-31 08:46:34 -0800

    Wipe unhelpful lengthy testcase ssISIS_alignNone_seedMode0_mapProj1_subPix0

--- ssISIS_alignNone_seedMode0_mapProj1_subPix0/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x verbose
-rm -rfv run
-
-stereo ../data/M0100115.map.cub ../data/E0201461.map.cub run/run -s stereo.default --corr-search -300 -300 -200 -200 --left-image-crop-win 0 1024 672 4864 --alignment-method none --corr-seed-mode 0 --subpixel-mode 0
-point2dem -r mars run/run-PC.tif --nodata-value -32767
-
