f4a6c7550f2284fa4574bdd06bc1ea3d82bc9a0b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-02-07 14:14:08 -0800

    Added test for ISIS frame camera

---
+++ ssISIS_alignAffEp_seedMode1_mapProj0_subPix1_frameCam/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+stereo  ../data/AS15-M-0759.lev1_crop.cub ../data/AS15-M-0760.lev1_crop.cub run/run --alignment-method affineepipolar --subpixel-mode 1 --disable-fill-holes --left-image-crop-win 1024 1024 1024 1024 --threads 1
+
+point2dem -r moon run/run-PC.tif --nodata-value -32767 --errorimage --fsaa 4
+
+
+
+
---
+++ ssISIS_alignAffEp_seedMode1_mapProj0_subPix1_frameCam/validate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in run/run-DEM.tif run/run-IntersectionErr.tif; do 
+
+  echo $file $gold
+  gold=${file/run\/run/gold\/run}
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+  # Remove cached xmls
+  rm -fv "$file.aux.xml"
+  rm -fv "$gold.aux.xml"
+
+  cmp_stats.sh $file $gold
+  gdalinfo -stats $file | grep -v Files | grep -v -i tif > run.txt
+  gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold.txt
+
+  diff=$(diff run.txt gold.txt)
+  cat run.txt
+
+  rm -f run.txt gold.txt
+
+  echo diff is $diff
+  if [ "$diff" != "" ]; then
+      echo Validation failed
+      exit 1
+  fi
+
+done
+
+echo Validation succeded
+exit 0
