2b16488aa005308198809ea9cdde10ff289d9b57
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-02-10 09:40:48 -0800

    datum is needed when no dem is present

--- ss_pc_align_SanLuisResevoir/run.sh
+++ ss_pc_align_SanLuisResevoir/run.sh
@@ -3,11 +3,5 @@
 set -x verbose
 rm -rfv run
 
-pc_align ../data/zone10-CA_SanLuisResevoir-9m_sub5-PC.tif ../data/zone10-CA_SanLuisResevoir-9m_sub5-PC_shift50_angle0.01.tif --save-transformed-source-points --output-prefix run/run --max-displacement -1
-
-
-
-
-
-
+pc_align ../data/zone10-CA_SanLuisResevoir-9m_sub5-PC.tif ../data/zone10-CA_SanLuisResevoir-9m_sub5-PC_shift50_angle0.01.tif --save-transformed-source-points --output-prefix run/run --max-displacement -1 --datum WGS_1984
 
