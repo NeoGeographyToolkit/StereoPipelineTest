4bb6da11a0d2aefb82bf36fbd509ff0d936ec5f9
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-03-03 12:52:38 -0800

    Test for pc_pc alignment

---
+++ ss_pc_align_mars_pc_pc/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+pc_align --max-displacement 100 ../data/ref-mars-PC.tif ../data/ref-mars-PC.tif  --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run
+
