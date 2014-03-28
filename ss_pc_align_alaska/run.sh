29216b77c51502413dcbc280528bc2fcaad6fe43
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-03-28 08:56:09 -0700

    pc_align: Use fewer points

--- ss_pc_align_alaska/run.sh
+++ ss_pc_align_alaska/run.sh
@@ -5,7 +5,3 @@ rm -rfv run
 
 pc_align ../data/rock_dem_50pct.tif ../data/lat_lon_z.csv --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --max-num-reference-points 1000000000 --max-displacement 100 
 
-
-
-
-
--- ss_pc_align_alaska_dem_dem/run.sh
+++ ss_pc_align_alaska_dem_dem/run.sh
@@ -3,6 +3,5 @@
 set -x verbose
 rm -rfv run
 
-pc_align ../data/rock_dem_50pct_crop1.tif ../data/rock_dem_50pct_crop2.tif --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --max-num-reference-points 1000000000 --max-displacement 1000
-
+pc_align ../data/rock_dem_50pct_crop1.tif ../data/rock_dem_50pct_crop2.tif --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --max-num-reference-points 100000 --max-num-source-points 10000 --max-displacement 1000
 
