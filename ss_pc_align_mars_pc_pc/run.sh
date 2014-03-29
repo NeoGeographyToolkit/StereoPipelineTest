375b6fb3e2547ce94e4224b24ce1f9288b51d261
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-03-28 19:31:56 -0700

    Make a testcase more relevant

--- ss_pc_align_mars_pc_pc/run.sh
+++ ss_pc_align_mars_pc_pc/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-pc_align --max-displacement 100 ../data/ref-mars-PC.tif ../data/ref-mars-PC.tif  --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run
+pc_align --max-displacement 100 ../data/ref-mars-PC.tif ../data/ref-mars-PC_trans.tif  --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run
 
