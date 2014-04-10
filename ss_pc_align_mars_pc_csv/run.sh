e090a573b18fb2fb6ea6d13ffbb5f87bc3ae7db8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-04-10 08:43:58 -0700

    Reduce number of points to load

--- ss_pc_align_mars_pc_csv/run.sh
+++ ss_pc_align_mars_pc_csv/run.sh
@@ -3,6 +3,6 @@
 set -x verbose
 rm -rfv run
 
-pc_align --max-displacement 100 ../data/ref-mars-PC.tif ../data/ref-mars.csv  --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --datum D_MARS
+pc_align --max-displacement 100 ../data/ref-mars-PC.tif ../data/ref-mars.csv  --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --datum D_MARS --max-num-reference-points 10000000
 
 
