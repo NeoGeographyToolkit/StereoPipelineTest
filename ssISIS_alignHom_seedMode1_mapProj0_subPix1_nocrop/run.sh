a331c1fd3dd7544fdfd68486b27b028875c7dd2d
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-04-21 18:07:29 -0700

    Minor test change

--- ssISIS_alignHom_seedMode1_mapProj0_subPix1_nocrop/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1_nocrop/run.sh
@@ -3,8 +3,8 @@
 set -x verbose
 rm -rfv run
 
-stereo --enable-fill-holes ../data/M0100115_small.cub ../data/E0201461_small.cub run/run -s stereo.default  --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --max-valid-triangulation-error 2
-point2dem -r mars run/run-PC.tif --nodata-value -32767
+stereo --save-double-precision-point-cloud ../data/M0100115_small.cub ../data/E0201461_small.cub run/run -s stereo.default --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1
+point2dem -r mars run/run-PC.tif --nodata-value -32767 --max-valid-triangulation-error 2
 
 
 
