fd1b01d0f666ca989052b8f35d89f8673481814e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-04-25 16:44:28 -0700

    Test setting seed-mode on command line

--- ssISIS_alignAffEpp_seedMode3_mapProj0_subPix1/run.sh
+++ ssISIS_alignAffEpp_seedMode3_mapProj0_subPix1/run.sh
@@ -3,9 +3,6 @@
 set -x verbose
 rm -rfv run
 
-
-stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method affineepipolar --corr-seed-mode 3 --subpixel-mode 1 --verbose
+stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method affineepipolar --subpixel-mode 1 --verbose
 point2dem -r mars run/run-PC.tif --nodata-value -32767
-
-
 
