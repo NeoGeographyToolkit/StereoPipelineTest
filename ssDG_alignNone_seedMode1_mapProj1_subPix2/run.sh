ffc2d7760a63c5921e8a9a2580dd594687ed429b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-04-30 16:12:40 -0700

    Add test with subpixel2 and --skip-image-normalization

--- ssDG_alignNone_seedMode1_mapProj1_subPix2/run.sh
+++ ssDG_alignNone_seedMode1_mapProj1_subPix2/run.sh
@@ -3,6 +3,6 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/left.tif ../data/right.tif ../data/left.xml ../data/right.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif  --disable-fill-holes -s stereo.default -t dg --alignment-method none --corr-seed-mode 1 --subpixel-mode 2 --left-image-crop-win 0 0 256 512
+stereo ../data/left.tif ../data/right.tif ../data/left.xml ../data/right.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif  --disable-fill-holes -s stereo.default -t dg --alignment-method none --corr-seed-mode 1 --subpixel-mode 2 --left-image-crop-win 0 0 256 512 --skip-image-normalization
 point2dem -r Earth run/run-PC.tif --nodata-value -32767
 
