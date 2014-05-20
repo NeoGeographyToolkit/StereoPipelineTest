514f6f9bc78db8544e147a280c2b96a1865f593e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-05-20 14:39:24 -0700

    test for --save-lon-lat-height-cloud-only

--- ssDG_alignNone_seedMode1_mapProj1_subPix2_leastSq/run.sh
+++ ssDG_alignNone_seedMode1_mapProj1_subPix2_leastSq/run.sh
@@ -4,5 +4,5 @@ set -x verbose
 rm -rfv run
 
 stereo ../data/left.tif ../data/right.tif ../data/left.xml ../data/right.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif -s stereo.default -t dg --left-image-crop-win 0 0 150 150 --alignment-method none --corr-seed-mode 1 --subpixel-mode 2 --use-least-squares
-point2dem -r Earth run/run-PC.tif --nodata-value -32767
+point2dem -r Earth run/run-PC.tif --nodata-value -32767 --save-lon-lat-height-cloud-only
 
