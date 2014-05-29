c2e119e6966d78f42222d373d808e784ddabbf69
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-05-28 17:55:15 -0700

    tweak MER testcase

--- ssPinHole_alignHom_seedMode1_mapProj0_subPix1_MER/run.sh
+++ ssPinHole_alignHom_seedMode1_mapProj0_subPix1_MER/run.sh
@@ -5,5 +5,6 @@ rm -rfv run
 
 stereo ../data/1m163693341eff5000p2956m2f1.img ../data/1m163693440eff5000p2956m2f1.img ../data/1m163693341eff5000p2956m2f1.cahvor ../data/1m163693440eff5000p2956m2f1.cahvor run/run
 
-point2dem -r Earth run/run-PC.tif --nodata-value -32767 --remove-outliers
+point2dem -r Moon run/run-PC.tif --nodata-value -32767 
+
 
