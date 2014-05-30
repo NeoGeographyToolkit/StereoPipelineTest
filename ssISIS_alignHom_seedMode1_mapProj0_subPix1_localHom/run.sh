ebd40152025c9bc5f866bf53119fe7e657b02589
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-05-29 21:18:31 -0700

    Testcase for recently fixed bug

--- ssISIS_alignHom_seedMode1_mapProj0_subPix1_localHom/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1_localHom/run.sh
@@ -4,6 +4,6 @@ set -x verbose
 rm -rfv run
 
 stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --use-local-homography --compute-error-vector
-point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage
+point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage --x-offset -161 --t_srs "+proj=eqc +lon_0=0 +x_0=0 +y_0=0 +a=3396190 +b=3396190 +units=m +no_defs" --tr 5
 
 
