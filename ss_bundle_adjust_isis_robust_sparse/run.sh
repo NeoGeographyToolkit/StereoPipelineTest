c7302dd946f0934d43bc01bf914830a37838e0b3
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-06-09 13:51:29 -0700

    unit tests for ceres

---
+++ ss_bundle_adjust_isis_robust_sparse/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub --bundle-adjuster robustsparse --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/run
+
+
