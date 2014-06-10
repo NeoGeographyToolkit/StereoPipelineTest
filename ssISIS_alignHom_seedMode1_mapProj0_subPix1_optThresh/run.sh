cc4407b612af81d90a003aa2759f4e52f4fe81e0
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-06-10 14:46:56 -0700

    Add stereo test using ba

--- ssISIS_alignHom_seedMode1_mapProj0_subPix1_optThresh/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1_optThresh/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1  --nodata-optimal-threshold-factor 0.5
+stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1  --nodata-optimal-threshold-factor 0.5 --bundle-adjust-prefix ../data/run_ba/run
 point2dem -r mars run/run-PC.tif --nodata-value -32767
 
 
