cc16b9de0c16d43a0ad8022d72b913f02e0a9ec3
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-06-16 09:27:48 -0700

    Make testcase smaller

--- ss_pc_align_mars_dt4/run.sh
+++ ss_pc_align_mars_dt4/run.sh
@@ -3,6 +3,6 @@
 set -x verbose
 rm -rfv run
 
-pc_align --max-displacement 100 ../data/H2145_0000_DT4_crop.cub ../data/H2145_0000_DT4_crop.cub --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --datum D_MARS 
+pc_align --max-displacement 100 ../data/H2145_0000_DT4_crop.cub ../data/H2145_0000_DT4_crop.cub --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --datum D_MARS  --max-num-source-points 1000000 --max-num-reference-points 10000
 
 
