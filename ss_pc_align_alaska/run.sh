ba9c2ead13d62e4eb262bdbb570e67d6a9ae118b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-06-16 13:34:58 -0700

    Fine-tune test

--- ss_pc_align_alaska/run.sh
+++ ss_pc_align_alaska/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-pc_align ../data/rock_dem_50pct.tif ../data/lat_lon_z.csv --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --max-num-reference-points 10000000 --max-num-source-points 10000  --max-displacement 100 
+pc_align ../data/rock_dem_50pct.tif ../data/lat_lon_z.csv --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --max-num-reference-points 1000000 --max-num-source-points 5000  --max-displacement 100 
 
