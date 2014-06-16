9f5450928e8d32c6253c0137e65aa87be9b1a634
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-06-16 09:09:59 -0700

    Add testcase for .cub dtm

---
+++ ss_pc_align_mars_dt4/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+pc_align --max-displacement 100 ../data/H2145_0000_DT4_crop.cub ../data/H2145_0000_DT4_crop.cub --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --datum D_MARS 
+
+
