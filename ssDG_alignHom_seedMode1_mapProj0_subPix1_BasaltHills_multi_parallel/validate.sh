fffaae6e72e2289a780ea0b1705ec8bb01a587aa
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-08-05 14:04:05 -0700

    New test for multiview

--- ssDG_alignHom_seedMode1_mapProj0_subPix1_BasaltHills_multi_parallel/validate.sh
+++ ssDG_alignHom_seedMode1_mapProj0_subPix1_BasaltHills_multi_parallel/validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-for file in run/run-DEM.tif run/run-IntersectionErr.tif; do 
+for file in run/run-DEM.tif run/run-DRG.tif; do 
 
   echo $file $gold
   gold=${file/run\/run/gold\/run}
