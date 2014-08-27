6a2f11c91fcb9e75fe1d6290f24d6eaecac98b51
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-08-27 14:25:36 -0700

    Add dg bundle adjust testcase

---
+++ ss_bundle_adjust_dg_ceres/validate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in run/run-WV01_11JAN131652275-P1BS-10200100104A0300.r12.adjust \
+        	run/run-WV01_11JAN131653225-P1BS-1020010011862E00.r12.adjust; do 
+
+  gold=${file/run\/run/gold\/run}
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+  echo diff $file $gold
+  diff=$(diff $file $gold)
+
+  echo diff is $diff
+  if [ "$diff" != "" ]; then
+      echo Validation failed
+      exit 1
+  fi
+
+done
+
+echo Validation succeded
+exit 0
