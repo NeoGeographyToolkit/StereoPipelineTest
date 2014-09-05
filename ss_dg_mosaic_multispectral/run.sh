fed5f8d6f15f2dfeef273a2447c3c9226cf1d731
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-09-05 15:06:52 -0700

    multispectral tests updated

--- ss_dg_mosaic_multispectral/run.sh
+++ ss_dg_mosaic_multispectral/run.sh
@@ -3,7 +3,5 @@
 set -x verbose
 rm -rfv run
 
-dg_mosaic --verbose ../data/14JUN19185250*TIF --output-prefix run/run
-
-
+dg_mosaic --verbose ../data/14JUN19185250*TIF --output-prefix run/run --band 2
 
