40b8c22ce053c04dd9cad809748344b53397488e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-09-24 18:16:45 -0700

    tweak test

--- ss_point2dem_las/run.sh
+++ ss_point2dem_las/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-point2dem -r Earth --tr 0.000012676199264 ../data/stadium-utm.laz -o run/run --nodata-value -32767
+point2dem -r Earth --tr 1 ../data/stadium-utm.laz -o run/run --nodata-value -32767
 
