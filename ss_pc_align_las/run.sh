643403e761a86e5d01276eb160b4a9d5a40ccbbd
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-09-30 11:27:55 -0700

    Tweak run for low mem

--- ss_pc_align_las/run.sh
+++ ss_pc_align_las/run.sh
@@ -3,5 +3,10 @@
 set -x verbose
 rm -rfv run
 
-pc_align ../data/stadium-utm.laz ../data/stadium-utm.csv --max-displacement 100 --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --csv-format 'utm:10N 1:easting 2:northing 3:height_above_datum'
+machine=$(uname -n | grep centos-32-5)
+if [ "$machine" !=  "" ]; then 
+ opt="--max-num-reference-points 1000000" # to avoid running out of memory
+fi
+
+pc_align ../data/stadium-utm.laz ../data/stadium-utm.csv --max-displacement 100 --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --csv-format 'utm:10N 1:easting 2:northing 3:height_above_datum' $opt
 
