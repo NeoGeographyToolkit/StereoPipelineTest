ae3136eb6f9b32c1616f1d43fa84ba0a8b64778c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-09-30 15:41:07 -0700

    dem_mosaic add tests for first, last, mean, max, etc.

---
+++ ss_dem_mosaic_first_last/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+echo ../data/dem1_10pct.tif > list.txt
+echo ../data/dem2_10pct.tif >> list.txt
+
+for f in "" first last min max mean median count; do 
+	 
+  opt=""
+  if [ "$f" != "" ]; then 
+    opt="--$f";
+  fi
+
+  dem_mosaic ../data/dem_clip1.tif ../data/dem_clip2.tif -o run/run $opt
+done
+
---
+++ ss_dem_mosaic_first_last/validate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+rm -f run/*xml gold/*xml
+
+for f in "" first- last- min- max- mean- median- count-; do 
+
+  file=run/run-tile-"$f"0.tif
+  gold=gold/run-tile-"$f"0.tif
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+  cmp_stats.sh $file $gold
+  gdalinfo -stats $file | grep -v Files | grep -v -i tif > run.txt
+  gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold.txt
+
+  diff=$(diff run.txt gold.txt)
+
+  if [ "$diff" != "" ]; then
+    echo Validation failed
+    echo diff is $diff
+    exit 1
+  fi
+done
+
+echo Validation succeded
+exit 0
