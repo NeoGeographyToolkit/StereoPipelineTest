14b540421da08cacedc6a8d01f289edeb5b349e3
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-10-05 13:05:59 -0700

    Tweak tests

--- ssDG_alignAffEpp_seedMode1_mapProj0_subPix1/run.sh
+++ ssDG_alignAffEpp_seedMode1_mapProj0_subPix1/run.sh
@@ -3,6 +3,6 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.xml ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.xml run/run --disable-fill-holes --stereo-file stereo.default --left-image-crop-win 2048 0 1024 1024 -t dg --alignment-method affineepipolar --corr-seed-mode 1 --subpixel-mode 1
+stereo --corr-timeout 6000 ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.xml ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.xml run/run --disable-fill-holes --stereo-file stereo.default --left-image-crop-win 2048 0 1024 1024 -t dg --alignment-method affineepipolar --corr-seed-mode 1 --subpixel-mode 1
 
 point2dem -r Earth run/run-PC.tif --nodata-value -32767
--- ssISIS_alignAffEpp_seedMode1_mapProj0_subPix1/run.sh
+++ ssISIS_alignAffEpp_seedMode1_mapProj0_subPix1/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method affineepipolar --corr-seed-mode 1 --subpixel-mode 1
+stereo --corr-timeout 6000 --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method affineepipolar --corr-seed-mode 1 --subpixel-mode 1
 point2dem -r mars run/run-PC.tif --nodata-value -32767
 
 
