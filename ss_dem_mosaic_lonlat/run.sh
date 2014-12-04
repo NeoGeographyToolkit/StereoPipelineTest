a62fd16347876ae6f7412d62f972442108e5700d
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-12-04 13:32:25 -0800

    Update one test

--- ss_dem_mosaic_lonlat/run.sh
+++ ss_dem_mosaic_lonlat/run.sh
@@ -6,5 +6,5 @@ rm -rfv run
 echo ../data/dem1_10pct.tif > list.txt
 echo ../data/dem2_10pct.tif >> list.txt
 
-dem_mosaic -l list.txt -o run/run --blending-len 200 --erode-len 1  --t_projwin 125 -23 131 -14 --tr 0.01
+dem_mosaic -l list.txt -o run/run --blending-len 200 --erode-len 1  --t_projwin 125 -23 131 -14 --tr 0.01  --hole-fill-len 200
 
