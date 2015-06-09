559cbba4351cdb7254165695c61d5a81d0329ed9
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-06-09 09:05:51 -0700

    ss_point2dem_csv_proj4: Fix test

--- ss_point2dem_csv_proj4/run.sh
+++ ss_point2dem_csv_proj4/run.sh
@@ -3,6 +3,6 @@
 set -x verbose
 rm -rfv run
 
-point2dem -r Earth ../data/zone10-CA_SanLuisResevoir-9m_10pct-utm.csv --tr .0009765625000000 -o run/run --nodata-value -32767 --csv-format '1:easting 2:northing 3:height_above_datum' --csv-proj4 '+proj=utm +zone=10 +datum=WGS84 +units=m +no_defs'
+point2dem -r Earth ../data/zone10-CA_SanLuisResevoir-9m_10pct-utm.csv --tr 10 -o run/run --nodata-value -32767 --csv-format '1:easting 2:northing 3:height_above_datum' --csv-proj4 '+proj=utm +zone=10 +datum=WGS84 +units=m +no_defs'
 
 
