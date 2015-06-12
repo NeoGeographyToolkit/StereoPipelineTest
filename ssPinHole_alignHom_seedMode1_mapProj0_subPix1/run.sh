57eca997e674ac7209f334a1aa239b2a0e7598de
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-06-12 10:00:07 -0700

    tests for adjusted models

--- ssPinHole_alignHom_seedMode1_mapProj0_subPix1/run.sh
+++ ssPinHole_alignHom_seedMode1_mapProj0_subPix1/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/1n270487304eff90cip1952l0m1.tif ../data/1n270487304eff90cip1952r0m1.tif ../data/1n270487304eff90cip1952l0m1.cahvor ../data/1n270487304eff90cip1952r0m1.cahvor run/run -s stereo.default --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1
+stereo ../data/1n270487304eff90cip1952l0m1.tif ../data/1n270487304eff90cip1952r0m1.tif ../data/1n270487304eff90cip1952l0m1.cahvor ../data/1n270487304eff90cip1952r0m1.cahvor run/run -s stereo.default --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --bundle-adjust-prefix ../data/run_pinhole_ba/run
 point2dem --nodata-value -32767 --semi-major-axis 1 --semi-minor-axis 1 run/run-PC.tif
 
 
