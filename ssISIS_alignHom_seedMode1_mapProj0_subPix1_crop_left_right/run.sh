bd6bbc684d6d489e265857d05ae43e838dcf07f9
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-06-12 16:53:05 -0700

    Add testcase with left and right crop win

---
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1_crop_left_right/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default  --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --filter-mode 2 --left-image-crop-win 100 2200 1000 2000 --right-image-crop-win 201 1600 1200 1983
+point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage --fsaa 4 --use-surface-sampling
+
+
