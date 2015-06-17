5d0b0139cb11c94dddc129947a084848ad4dccb7
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-06-17 15:15:18 -0700

    Tweak test

--- ssISIS_alignAffEpp_seedMode1_mapProj1_subPix1/run.sh
+++ ssISIS_alignAffEpp_seedMode1_mapProj1_subPix1/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo --corr-timeout 3000 --enable-fill-holes ../data/M0100115.map.small.tif ../data/E0201461.map.small.tif ../data/M0100115.cub ../data/E0201461.cub  run/run ../data/M0100115_dem_lowRes.tif -s stereo.default --alignment-method none --corr-seed-mode 1 --subpixel-mode 1 -t isismapisis --threads 1 #--left-image-crop-win 200 500 200 200
+stereo --corr-timeout 3000 --enable-fill-holes ../data/M0100115.map.small.tif ../data/E0201461.map.small.tif ../data/M0100115.cub ../data/E0201461.cub  run/run ../data/M0100115_dem_lowRes.tif -s stereo.default --alignment-method none --corr-seed-mode 1 --subpixel-mode 1 -t isismapisis 
 point2dem -r mars run/run-PC.tif --nodata-value -32767
 
 
