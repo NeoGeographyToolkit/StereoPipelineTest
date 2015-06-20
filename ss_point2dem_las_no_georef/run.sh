e7161d15b5b2f8b849fcc643e734d2b5a8528720
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-06-20 10:45:56 -0700

    Add ss_point2dem_las_no_georef

--- ss_point2dem_las_no_georef/run.sh
+++ ss_point2dem_las_no_georef/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-point2dem -r Earth --tr 1 ../data/ref-PC.las -o run/run --nodata-value -32767
+point2dem -r Earth --tr 0.01 ../data/ref-PC.las -o run/run --nodata-value -32767
 
