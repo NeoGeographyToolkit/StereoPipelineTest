b6f98a28da7bfb0c2dfd14b1a89c6173c8b66be4
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-07-20 22:29:27 -0700

    parallel_stereo with right-crop-win

--- ssISIS_alignHom_seedMode1_mapProj0_subPix1_crop_left_right/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1_crop_left_right/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default  --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --filter-mode 2 --left-image-crop-win 100 2200 1000 2000 --right-image-crop-win 201 1600 1200 1983
+parallel_stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default  --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --filter-mode 2 --left-image-crop-win 100 2200 1000 2000 --right-image-crop-win 201 1600 1200 1983
 point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage --fsaa 4 --use-surface-sampling
 
 
