73980bc030ced35dcb51047aee6df759e9f12182
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-08-15 15:25:34 -0700

    Make test smallers to run faster

--- ssPinHole_alignNone_seedMode1_mapProj1_subPix1/run.sh
+++ ssPinHole_alignNone_seedMode1_mapProj1_subPix1/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo -t pinholemappinhole ../data/DZB1212-500082L002001_bb_mapproj_1e4.tif ../data/DZB1212-500082L003001_bb_mapproj_1e4.tif ../data/DZB1212-500082L002001_bb_50pct_2x.pinhole ../data/DZB1212-500082L003001_bb_50pct_2x.pinhole run/run ../data/good_dem.tif  -s stereo.default --corr-seed-mode 1 --subpixel-mode 1
+stereo -t pinholemappinhole ../data/DZB1212-500082L002001_bb_mapproj_1e4.tif ../data/DZB1212-500082L003001_bb_mapproj_1e4.tif ../data/DZB1212-500082L002001_bb_50pct_2x.pinhole ../data/DZB1212-500082L003001_bb_50pct_2x.pinhole run/run ../data/good_dem.tif -s stereo.default --corr-seed-mode 1 --subpixel-mode 1 --left-image-crop-win 3701 751 1165 988 --right-image-crop-win 3672 810 1091 900
 
 point2dem --nodata-value -32767 --semi-major-axis 1 --semi-minor-axis 1 run/run-PC.tif
 
--- ssRPC_alignNone_seedMode1_mapProj0_subPix1/run.sh
+++ ssRPC_alignNone_seedMode1_mapProj0_subPix1/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/WV01_11JAN131652222-P1BS-10200100104A0300_sub8.tif ../data/WV01_11JAN131653180-P1BS-1020010011862E00_sub8.tif ../data/WV01_11JAN131652222-P1BS-10200100104A0300_sub8.xml ../data/WV01_11JAN131653180-P1BS-1020010011862E00_sub8.xml run/run -s stereo.default --disable-fill-holes --corr-search -20 -20 20 20 --left-image-crop-win 4096 2048 1024 1024 -t rpc --alignment-method none --corr-seed-mode 1 --subpixel-mode 1
+stereo ../data/WV01_11JAN131652222-P1BS-10200100104A0300_sub8.tif ../data/WV01_11JAN131653180-P1BS-1020010011862E00_sub8.tif ../data/WV01_11JAN131652222-P1BS-10200100104A0300_sub8.xml ../data/WV01_11JAN131653180-P1BS-1020010011862E00_sub8.xml run/run -s stereo.default --disable-fill-holes --corr-search -15 -15 15 15 --left-image-crop-win 4096 2048 512 512 -t rpc --alignment-method none --corr-seed-mode 1 --subpixel-mode 1
 point2dem -r Earth run/run-PC.tif --nodata-value -32767
