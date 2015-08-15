8b317ce85e066311dad6d4178f924abae5e5dedf
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-08-15 15:40:06 -0700

    Make more testcases smaller

--- ssISIS_alignAffEpp_seedMode3_mapProj0_subPix1/run.sh
+++ ssISIS_alignAffEpp_seedMode3_mapProj0_subPix1/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method affineepipolar --subpixel-mode 1 --verbose
+stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 1200 --alignment-method affineepipolar --subpixel-mode 1 --verbose
 point2dem -r mars --gnomonic --proj-lat 141.5608796 --proj-lon 34.2926059 run/run-PC.tif
 
 
--- ss_pc_align_mars_pc_pc/run.sh
+++ ss_pc_align_mars_pc_pc/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-pc_align --max-displacement 100 ../data/ref-mars-PC.tif ../data/ref-mars-PC_trans.tif  --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run
+pc_align --max-displacement 100 ../data/ref-mars-PC-crop.tif ../data/ref-mars-PC_trans-crop.tif  --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run  --max-num-reference-points 1000000 --max-num-source-points 10000
 
