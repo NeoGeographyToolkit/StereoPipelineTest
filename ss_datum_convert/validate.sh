dc59d447757bc965fe2ec79f41b68fad558ef9b9
Scott <scott.t.mcmichael@nasa.gov>
2015-08-24 18:31:54 -0700

    Added datum_convert test

---
+++ ss_datum_convert/validate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+file=run/run-transform.txt
+gold=gold/run-transform.txt
+
+if [ ! -e "$file" ]; then
+    echo "ERROR: File $file does not exist."
+    exit 1;
+fi
+
+if [ ! -e "$gold" ]; then
+    echo "ERROR: File $gold does not exist."
+    exit 1;
+fi
+
+diff=$(diff $file $gold)
+
+echo diff is $diff
+if [ "$diff" != "" ]; then
+    echo Validation failed
+    exit 1
+fi
+
+echo Validation succeded
+exit 0
+
