2a795f966a8e11583f28c3d3870a71f32f5e73db
Scott <scott.t.mcmichael@nasa.gov>
2015-08-27 16:41:49 -0700

    Finished pc_merge tests

--- ss_pc_merge/run.sh
+++ ss_pc_merge/run.sh
@@ -2,9 +2,9 @@
 
 set -x verbose
 rm -rfv run
+mkdir run
 
-
-pc_merge ../data/ref-mars-crop1-PC.tif ../data/ref-mars-crop2-PC.tif -o run/run-PC.tif
+/byss/smcmich1/repo/StereoPipeline/src/asp/Tools/pc_merge ../data/ref-mars-crop1-PC.tif ../data/ref-mars-crop2-PC.tif -o run/run-PC.tif
 point2dem -r mars run/run-PC.tif --nodata-value -32768 
 
 
