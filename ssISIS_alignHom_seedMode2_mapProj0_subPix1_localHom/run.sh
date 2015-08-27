fedc04e618bb5f7875a0611068d26ce709a69eb6
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-08-27 09:26:11 -0700

    Speed up tests a bit

--- ssISIS_alignHom_seedMode2_mapProj0_subPix1_localHom/run.sh
+++ ssISIS_alignHom_seedMode2_mapProj0_subPix1_localHom/run.sh
@@ -3,5 +3,6 @@
 set -x verbose
 rm -rfv run
 # Multiview 
-stereo --enable-fill-holes ../data/M0100115_crop.cub ../data/E0201461.cub ../data/E0201461_copy.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 1200  --disparity-estimation-dem ../data/ref-DEM.tif --disparity-estimation-dem-error 5 --alignment-method homography --corr-seed-mode 2 --subpixel-mode 1 --use-local-homography
+stereo --enable-fill-holes ../data/M0100115_crop2.cub ../data/E0201461_crop.cub ../data/E0201461_crop2.cub run/run -s stereo.default --left-image-crop-win 0 124 672 4864 --disparity-estimation-dem ../data/ref-DEM.tif --disparity-estimation-dem-error 5 --alignment-method homography --corr-seed-mode 2 --subpixel-mode 1 --use-local-homography
+
 point2dem -r mars run/run-PC.tif --nodata-value -32767
