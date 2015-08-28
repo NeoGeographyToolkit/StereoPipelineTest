1ad524d8fd8546d9d62a73adea278cfafcbcaffe
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-08-28 13:14:21 -0700

    Add tests for colormap and hillshade

---
+++ ss_colormap/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+hillshade ../data/dem1_10pct.tif  -o run/run-colormap.tif
+
+
---
+++ ss_colormap/validate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in run/run-colormap.tif; do 
+
+  echo $file $gold
+  gold=${file/run\/run/gold\/run}
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+  # Remove cached xmls
+  rm -fv "$file.aux.xml"
+  rm -fv "$gold.aux.xml"
+
+  cmp_stats.sh $file $gold
+  gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v xml > run.txt
+  gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v xml > gold.txt
+
+  diff=$(diff run.txt gold.txt)
+  cat run.txt
+
+  rm -f run.txt gold.txt
+
+  echo diff is $diff
+  if [ "$diff" != "" ]; then
+      echo Validation failed
+      exit 1
+  fi
+
+done
+
+echo Validation succeded
+exit 0
