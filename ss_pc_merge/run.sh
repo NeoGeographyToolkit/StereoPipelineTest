62e4ccb158504d6b1daf6323e2ed7b85270ea518
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-08-28 09:54:25 -0700

    pc_merge: Remove hardcoded path

--- ss_pc_merge/run.sh
+++ ss_pc_merge/run.sh
@@ -4,7 +4,7 @@ set -x verbose
 rm -rfv run
 mkdir run
 
-/byss/smcmich1/repo/StereoPipeline/src/asp/Tools/pc_merge ../data/ref-mars-crop1-PC.tif ../data/ref-mars-crop2-PC.tif -o run/run-PC.tif
+pc_merge ../data/ref-mars-crop1-PC.tif ../data/ref-mars-crop2-PC.tif -o run/run-PC.tif
 point2dem -r mars run/run-PC.tif --nodata-value -32768 
 
 
