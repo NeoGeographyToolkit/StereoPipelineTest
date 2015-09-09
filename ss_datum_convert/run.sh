95a2671dadca640986a5f3351022ec244a599ec7
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-09-09 11:55:00 -0700

    Tweak tests a bit

--- ss_datum_convert/run.sh
+++ ss_datum_convert/run.sh
@@ -5,7 +5,6 @@ rm -rfv run
 
 
 # Generate copy of the DEM in a new datum
-mkdir run
-datum_convert ../data/san_luis_dam_75m.dem.tif wgs84 run/san_luis_dam_75m_wgs84.tif
+datum_convert ../data/san_luis_dam_75m.dem.tif run/san_luis_dam_75m_wgs84.tif --output-datum wgs84
 
 
