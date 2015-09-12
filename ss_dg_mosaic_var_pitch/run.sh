b26fe480111d2252179311bab830f6eb6c2e5c3d
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-09-12 08:36:35 -0700

    Update image_calc and dg_mosaic tests

--- ss_dg_mosaic_var_pitch/run.sh
+++ ss_dg_mosaic_var_pitch/run.sh
@@ -5,6 +5,6 @@
 set -x verbose
 rm -rfv run
 
-dg_mosaic --verbose ../data/WV02_10JUN212134084-P1BS-1030010006271A00.r5.tif  ../data/WV02_10JUN212134088-P1BS-1030010006271A00.r5.tif  ../data/WV02_10JUN212134096-P1BS-1030010006271A00.r5.tif  ../data/WV02_10JUN212134100-P1BS-1030010006271A00.r5.tif --skip-rpc-gen --output-prefix run/run --reduce-percent 50 --preview
+dg_mosaic --verbose ../data/WV02_10JUN212134084-P1BS-1030010006271A00.r5.tif  ../data/WV02_10JUN212134088-P1BS-1030010006271A00.r5.tif  ../data/WV02_10JUN212134096-P1BS-1030010006271A00.r5.tif  ../data/WV02_10JUN212134100-P1BS-1030010006271A00.r5.tif --skip-rpc-gen --output-prefix run/run --reduce-percent 50 --preview --fix-seams
 
 
--- ss_image_calc/run.sh
+++ ss_image_calc/run.sh
@@ -4,5 +4,5 @@ set -x verbose
 rm -rfv run
 
 mkdir run
-image_calc -o run/output.tif -d 6 -c "pow(var_0/3.0, 1.1)"  ../data/dem1_10pct.tif
+image_calc -o run/output.tif -d float64 -c "pow(var_0/3.0, 1.1)"  ../data/dem1_10pct.tif
 
