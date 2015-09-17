655d8b3bd7cbf96ece33d0dccff54e8b5696a9da
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-09-16 17:04:00 -0700

    Make pc_merge also merge single channel images

--- ss_pc_merge/run.sh
+++ ss_pc_merge/run.sh
@@ -2,9 +2,11 @@
 
 set -x verbose
 rm -rfv run
-mkdir run
 
-pc_merge ../data/ref-mars-crop1-PC.tif ../data/ref-mars-crop2-PC.tif -o run/run-PC.tif
-point2dem -r mars run/run-PC.tif --nodata-value -32768 
+pc_merge ../data/ref-mars-crop1-PC.tif ../data/ref-mars-crop2-PC.tif -o run/run-merge-PC.tif
+
+pc_merge ../data/ref-mars-crop1-L.tif ../data/ref-mars-crop2-L.tif -o run/run-merge-L.tif  
+
+point2dem -r mars run/run-merge-PC.tif --nodata-value -32768 --orthoimage run/run-merge-L.tif 
 
 
