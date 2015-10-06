d5b8967a1414c0c6a42f4125ca1d479013d18bc6
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-10-05 18:41:07 -0700

    Test for dem_mosaic save index map and save dem weights

--- ss_dem_mosaic_first_last/run.sh
+++ ss_dem_mosaic_first_last/run.sh
@@ -14,5 +14,13 @@ for f in "" first last min max mean stddev median count; do
   fi
 
   dem_mosaic ../data/dem_clip1.tif ../data/dem_clip2.tif -o run/run $opt
+
+  if [ "$f" = "" ] || [ "$f" == "first" ] || [ "$f" == "last" ] || \
+	  [ "$f" == "min" ] || [ "$f" == "max" ];  then
+  	for j in 0 1; do
+	   dem_mosaic ../data/dem_clip1.tif ../data/dem_clip2.tif -o run/run $opt --save-dem-weight $j
+	 done
+  fi
+  
 done
 
--- ss_dem_mosaic_utm_lonlat/run.sh
+++ ss_dem_mosaic_utm_lonlat/run.sh
@@ -5,4 +5,6 @@ rm -rfv run
 
 dem_mosaic ../data/clip2_utm.tif ../data/clip1_lonlat.tif -o run/run
 
+dem_mosaic ../data/clip2_utm.tif ../data/clip1_lonlat.tif -o run/run --save-index-map --first
+
 
