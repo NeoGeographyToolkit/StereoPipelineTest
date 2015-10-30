4cb9d783577f795d049367de36ab6e81a726476b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-10-30 16:03:36 -0700

    Tweak newly added test

--- ssISISMAPISIS_alignHom_seedMode1_mapProj0_subPix1/run.sh
+++ ssISISMAPISIS_alignHom_seedMode1_mapProj0_subPix1/run.sh
@@ -3,7 +3,8 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/M0100115.tif ../data/E0201461.tif ../data/M0100115.cub ../data/E0201461.cub run/run ../data/moc-ref-DEM.tif
+stereo ../data/M0100115.tif ../data/E0201461.tif ../data/M0100115.cub ../data/E0201461.cub run/run ../data/moc-ref-DEM.tif --left-image-crop-win 141 1881 585 613 --right-image-crop-win 0 2036 578 642
+
 
 point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage --fsaa 4 --use-surface-sampling
 
