cd8c020865b3edab3d3acf3298d46a86a9877e28
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-10-30 21:28:43 -0700

    Refine a bit the rpc tests

--- ss_mapproject_isis/run.sh
+++ ss_mapproject_isis/run.sh
@@ -3,5 +3,8 @@
 set -x verbose
 rm -rfv run
 
-mapproject ../data/sfs-DEM.tif ../data/sfs-cam.cub run/run.tif --tile-size 50
+echo localhost > machines.txt
+
+mapproject ../data/sfs-DEM.tif ../data/sfs-cam.cub run/run.tif --tile-size 50 --nodes-list machines.txt
+
 
--- ss_rpc_mapproject_unitDegree/run.sh
+++ ss_rpc_mapproject_unitDegree/run.sh
@@ -2,6 +2,7 @@
 
 set -x verbose
 rm -rfv run
+echo localhost > machines.txt
 
-mapproject --mpp 10 ../data/ref-DEM-unitDeg.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.xml run/run-RPC.tif -t dg
+mapproject --mpp 10 ../data/ref-DEM-unitDeg.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.xml run/run-RPC.tif -t dg --tile-size 512 --nodes-list machines.txt
 
