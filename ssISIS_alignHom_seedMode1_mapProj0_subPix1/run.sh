684d5650e2eb9d92ccfc8aa3ee9a1d61103994d0
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-11-02 14:11:57 -0800

    Temporarily turn off opencv ip detection mode, to debug mac problems

--- ssISIS_alignHom_seedMode1_mapProj0_subPix1/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --filter-mode 2 --ip-detect-method 2
+stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --filter-mode 2 --ip-detect-method 0 # 2
 point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage --fsaa 4 --use-surface-sampling
 
 
