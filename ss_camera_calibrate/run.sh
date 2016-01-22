269aaaddcda67ddacb5d3f5de697a3616699c192
Scott <scott.t.mcmichael@nasa.gov>
2016-01-22 15:58:24 -0800

    Added camera_calibrate test

---
+++ ss_camera_calibrate/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#set -x verbose
+#rm -rfv run
+
+camera_calibrate run 6 9 "../data/chessboard_pics/*" --overwrite
+
+
---
+++ ss_camera_calibrate/validate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in run/ocv_cam_params.yml run/solve_cam_params.txt run/vw_cam_params.tsai; do 
+
+  echo $file $gold
+  gold=${file/run\/run/gold\/run}
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+
+  diff=$(diff $file $gold)
+
+  echo diff is $diff
+  if [ "$diff" != "" ]; then
+      echo Validation failed
+      exit 1
+  fi
+
+done
+
+echo Validation succeded
+exit 0
