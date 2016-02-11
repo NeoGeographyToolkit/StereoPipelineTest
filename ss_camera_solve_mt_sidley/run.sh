49478ace419f73726340893617b84a01e89cf58c
Scott <scott.t.mcmichael@nasa.gov>
2016-02-10 20:52:28 -0800

    Updated camera_solve test for new pinhole files

--- ss_camera_solve_mt_sidley/run.sh
+++ ss_camera_solve_mt_sidley/run.sh
@@ -5,6 +5,6 @@ rm -rfv run
 
 camera_solve run/ ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif --calib-file camera_info.txt  --gcp-file ground_control_points.gcp
 
-stereo ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif run/mt_sidley_47.tif.pinhole run/mt_sidley_48.tif.pinhole run/out --left-image-crop-win 1891 6034 690 345  --right-image-crop-win 4750 5889 708 369
+stereo ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif run/mt_sidley_47.tif.tsai run/mt_sidley_48.tif.tsai run/out --left-image-crop-win 1891 6034 690 345  --right-image-crop-win 4750 5889 708 369
 
 point2dem run/out-PC.tif
