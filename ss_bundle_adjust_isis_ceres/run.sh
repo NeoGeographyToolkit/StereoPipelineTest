4a34b1b8f4990a7732a3166a1da4370eda0c3f25
Scott <scott.t.mcmichael@nasa.gov>
2016-02-25 11:13:37 -0800

    Retain bundle_adjust results for test.

--- ss_bundle_adjust_isis_ceres/run.sh
+++ ss_bundle_adjust_isis_ceres/run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 set -x verbose
-rm -rfv ba
 rm -rfv run
 
-bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --bundle-adjuster ceres --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o ba/run --datum D_MOON
+mkdir run
+bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --bundle-adjuster ceres --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/out --datum D_MOON
 
-stereo --bundle-adjust-prefix ba/run ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub run/run
+stereo --bundle-adjust-prefix run/ba/out ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub run/run
 
 point2dem run/run-PC.tif --errorimage
-
-
