3b7520c0b6578d227e97f5057681eacfc187e641
Scott <scott.t.mcmichael@nasa.gov>
2016-04-26 16:48:38 -0700

    Convert --tr to --mpp

--- ss_rpc_mapproject_cambox_bug/run.sh
+++ ss_rpc_mapproject_cambox_bug/run.sh
@@ -8,5 +8,5 @@ rm -rfv $dir
 # Bug based on Basalt Hills data. The cam box was 5x larger than necessary.
 
 opt='+proj=utm +zone=10 +datum=WGS84 +units=m +no_defs '
-mapproject --tr 2 ../data/filled_dem.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001.xml -t rpc  --t_srs "$opt" run/run.tif
+mapproject --mpp 2 ../data/filled_dem.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001.xml -t rpc  --t_srs "$opt" run/run.tif
 
--- ss_rpc_mapproject_cambox_bug2/run.sh
+++ ss_rpc_mapproject_cambox_bug2/run.sh
@@ -8,7 +8,7 @@ rm -rfv $dir
 # Bug based on Basalt Hills data. The cam box was 5x larger than necessary.
 
 opt='+proj=utm +zone=10 +datum=WGS84 +units=m +no_defs '
-mapproject --tr 13 ../data/zone10-CA_SanLuisResevoir-9m.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001.xml run/run.tif -t rpc --t_srs "$opt"  --tile-size 200
+mapproject --mpp 13 ../data/zone10-CA_SanLuisResevoir-9m.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001.xml run/run.tif -t rpc --t_srs "$opt"  --tile-size 200
 
 
 
