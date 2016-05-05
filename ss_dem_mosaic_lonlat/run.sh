33e8394f2d14c218793103f769f912b152758cb5
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2016-05-04 19:26:25 -0700

    Adjust dem_mosaic test

--- ss_dem_mosaic_lonlat/run.sh
+++ ss_dem_mosaic_lonlat/run.sh
@@ -6,5 +6,5 @@ rm -rfv run
 echo ../data/dem1_10pct.tif > list.txt
 echo ../data/dem2_10pct.tif >> list.txt
 
-dem_mosaic -l list.txt -o run/run --extra-crop-length 200 --erode-length 1  --t_projwin 125 -23 131 -14 --tr 0.01  --hole-fill-len 200
+dem_mosaic -l list.txt -o run/run --extra-crop-length 200 --erode-length 1  --t_projwin 123.62 -14.26 133.03 -24.18 --tr 0.01  --hole-fill-len 200
 
