1eba3b72e9a4f9d6840232fd2573866d21763120
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2016-05-05 18:14:47 -0700

    Add new ss_dg_mosaic_180bug test

---
+++ ss_dg_mosaic_180bug/validate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for g in gold/run.r100.tif gold/run.r100.xml; do
+  
+  if [ ! -e "$g" ]; then
+      echo "ERROR: File $g does not exist."
+      exit 1
+  fi
+
+  f=${g/gold/run}
+  if [ ! -e "$f" ]; then
+      echo "ERROR: File $f does not exist."
+      exit 1
+  fi
+
+  diff=$(cmp $f $g)
+  echo diff is $diff
+ 
+  if [ "$diff" != "" ]; then
+    echo Validation failed
+    exit 1
+  fi
+
+done
+
+echo Validation succeded
+exit 0
+
--- ss_dg_mosaic_var_pitch/validate.sh
+++ ss_dg_mosaic_var_pitch/validate.sh
@@ -42,8 +42,8 @@ rm -fv "$file.aux.xml"
 rm -fv "$gold.aux.xml"
 
 cmp_stats.sh $file $gold
-gdalinfo -stats $file | grep -v Files | grep -v -i tif > run.txt
-gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold.txt
+gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v xml > run.txt
+gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v xml > gold.txt
 
 diff=$(diff run.txt gold.txt)
 cat run.txt
