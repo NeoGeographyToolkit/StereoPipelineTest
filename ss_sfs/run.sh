07bc405dd6f95f8f46169a685203fc711735821a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2016-06-09 09:44:10 -0700

    Update sfs testcase

--- ss_sfs/run.sh
+++ ss_sfs/run.sh
@@ -3,5 +3,6 @@
 set -x verbose
 rm -rfv run
 
-sfs -i ../data/sfs-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --use-approx-camera-models 
+sfs -i ../data/sfs-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --use-approx-camera-models  --crop-input-images --threads 8
+
 
