7fbd019212e8d12555f3d65ed3016e32cf0c8d00
Scott <scott.t.mcmichael@nasa.gov>
2016-11-23 13:13:10 -0800

    Replaced binary pinhole models

--- ssPinHole_alignNone_seedMode1_mapProj1_subPix1/run.sh
+++ ssPinHole_alignNone_seedMode1_mapProj1_subPix1/run.sh
@@ -3,8 +3,8 @@
 set -x verbose
 rm -rfv run
 
-stereo -t pinholemappinhole ../data/DZB1212-500082L002001_bb_mapproj_1e4.tif ../data/DZB1212-500082L003001_bb_mapproj_1e4.tif ../data/DZB1212-500082L002001_bb_50pct_2x.pinhole ../data/DZB1212-500082L003001_bb_50pct_2x.pinhole run/run ../data/good_dem.tif -s stereo.default --corr-seed-mode 1 --subpixel-mode 1 --left-image-crop-win 3701 751 1165 988 --right-image-crop-win 3672 810 1091 900
+stereo -t pinholemappinhole ../data/pinhole_proj_47.tif ../data/pinhole_proj_48.tif ../data/pinhole_proj_47.tif.tsai ../data/pinhole_proj_48.tif.tsai   run/run ../data/krigged_dem_nsidc_ndv0_fill.tif -s stereo.default --corr-seed-mode 1 --subpixel-mode 1
 
-point2dem --nodata-value -32767 --semi-major-axis 1 --semi-minor-axis 1 run/run-PC.tif
+point2dem --nodata-value -32767 run/run-PC.tif
 
 
