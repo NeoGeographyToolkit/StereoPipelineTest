cec3b66a99d6e81ffe41b2f4788490c8b628d258
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2016-12-07 16:26:20 -0800

    test --overlap-list for bundle adjustment

--- ss_bundle_adjust_null/run.sh
+++ ss_bundle_adjust_null/run.sh
@@ -3,4 +3,6 @@
 set -x verbose
 rm -rfv run
 
-bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.null.tsai ../data/right_sub16.null.tsai ../data/gcp_sub16.gcp --local-pinhole -t pinhole --datum WGS84 -o run/run --intrinsics-to-float "focal_length distortion_params"
+echo ../data/left_sub16.tif ../data/right_sub16.tif > list.txt
+bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.null.tsai ../data/right_sub16.null.tsai ../data/gcp_sub16.gcp --local-pinhole -t pinhole --datum WGS84 -o run/run --intrinsics-to-float "focal_length distortion_params" --overlap-list list.txt
+
