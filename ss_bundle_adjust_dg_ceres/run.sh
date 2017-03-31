5166e6f2b647cfa703c13279166c6002dc2bbcf7
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2017-03-31 16:40:22 -0700

    Test for initial transform

--- ss_bundle_adjust_dg_ceres/run.sh
+++ ss_bundle_adjust_dg_ceres/run.sh
@@ -3,6 +3,12 @@
 set -x verbose
 rm -rfv run
 
-bundle_adjust ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.xml ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.xml -o run/run --ip-detect-method 1
+echo "1 0 0 2" > transform.txt
+echo "0 1 0 0" >> transform.txt
+echo "0 0 1 0" >> transform.txt
+echo "0 0 0 0" >> transform.txt
+
+bundle_adjust ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.xml ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.xml -o run/run --ip-detect-method 1 --initial-transform transform.txt
+
 
 
--- ss_bundle_adjust_tsai/run.sh
+++ ss_bundle_adjust_tsai/run.sh
@@ -3,5 +3,10 @@
 set -x verbose
 rm -rfv run
 
-bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.v2.tsai ../data/right_sub16.v2.tsai ../data/gcp_sub16.gcp --local-pinhole -t pinhole --datum WGS84 -o run/run --intrinsics-to-float "distortion_params"
+echo "1 0 0 2" > transform.txt
+echo "0 1 0 0" >> transform.txt
+echo "0 0 1 0" >> transform.txt
+echo "0 0 0 0" >> transform.txt
+
+bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.v2.tsai ../data/right_sub16.v2.tsai ../data/gcp_sub16.gcp --local-pinhole -t pinhole --datum WGS84 -o run/run --intrinsics-to-float "distortion_params" --initial-transform transform.txt
 
