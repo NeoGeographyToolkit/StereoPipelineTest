2952f26f9f617cb1b46bcf9eeca29ff37d4dcbeb
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2017-06-06 16:58:19 -0700

    Shorten one test

--- ss_bundle_adjust_isis_ceres/run.sh
+++ ss_bundle_adjust_isis_ceres/run.sh
@@ -6,6 +6,6 @@ rm -rfv run
 mkdir run
 bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --bundle-adjuster ceres --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/out --datum D_MOON
 
-stereo --bundle-adjust-prefix run/ba/out ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub run/run
+stereo --bundle-adjust-prefix run/ba/out ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub run/run --left-image-crop-win 601 1348 441 407 --right-image-crop-win 1751 1393 516 441
 
 point2dem run/run-PC.tif --errorimage
