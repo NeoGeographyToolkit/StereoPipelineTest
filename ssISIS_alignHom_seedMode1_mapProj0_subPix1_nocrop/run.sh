4eb15c764e9943f01a15fbff4886f77b404c9ccc
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2017-06-29 10:25:40 -0700

    Tweak some tests for point2dem

--- ssISIS_alignHom_seedMode1_mapProj0_subPix1_nocrop/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1_nocrop/run.sh
@@ -4,6 +4,8 @@ set -x verbose
 rm -rfv run
 
 stereo --save-double-precision-point-cloud ../data/M0100115_small.cub ../data/E0201461_small.cub run/run -s stereo.default --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1
+
+# Test the case when semi-major and semi-minor axes are different
 point2dem run/run-PC.tif --nodata-value -32767 --max-valid-triangulation-error 2 --semi-major-axis 3396180 --semi-minor-axis 3396170
 
 
