1e55412ee9d36f3e9546a92c91f8c3ddd282478f
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2018-02-14 13:12:00 -0800

    Add --mo to image_calc

--- ss_image_calc/run.sh
+++ ss_image_calc/run.sh
@@ -4,5 +4,6 @@ set -x verbose
 rm -rfv run
 
 mkdir run
-image_calc -o run/output.tif -d float64 -c "pow(var_0/3.0, 1.1)"  ../data/dem1_10pct.tif
+image_calc -o run/output.tif -d float64 -c "pow(var_0/3.0, 1.1)"  ../data/dem1_10pct.tif --mo 'tmp1=val1 tmp2=val2'
+
 
