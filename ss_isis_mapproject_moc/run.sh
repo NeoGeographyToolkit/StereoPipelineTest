645e1662b84baa67c672e76c19a78af7dbcaba3b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2018-02-13 19:39:02 -0800

    Add test for --mo

--- ss_isis_mapproject_moc/run.sh
+++ ss_isis_mapproject_moc/run.sh
@@ -3,5 +3,4 @@
 set -x verbose
 rm -rfv run
 
-mapproject --mpp 40 ../data/ref-ortho-moc-DEM.tif ../data/M0100115.cub run/run-ortho.tif  --bundle-adjust-prefix ../data/run_ba/run
-
+mapproject --mpp 40 ../data/ref-ortho-moc-DEM.tif ../data/M0100115.cub run/run-ortho.tif --bundle-adjust-prefix ../data/run_ba/run --mo 'var1=val1 var2=val2' --tile-size 100
