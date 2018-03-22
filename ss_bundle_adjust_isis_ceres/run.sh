ac8bd058f0b6601e39b0bb914682ec7260b848fe
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2018-03-22 09:20:23 -0700

    test for --input-adjustments-prefix

--- ss_bundle_adjust_isis_ceres/run.sh
+++ ss_bundle_adjust_isis_ceres/run.sh
@@ -4,7 +4,10 @@ set -x verbose
 rm -rfv run
 
 mkdir run
-bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --bundle-adjuster ceres --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/out --datum D_MOON
+bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --bundle-adjuster ceres --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/in --datum D_MOON
+
+# Testing reading adjustments
+bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --bundle-adjuster ceres --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/out --datum D_MOON --input-adjustments-prefix run/ba/in
 
 stereo --bundle-adjust-prefix run/ba/out ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub run/run --left-image-crop-win 601 1348 441 407 --right-image-crop-win 1751 1393 516 441
 
