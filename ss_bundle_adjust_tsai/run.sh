7c32bdd5a47d8bbc4a31cd0cd8c625ca19be2c5f
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2018-03-29 09:05:22 -0700

    Update tests to reflect the renamed option

--- ss_bundle_adjust_tsai/run.sh
+++ ss_bundle_adjust_tsai/run.sh
@@ -8,5 +8,5 @@ echo "0 1 0 0" >> transform.txt
 echo "0 0 1 0" >> transform.txt
 echo "0 0 0 0" >> transform.txt
 
-bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.v2.tsai ../data/right_sub16.v2.tsai ../data/gcp_sub16.gcp --local-pinhole -t pinhole --datum WGS84 -o run/run --intrinsics-to-float "distortion_params" --initial-transform transform.txt
+bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.v2.tsai ../data/right_sub16.v2.tsai ../data/gcp_sub16.gcp --create-pinhole-cameras -t pinhole --datum WGS84 -o run/run --intrinsics-to-float "distortion_params" --initial-transform transform.txt
 
