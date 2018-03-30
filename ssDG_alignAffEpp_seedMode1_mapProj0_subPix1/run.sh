5d90f21324bba16f4233f76d348bc3a9fa756346
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2018-03-30 13:50:17 -0700

    Minor tweak

--- ssDG_alignAffEpp_seedMode1_mapProj0_subPix1/run.sh
+++ ssDG_alignAffEpp_seedMode1_mapProj0_subPix1/run.sh
@@ -5,4 +5,6 @@ rm -rfv run
 
 stereo --corr-timeout 6000 ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.xml ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.xml run/run --disable-fill-holes --stereo-file stereo.default --left-image-crop-win 2048 0 1024 1024 -t dg --alignment-method affineepipolar --corr-seed-mode 1 --subpixel-mode 1
 
-point2dem -r Earth run/run-PC.tif --nodata-value -32767
+point2dem -r Earth run/run-PC.tif --orthoimage run/run-L.tif --nodata-value -32767 --orthoimage-hole-fill-len 100 --max-valid-triangulation-error 1000 --orthoimage-hole-fill-extra-len 20
+
+
