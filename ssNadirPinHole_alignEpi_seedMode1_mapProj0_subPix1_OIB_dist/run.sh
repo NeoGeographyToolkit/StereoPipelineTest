f90de79e12b00f1c1c1829aeea7b23cc5aaac5ee
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2018-08-08 11:36:02 -0700

    bundle_adjust: test to apply the initial transform on top of initial adjustments

--- ssNadirPinHole_alignEpi_seedMode1_mapProj0_subPix1_OIB_dist/run.sh
+++ ssNadirPinHole_alignEpi_seedMode1_mapProj0_subPix1_OIB_dist/run.sh
@@ -7,7 +7,3 @@ stereo --alignment-method epipolar --xcorr-threshold -1 --corr-kernel 5 5 --corr
 
 point2dem  --t_srs '+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs' --tr 0.4 run/run-PC.tif
 
-
-
-
-
