f435ef0484f64a7b60ec41455d8837de0bc3cc3e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2018-08-08 17:00:25 -0700

    Add tests with bundle adjust with scale correction

--- ss_bundle_adjust_isis_ceres/run.sh
+++ ss_bundle_adjust_isis_ceres/run.sh
@@ -6,10 +6,10 @@ rm -rfv run
 mkdir run
 bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --bundle-adjuster ceres --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/in --datum D_MOON
 
-echo "1 0 0 0.2" > transform.txt
-echo "0 1 0 0" >> transform.txt
-echo "0 0 1 0" >> transform.txt
-echo "0 0 0 0" >> transform.txt
+echo "1.005  0       0     200"  > transform.txt
+echo "0      1.005   0     300" >> transform.txt
+echo "0      0       1.005 400" >> transform.txt
+echo "0      0       0       0" >> transform.txt
 
 # Testing reading adjustments and applying a transform on top
 bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --bundle-adjuster ceres --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/out --datum D_MOON --input-adjustments-prefix run/ba/in --initial-transform transform.txt
--- ss_bundle_adjust_tsai/run.sh
+++ ss_bundle_adjust_tsai/run.sh
@@ -3,10 +3,15 @@
 set -x verbose
 rm -rfv run
 
-echo "1 0 0 2" > transform.txt
-echo "0 1 0 0" >> transform.txt
-echo "0 0 1 0" >> transform.txt
-echo "0 0 0 0" >> transform.txt
+echo "1.2  0    0    2000"  > transform.txt
+echo "0    1.2  0    3000" >> transform.txt
+echo "0    0    1.2  4000" >> transform.txt
+echo "0    0    0       1" >> transform.txt
 
-bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.v2.tsai ../data/right_sub16.v2.tsai ../data/gcp_sub16.gcp --create-pinhole-cameras -t pinhole --datum WGS84 -o run/run --intrinsics-to-float "distortion_params" --initial-transform transform.txt
+# First do bundle adjustment
+bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.v2.tsai ../data/right_sub16.v2.tsai ../data/gcp_sub16.gcp --create-pinhole-cameras -t pinhole --datum WGS84 -o run/ba1/run --intrinsics-to-float "distortion_params" 
+
+# Then just apply a transform
+bundle_adjust ../data/left_sub16.tif ../data/right_sub16.tif  run/ba1/run-left_sub16.v2.tsai run/ba1/run-right_sub16.v2.tsai  --create-pinhole-cameras -t pinhole --datum WGS84 -o run/run  --initial-transform transform.txt --max-iterations 0
+
 
