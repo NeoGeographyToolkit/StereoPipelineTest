00a08956e9506c1d02d9d7036ff0fd0fd8d153b5
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2018-08-22 13:23:59 -0700

    Add testcase for bundle adjustment with outlier filtering

---
+++ ss_bundle_adjust_filter_outliers/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+bundle_adjust ../data/DMS_20171029_183704_02500.tif ../data/DMS_20171029_183706_02501.tif ../data/DMS_20171029_183707_02502.tif ../data/DMS_20171029_183704_02500.tsai ../data/DMS_20171029_183706_02501.tsai ../data/DMS_20171029_183707_02502.tsai -o run/run --ip-per-tile 2000 --num-passes 2 --remove-outliers-params "75 3 1 2"
+
---
+++ ss_bundle_adjust_filter_outliers/validate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in run/run-DMS_20171029_183704_02500.adjust run/run-DMS_20171029_183706_02501.adjust run/run-DMS_20171029_183707_02502.adjust; do
+  gold=${file/run\/run/gold\/run}
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+  echo diff $file $gold
+  diff=$(diff $file $gold)
+
+  echo diff is $diff
+  if [ "$diff" != "" ]; then
+      echo Validation failed
+      exit 1
+  fi
+
+done
+
+echo Validation succeded
+exit 0
