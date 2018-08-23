0be984f7598547413ae660ecfa1b3068f1bbf73b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2018-08-22 17:41:45 -0700

    Update two pc_align tests

--- ss_pc_align_manual_trans/run.sh
+++ ss_pc_align_manual_trans/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-pc_align ../data/pluto-dem1.tif ../data/pluto-dem2.tif --match-file ../data/pluto1-pluto2.match --max-displacement -1
+pc_align ../data/pluto-dem1.tif ../data/pluto-dem2.tif --match-file ../data/pluto1-pluto2.match --max-displacement -1 --num-iterations 0
 
 
 
