b859418edd0f001a0d2dd73b69d8dd2fe759daf1
Scott <scott.t.mcmichael@nasa.gov>
2018-11-08 21:52:19 -0800

    Update tests for new SGM subpixel handling

--- ssNadirPinHole_alignEpi_seedMode1_mapProj0_subPix1_OIB_dist/run.sh
+++ ssNadirPinHole_alignEpi_seedMode1_mapProj0_subPix1_OIB_dist/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo --alignment-method epipolar --xcorr-threshold -1 --corr-kernel 5 5 --corr-tile-size 6400 --cost-mode 4 --subpixel-mode 0 --stereo-algorithm 2 --threads 16 --corr-seed-mode 1 --session-type nadirpinhole ../data/img_icebridge2.tif ../data/img_icebridge3.tif ../data/img_icebridge2.tsai ../data/img_icebridge3.tsai run/run 
+stereo --alignment-method epipolar --xcorr-threshold -1 --corr-kernel 5 5 --corr-tile-size 6400 --cost-mode 4  --stereo-algorithm 2 --threads 16 --corr-seed-mode 1 --session-type nadirpinhole ../data/img_icebridge2.tif ../data/img_icebridge3.tif ../data/img_icebridge2.tsai ../data/img_icebridge3.tsai run/run 
 
 point2dem  --t_srs '+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs' --tr 0.4 run/run-PC.tif
 
