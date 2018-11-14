87f1bafad841c93b50f33835d151940af7973c80
Scott <scott.t.mcmichael@nasa.gov>
2018-11-14 11:42:46 -0800

    Update to match bundle_adjust interface change

--- ss_bundle_adjust_isis_ceres/run.sh
+++ ss_bundle_adjust_isis_ceres/run.sh
@@ -12,7 +12,7 @@ echo "0      0       1.005 400" >> transform.txt
 echo "0      0       0       0" >> transform.txt
 
 # Testing reading adjustments and applying a transform on top
-bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --bundle-adjuster ceres --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/out --datum D_MOON --input-adjustments-prefix run/ba/in --initial-transform transform.txt
+bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/out --datum D_MOON --input-adjustments-prefix run/ba/in --initial-transform transform.txt
 
 stereo --bundle-adjust-prefix run/ba/out ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub run/run --left-image-crop-win 601 1348 441 407 --right-image-crop-win 1751 1393 516 441
 
