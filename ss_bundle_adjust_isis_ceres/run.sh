1323df1834715d34ce555fb669db1b908986c672
Scott <scott.t.mcmichael@nasa.gov>
2018-11-15 10:04:33 -0800

    Update test for ba change

--- ss_bundle_adjust_isis_ceres/run.sh
+++ ss_bundle_adjust_isis_ceres/run.sh
@@ -4,7 +4,7 @@ set -x verbose
 rm -rfv run
 
 mkdir run
-bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --bundle-adjuster ceres --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/in --datum D_MOON
+bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/in --datum D_MOON
 
 echo "1.005  0       0     200"  > transform.txt
 echo "0      1.005   0     300" >> transform.txt
