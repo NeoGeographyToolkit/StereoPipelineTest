f25b78a180770f6824b837f28ab9eaf1d55d4a82
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2018-12-24 16:48:30 -0800

    Update the ss_cam_gen test

--- ss_cam_gen/run.sh
+++ ss_cam_gen/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-cam_gen --datum WGS84 --height-above-datum 0 --lon-lat-corners '-122.38 37.62 -122.35 37.62 -122.35 37.61 -122.39 37.61' --focal-length 28.968757624607505 --optical-center 18.11581494334796 12.0750534040106 --pixel-pitch 0.0063 --refine-camera ../data/DMS_20171029_183706_02501.tif -o run/run.tsai --gcp-file run/run.gcp
+cam_gen --datum WGS84 --height-above-datum 0 --lon-lat-values '-122.38 37.62 -122.35 37.62 -122.35 37.61 -122.39 37.61' --focal-length 28.968757624607505 --optical-center 18.11581494334796 12.0750534040106 --pixel-pitch 0.0063 --refine-camera ../data/DMS_20171029_183706_02501.tif -o run/run.tsai --gcp-file run/run.gcp
 
