4c736846fdc2d91dc64cd59d7d1479807645fafe
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2019-03-05 19:36:17 -0800

    Minor update to ss_convert_pinhole_model/run.sh

--- ss_convert_pinhole_model/run.sh
+++ ss_convert_pinhole_model/run.sh
@@ -4,6 +4,6 @@ set -x verbose
 rm -rfv run
 mkdir -p run
 
-convert_pinhole_model ../data/DMS_20171029_183704_02500.tif ../data/DMS_20171029_183704_02500.tsai --output-type RPC --sample-spacing 50 -o run/DMS_20171029_183704_02500_RPC.tsai
+convert_pinhole_model ../data/DMS_20171029_183704_02500.tif ../data/DMS_20171029_183704_02500.tsai --output-type RPC  -o run/DMS_20171029_183704_02500_RPC.tsai
 
 
