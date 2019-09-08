453c23cfec54d7c138a789680a6bf5aded570b33
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2019-09-08 08:59:45 -0700

    Fix camera_solve test for robustness

--- ss_camera_solve_mt_sidley/run.sh
+++ ss_camera_solve_mt_sidley/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-camera_solve run/ ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif --calib-file camera_info.tsai  --gcp-file ground_control_points.gcp
+camera_solve run/ ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif --calib-file camera_info.tsai  --gcp-file ground_control_points.gcp --theia-retries 10
 
 stereo ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif run/mt_sidley_47.tif.final.tsai run/mt_sidley_48.tif.final.tsai run/out --left-image-crop-win 1891 6034 690 345  --right-image-crop-win 4750 5889 708 369
 
