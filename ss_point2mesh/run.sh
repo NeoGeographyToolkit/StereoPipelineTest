857a332448917ef2b45e5bfefa36933fabf3c8fb
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-07-06 18:06:32 -0700

    Fixes for fgr and point2mesh

--- ss_point2mesh/run.sh
+++ ss_point2mesh/run.sh
@@ -3,6 +3,4 @@
 set -x verbose
 rm -rfv run
 
-point2mesh ../data/run-PC.tif ../data/run-L.tif -l --output-prefix run/run
-
-
+point2mesh ../data/run-PC.tif ../data/run-L.tif --output-prefix run/run
--- ss_point2mesh/validate.sh
+++ ss_point2mesh/validate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-file=run/run.osgb
-gold=gold/run.osgb
+file=run/run.obj
+gold=gold/run.obj
 
 if [ ! -e "$file" ]; then
     echo "ERROR: File $file does not exist."
@@ -14,29 +14,12 @@ if [ ! -e "$gold" ]; then
     exit 1;
 fi
 
-#diff=$(cmp $file $gold 2>&1)
+diff=$(cmp $file $gold 2>&1)
 
-#echo diff is $diff
-#if [ "$diff" != "" ]; then
-#    echo Validation failed
-#    exit 1
-#fi
-
-# To dislodge stuck osgviewer
-(sleep 5; killall osgviewer > /dev/null 2>&1; exit 0) &
-
-osgviewer run/run.osgb
-status=$?
-
-if [ "$status" = "143" ]; then
-   # Declare the test passed if osgviewer started well, but
-   # failed to quit and had to be killed
-   status=0
-fi
-
-if [ "$status" != "0" ]; then
-  echo Validation failed
-  exit 1
+echo diff is $diff
+if [ "$diff" != "" ]; then
+    echo Validation failed
+    exit 1
 fi
 
 echo Validation succeded
