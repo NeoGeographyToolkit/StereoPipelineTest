3849a53462419721bbd7b7d1fd5202abf82ceda0
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-07-11 16:16:36 -0700

    Tweak fgr test

--- ss_pc_align_fgr/run.sh
+++ ss_pc_align_fgr/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-pc_align ../data/rock_dem_50pct.tif ../data/lat_lon_z.csv --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --max-num-reference-points 1000 --max-num-source-points 1000  --max-displacement 100  --initial-ned-translation '-1 -1 -2' --alignment-method fgr
+pc_align ../data/rock_dem_50pct.tif ../data/lat_lon_z.csv --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --max-num-reference-points 50000 --max-num-source-points 3000  --max-displacement 50  --initial-ned-translation '-1 -1 -2' --alignment-method fgr
 
