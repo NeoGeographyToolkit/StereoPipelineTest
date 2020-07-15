22e27ff3aaa1428cad4f2ea3c26df6726112e299
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-07-15 09:39:50 -0700

    Robustify more

--- ss_camera_solve_mt_sidley/run.sh
+++ ss_camera_solve_mt_sidley/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 set -x verbose
-rm -rfv run
 
 # Not sure why sometimes it fails. This is a temporary hack, just try again.
 for ((i=0; i<=10;i++)); do 
+
+  rm -rfv run
 
   camera_solve run/ ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif --calib-file camera_info.tsai  --gcp-file ground_control_points.gcp --theia-retries 10
 
@@ -15,6 +16,6 @@ for ((i=0; i<=10;i++)); do
   if [ -f run/out-DEM.tif ]; then 
 	  break
   fi
-
+  sleep 60
 done
 
