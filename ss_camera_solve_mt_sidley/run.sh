11e5794e43d5b3dc03152fc9311eef62eeb037aa
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-07-15 09:38:28 -0700

    Robustify ss_camera_solve_mt_sidley

--- ss_camera_solve_mt_sidley/run.sh
+++ ss_camera_solve_mt_sidley/run.sh
@@ -3,8 +3,18 @@
 set -x verbose
 rm -rfv run
 
-camera_solve run/ ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif --calib-file camera_info.tsai  --gcp-file ground_control_points.gcp --theia-retries 10
+# Not sure why sometimes it fails. This is a temporary hack, just try again.
+for ((i=0; i<=10;i++)); do 
 
-stereo ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif run/mt_sidley_47.tif.final.tsai run/mt_sidley_48.tif.final.tsai run/out --left-image-crop-win 1891 6034 690 345  --right-image-crop-win 4750 5889 708 369
+  camera_solve run/ ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif --calib-file camera_info.tsai  --gcp-file ground_control_points.gcp --theia-retries 10
 
-point2dem run/out-PC.tif
+  stereo ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif run/mt_sidley_47.tif.final.tsai run/mt_sidley_48.tif.final.tsai run/out --left-image-crop-win 1891 6034 690 345  --right-image-crop-win 4750 5889 708 369
+
+  point2dem run/out-PC.tif
+
+  if [ -f run/out-DEM.tif ]; then 
+	  break
+  fi
+
+done
+
