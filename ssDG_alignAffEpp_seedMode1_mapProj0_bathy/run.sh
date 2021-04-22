af7bde05056cbc1cc0e2123be8d83867b35818ed
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-04-21 21:45:33 -0700

    Test wv_correct as part of bathy stereo

--- ssDG_alignAffEpp_seedMode1_mapProj0_bathy/run.sh
+++ ssDG_alignAffEpp_seedMode1_mapProj0_bathy/run.sh
@@ -12,9 +12,13 @@ mkdir -p run
 image_calc -c "max($lthresh, var_0)" --output-nodata-value $lthresh ../data/left_bathy_b7.tif -o run/left_bathy_b7_mask.tif
 image_calc -c "max($rthresh, var_0)" --output-nodata-value $rthresh ../data/right_bathy_b7.tif -o run/right_bathy_b7_mask.tif
 
+# Apply wv_correct
+wv_correct --band 3 ../data/left_bathy_b3.tif ../data/left_bathy.xml run/left_bathy_b3_corr.tif
+wv_correct --band 3 ../data/right_bathy_b3.tif ../data/right_bathy.xml run/right_bathy_b3_corr.tif
+
 # First run with no bathymetry
-stereo ../data/left_bathy_b3.tif ../data/right_bathy_b3.tif \
-    ../data/left_bathy.xml ../data/right_bathy.xml          \
+stereo run/left_bathy_b3_corr.tif run/right_bathy_b3_corr.tif \
+    ../data/left_bathy.xml ../data/right_bathy.xml            \
     run/run-nobathy
 point2dem run/run-nobathy-PC.tif --orthoimage run/run-nobathy-L.tif 
 
@@ -30,12 +34,12 @@ bathy_plane_calc --shapefile ../data/bathy_shoreline.shp    \
 #    --right-image-crop-win -189 3895 1413 1258              \
 
 # Run with bathy
-stereo ../data/left_bathy_b3.tif ../data/right_bathy_b3.tif \
-    ../data/left_bathy.xml ../data/right_bathy.xml          \
-    --left-bathy-mask run/left_bathy_b7_mask.tif            \
-    --right-bathy-mask run/right_bathy_b7_mask.tif          \
-    --refraction-index $waterRefractionIndex                \
-    --bathy-plane run/bathy-plane.txt                       \
+stereo run/left_bathy_b3_corr.tif run/right_bathy_b3_corr.tif \
+    ../data/left_bathy.xml ../data/right_bathy.xml            \
+    --left-bathy-mask run/left_bathy_b7_mask.tif              \
+    --right-bathy-mask run/right_bathy_b7_mask.tif            \
+    --refraction-index $waterRefractionIndex                  \
+    --bathy-plane run/bathy-plane.txt                         \
     run/run
 
 point2dem run/run-PC.tif
