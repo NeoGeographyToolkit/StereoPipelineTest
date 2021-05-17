254f04c5e5ae6d086b5ddce5cbeee3df7530bf43
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-05-17 13:04:47 -0700

    Use parallel_stereo in a test and other minor tweaks

--- ssDG_alignAffEpp_seedMode1_mapProj0_subPix1/run.sh
+++ ssDG_alignAffEpp_seedMode1_mapProj0_subPix1/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo --corr-timeout 6000 ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.xml ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.xml run/run --disable-fill-holes --stereo-file stereo.default --left-image-crop-win 2048 0 1024 1024 -t dg --alignment-method affineepipolar --corr-seed-mode 1 --subpixel-mode 1  --ip-per-tile 5000
+stereo --corr-timeout 6000 ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.xml ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.xml run/run --disable-fill-holes --stereo-file stereo.default --left-image-crop-win 1232 227 2271 1235 --right-image-crop-win 1324 298 2041 1174  -t dg --alignment-method affineepipolar --corr-seed-mode 1 --subpixel-mode 1  --ip-per-tile 10000
 
 point2dem -r Earth run/run-PC.tif --orthoimage run/run-L.tif --nodata-value -32767 --orthoimage-hole-fill-len 100 --max-valid-triangulation-error 1000 --orthoimage-hole-fill-extra-len 20
 
--- ssISIS_alignAffEpp_seedMode2_mapProj0_SGM/run.sh
+++ ssISIS_alignAffEpp_seedMode2_mapProj0_SGM/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/M0100115_crop.cub ../data/E0201461.cub run/run -s stereo.default --disable-fill-holes --left-image-crop-win 0 1024 672 4864 --disparity-estimation-dem ../data/ref-DEM.tif --disparity-estimation-dem-error 5 --alignment-method affineepipolar --corr-seed-mode 2  --stereo-algorithm 2 --corr-tile-size 2000
+parallel_stereo ../data/M0100115_crop.cub ../data/E0201461.cub run/run -s stereo.default --disable-fill-holes --left-image-crop-win 0 1024 672 4864 --disparity-estimation-dem ../data/ref-DEM.tif --disparity-estimation-dem-error 5 --alignment-method affineepipolar --corr-seed-mode 2  --stereo-algorithm 2 --corr-tile-size 2000
 point2dem -r mars run/run-PC.tif --nodata-value -32767
 
 
