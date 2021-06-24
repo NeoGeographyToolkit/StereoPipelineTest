1e65c9212324165278ecaa5b8c533896ad226446
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-06-24 09:43:33 -0700

    Add local epipolar alignment test with libelas external algorithm

---
+++ ssRPC_alignLocalEpi_Libelas/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+parallel_stereo --stereo-algorithm libelas --corr-tile-size 650 --sgm-collar-size 200 --alignment-method local_epipolar ../data/img_01_crop.tif ../data/img_02.tif run/run
+
+point2dem Earth run/run-PC.tif
+
