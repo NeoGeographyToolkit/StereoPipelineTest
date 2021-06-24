b00838d6ce1a972466fd5fc210e13c2ace45d494
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-06-24 12:44:06 -0700

    Minor tweak of libelas test

--- ssRPC_alignLocalEpi_Libelas/run.sh
+++ ssRPC_alignLocalEpi_Libelas/run.sh
@@ -5,5 +5,5 @@ rm -rfv run
 
 parallel_stereo --stereo-algorithm libelas --corr-tile-size 650 --sgm-collar-size 200 --alignment-method local_epipolar ../data/img_01_crop.tif ../data/img_02.tif run/run
 
-point2dem Earth run/run-PC.tif
+point2dem run/run-PC.tif
 
