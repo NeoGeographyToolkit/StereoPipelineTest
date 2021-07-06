2d61482ce26a9468339395d103cefb16b0e430fb
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-07-06 12:14:46 -0700

    ss_bundle_adjust_csm: Use one thread for bundle adjustment for reproducibility

--- ss_bundle_adjust_csm/run.sh
+++ ss_bundle_adjust_csm/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-bundle_adjust ../data/B17_016219_1978_XN_17N282W.8bit.cub ../data/B18_016575_1978_XN_17N282W.8bit.cub ../data/B17_016219_1978_XN_17N282W.8bit.json ../data/B18_016575_1978_XN_17N282W.8bit.json -o run/run
+bundle_adjust --threads 1 ../data/B17_016219_1978_XN_17N282W.8bit.cub ../data/B18_016575_1978_XN_17N282W.8bit.cub ../data/B17_016219_1978_XN_17N282W.8bit.json ../data/B18_016575_1978_XN_17N282W.8bit.json -o run/run
 
