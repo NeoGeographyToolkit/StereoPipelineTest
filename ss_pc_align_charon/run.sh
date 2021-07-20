729570e86bdb516f44adf22ead3bbd89fc59a30d
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-07-20 16:07:34 -0700

    Wipe charon testcase, the DEM seems invalid, recent GDAL cannot injest it

--- ss_pc_align_charon/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x verbose
-rm -rfv run
-
-pc_align --max-displacement 20000 ../data/cpdem1.cub ../data/cpdem2.cub -o run/run --save-transformed-source-points --save-inv-transformed-reference-points
-
-
