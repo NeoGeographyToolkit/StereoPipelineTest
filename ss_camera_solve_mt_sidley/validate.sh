2d64855472e12264608939e914bfa925df115f17
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-07-23 11:28:48 -0700

    Minor validation adjustments for ss_camera_solve_mt_sidley

--- ss_camera_solve_mt_sidley/validate.sh
+++ ss_camera_solve_mt_sidley/validate.sh
@@ -3,7 +3,6 @@ export PATH=../bin:$PATH
 
 for file in run/out-DEM.tif run/out-DEM.tif; do
 
-  echo $file $gold
   gold=${file/run\/run/gold\/run}
 
   if [ ! -e "$file" ]; then
@@ -21,8 +20,8 @@ for file in run/out-DEM.tif run/out-DEM.tif; do
   rm -fv "$gold.aux.xml"
 
   cmp_stats.sh $file $gold
-  gdalinfo -stats $file | grep -v Files | grep -v -i tif > run.txt
-  gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold.txt
+  gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v Min= > run.txt
+  gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v Min= > gold.txt
 
   diff=$(diff run.txt gold.txt)
   cat run.txt
