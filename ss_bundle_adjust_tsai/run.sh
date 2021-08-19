72b59532270e4d706f76b7814ef6d97b2fd9b631
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-08-18 17:05:17 -0700

    Add test for bundle_adjust with --apply-initial-transform-only

--- ss_bundle_adjust_tsai/run.sh
+++ ss_bundle_adjust_tsai/run.sh
@@ -12,6 +12,6 @@ echo "0    0    0       1" >> transform.txt
 bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.v2.tsai ../data/right_sub16.v2.tsai ../data/gcp_sub16.gcp --inline-adjustments -t pinhole --datum WGS84 -o run/ba1/run --intrinsics-to-float "other_intrinsics" --threads 1
 
 # Then just apply a transform
-bundle_adjust ../data/left_sub16.tif ../data/right_sub16.tif  run/ba1/run-left_sub16.v2.tsai run/ba1/run-right_sub16.v2.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run --initial-transform transform.txt --max-iterations 0 --threads 1
+bundle_adjust ../data/left_sub16.tif ../data/right_sub16.tif  run/ba1/run-left_sub16.v2.tsai run/ba1/run-right_sub16.v2.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run --initial-transform transform.txt --apply-initial-transform-only --threads 1
 
 
