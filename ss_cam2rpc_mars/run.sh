8da58a19f7a2e22c619b231ebd472255bfec9c58
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-09-24 14:52:36 -0700

    expand the cam2rpc test to run stereo with the obtained cameras

--- ss_cam2rpc_mars/run.sh
+++ ss_cam2rpc_mars/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Run stereo with cam2rpc-ed cameras. Those set the datum which stereo later uses.
+
 set -x verbose
 rm -rfv run
 
-cam2rpc ../data/M0100115.cub run/run.xml --session-type isis --datum D_MARS --save-tif-image --height-range -10020.707 -9925.348 --lon-lat-range 141.5447632 34.3363034 141.5701589 34.3063306 --num-samples 10 --penalty-weight 0.03 --gsd 1
+cam2rpc ../data/M0100115.cub run/M0100115.xml --session-type isis --datum D_MARS --save-tif-image --height-range -10020.707 -9925.348 --lon-lat-range 141.5447632 34.3363034 141.5701589 34.3063306 --num-samples 10 --penalty-weight 0.03 --gsd 1
+cam2rpc ../data/E0201461.cub run/E0201461.xml --session-type isis --datum D_MARS --save-tif-image --height-range -10020.707 -9925.348 --lon-lat-range 141.5447632 34.3363034 141.5701589 34.3063306 --num-samples 10 --penalty-weight 0.03 --gsd 1
+
+stereo --ip-per-tile 10000 --corr-seed-mode 1 --threads 16 run/M0100115.tif run/E0201461.tif run/M0100115.xml run/E0201461.xml run/run
+point2dem run/run-PC.tif
+
--- ss_cam2rpc_mars/validate.sh
+++ ss_cam2rpc_mars/validate.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-for file in run/run.tif run/run.xml; do
+file=run/run-DEM.tif
+gold=gold/run-DEM.tif
 
-  gold=${file/run\/run/gold\/run}
+if [ ! -e "$file" ]; then
+    echo "ERROR: File $file does not exist."
+    exit 1;
+fi
 
-  if [ ! -e "$file" ]; then
-      echo "ERROR: File $file does not exist."
-      exit 1;
-  fi
+if [ ! -e "$gold" ]; then
+    echo "ERROR: File $gold does not exist."
+    exit 1;
+fi
 
-  if [ ! -e "$gold" ]; then
-      echo "ERROR: File $gold does not exist."
-      exit 1;
-  fi
+# Remove cached xmls
+rm -fv "$file.aux.xml"
+rm -fv "$gold.aux.xml"
 
-  echo cmp $file $gold
-  diff=$(cmp $file $gold)
+cmp_stats.sh $file $gold
+gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v size | grep -v Left | grep -v Right > run.txt
+gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v size | grep -v Left | grep -v Right > gold.txt
 
-  echo diff is $diff
-  if [ "$file" = "run/run.tif" ]; then
-      # The gold tif file must be same as the current one
-      if [ "$diff" != "" ]; then
-          echo Validation failed
-          exit 1
-      fi
-  else
-      # For the xml file, compare the values with a relative error threshold
-      if [ "$file" = "run/run.xml" ]; then
-          max_err.pl $file $gold # print the error
-          ans=$(max_err.pl $file $gold 1e-8) # compare the error
-          if [ "$ans" -eq 0 ]; then
-          echo Validation failed
-          exit 1
-          fi
-      fi
-  fi
-done
+diff run.txt gold.txt
+
+max_err.pl run.txt gold.txt # print the error
+ans=$(max_err.pl run.txt gold.txt 1e-5) # compare the error
+if [ "$ans" -eq 0 ]; then
+    echo Validation failed
+    exit 1
+fi
+
+rm -f run.txt gold.txt
+
+echo diff is $diff
+if [ "$diff" != "" ]; then
+    echo Validation failed
+    exit 1
+fi
 
 echo Validation succeded
 exit 0
