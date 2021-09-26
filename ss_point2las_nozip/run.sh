3b63da5e88e14e00f826e66e282afe5733fe2a97
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-09-26 12:17:17 -0700

    Update point2las test and add more ip to a stereo test

--- ss_point2las_nozip/run.sh
+++ ss_point2las_nozip/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-point2las ../data/ref-PC.tif --output-prefix run/run-LAS -r Earth
+point2las --triangulation-error-factor 255 ../data/ref-PC.tif --output-prefix run/run-LAS -r Earth
 
