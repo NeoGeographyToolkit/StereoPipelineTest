25c38c71b3f6fc71e7b711ab4e39d8cca92ac44a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-09-30 14:26:55 -0700

    Change test to reflect testing of OpenCV_SGBM

--- ssISIS_alignLocalEpi_seedMode2_mapProj0_subPix1_OpenCVSGBM/run.sh
+++ ssISIS_alignLocalEpi_seedMode2_mapProj0_subPix1_OpenCVSGBM/run.sh
@@ -2,7 +2,7 @@
 
 set -x verbose
 rm -rfv run
-# Multiview
-parallel_stereo --enable-fill-holes ../data/M0100115_crop2.cub ../data/E0201461_crop.cub ../data/E0201461_crop2.cub run/run -s stereo.default --left-image-crop-win 0 124 672 4864 --disparity-estimation-dem ../data/ref-DEM.tif --disparity-estimation-dem-error 5 --alignment-method local_epipolar --corr-seed-mode 2 --subpixel-mode 1 
+# Multiview with OpenCV_SGBM stereo
+parallel_stereo --enable-fill-holes ../data/M0100115_crop2.cub ../data/E0201461_crop.cub ../data/E0201461_crop2.cub run/run -s stereo.default --left-image-crop-win 0 124 672 4864 --disparity-estimation-dem ../data/ref-DEM.tif --disparity-estimation-dem-error 5 --alignment-method local_epipolar --corr-seed-mode 2 --subpixel-mode 1 --stereo-algorithm opencv_sgbm
 
 point2dem -r mars run/run-PC.tif --nodata-value -32767
