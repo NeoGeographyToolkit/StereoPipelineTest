bf6840d9724e400ac39e5462475f0c8ec61dbe11
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-09-30 15:40:53 -0700

    Tweak some more local alignment algorithms

--- ssDG_alignHom_seedMode1_mapProj0_subPix1_BasaltHills/run.sh
+++ ssDG_alignHom_seedMode1_mapProj0_subPix1_BasaltHills/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.xml ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.xml run/run --left-image-crop-win 1024 1024 2048 2048 --corr-max-levels 5 -t dg -s stereo.default --alignment-method homography --subpixel-mode 1 --disable-fill-holes --threads 32 --subpixel-h-kernel 19 --subpixel-v-kernel 19 --corr-seed-mode 1
+parallel_stereo --corr-max-levels 5 -t dg --alignment-method local_epipolar --stereo-algorithm mgm --subpixel-mode 1 --disable-fill-holes --subpixel-h-kernel 19 --subpixel-v-kernel 19 --left-image-crop-win 2529 1535 707 372 --right-image-crop-win 2355 1483 768 376 --corr-tile-size 512 --sgm-collar-size 256 --corr-seed-mode 1 --stereo-file stereo.default --compute-point-cloud-center-only --threads 16 ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.xml ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.xml run/run
 
 point2dem -r Earth run/run-PC.tif --nodata-value -32767 --orthoimage run/run-L.tif --orthoimage-hole-fill-len 100 --dem-hole-fill-len 100 --tr .000174961596144 --remove-outliers  --median-filter-params 11 15  --erode-length 2
 
--- ssISIS_alignLocalEpi_seedMode1_mapProj0_subPix1/run.sh
+++ ssISIS_alignLocalEpi_seedMode1_mapProj0_subPix1/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-parallel_stereo --enable-fill-holes ../data/M0100115_crop.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method local_epipolar --corr-seed-mode 1 --subpixel-mode 1  --compute-error-vector
+parallel_stereo --enable-fill-holes --left-image-crop-win 0 1024 672 4864 --alignment-method local_epipolar --subpixel-mode 1 --compute-error-vector --stereo-algorithm asp_mgm --corr-seed-mode 1 --stereo-file stereo.default --compute-point-cloud-center-only --threads 16 ../data/M0100115_crop.cub ../data/E0201461.cub run/run 
 
 point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage --x-offset -161 --t_srs "+proj=eqc +lon_0=0 +x_0=0 +y_0=0 +a=3396190 +b=3396190 +units=m +no_defs" --tr 5
 
