c57b98b682f2db327487e00fb9fe6ee7d532d0e7
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-09-30 17:40:48 -0700

    Wipe some stereo default files and adjust some local alignment tests

--- ssISIS_alignHom_seedMode3_mapProj0_subPix1/run.sh
+++ ssISIS_alignHom_seedMode3_mapProj0_subPix1/run.sh
@@ -7,5 +7,3 @@ rm -rfv run
 stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method homography --corr-seed-mode 3 --subpixel-mode 1
 point2dem -r mars run/run-PC.tif --nodata-value -32767
 
-
-
--- ssISIS_alignLocalEpi_seedMode2_mapProj0_subPix1_OpenCVSGBM/run.sh
+++ ssISIS_alignLocalEpi_seedMode2_mapProj0_subPix1_OpenCVSGBM/run.sh
@@ -3,6 +3,6 @@
 set -x verbose
 rm -rfv run
 
-parallel_stereo --enable-fill-holes ../data/M0100115_crop2.cub ../data/E0201461_crop.cub run/run -s stereo.default --left-image-crop-win 0 124 672 4864 --disparity-estimation-dem ../data/ref-DEM.tif --disparity-estimation-dem-error 5 --alignment-method local_epipolar --corr-seed-mode 2 --subpixel-mode 1 --stereo-algorithm opencv_sgbm
+parallel_stereo ../data/M0100115_crop2.cub ../data/E0201461_crop.cub run/run --left-image-crop-win 0 124 672 4864 --disparity-estimation-dem ../data/ref-DEM.tif --disparity-estimation-dem-error 5 --alignment-method local_epipolar --corr-seed-mode 2 --stereo-algorithm opencv_sgbm
 
 point2dem -r mars run/run-PC.tif --nodata-value -32767
