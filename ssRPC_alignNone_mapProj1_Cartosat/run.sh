347dc957612d6043a33002e82786eb2a855852b0
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-11-11 15:05:11 -0800

    Test point2dem with a string using EPSG

--- ssRPC_alignNone_mapProj1_Cartosat/run.sh
+++ ssRPC_alignNone_mapProj1_Cartosat/run.sh
@@ -4,6 +4,7 @@ set -x verbose
 rm -rfv run
 
 stereo ../data/cartosatA.tif ../data/cartosatF.tif run/run ../data/cartosat-DEM.tif
-point2dem --orthoimage run/run-PC.tif run/run-L.tif
 
+# Test point2dem with a string using EPSG
+point2dem --t_srs 'EPSG:4326' --orthoimage run/run-PC.tif run/run-L.tif
 
