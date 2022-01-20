16c860967ec1544fcc6fb828e616b5bb789f3d85
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-01-20 11:58:38 -0800

    Add ASTER testcases, with exact and RPC models

---
+++ ssASTER_RPC_alignAffEpp/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+stereo -t RPC ../data/aster-Band3N.tif ../data/aster-Band3B.tif ../data/aster-Band3N.xml ../data/aster-Band3B.xml run/run --left-image-crop-win 2523 3311 820 753 --right-image-crop-win 3186 4068 1082 1218
+
+point2dem run/run-PC.tif
