443fbed3355b519797faab9464a71b0157c834fd
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-01-26 10:23:41 -0800

    Test the new dem_mosaic --tap option

--- ss_dem_mosaic_polar/run.sh
+++ ss_dem_mosaic_polar/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-dem_mosaic ../data/krigged_part1.tif ../data/krigged_part2.tif -o run/run
+dem_mosaic --tr 10000.0 --tap ../data/krigged_part1.tif ../data/krigged_part2.tif -o run/run
 
