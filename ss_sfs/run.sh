dd4db8776b943f53e59a326719964be6a4cdb8a8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-01-26 17:47:57 -0800

    Tweak the sfs test

--- ss_sfs/run.sh
+++ ss_sfs/run.sh
@@ -3,6 +3,6 @@
 set -x verbose
 rm -rfv run
 
-parallel_sfs -i ../data/sfs-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --use-approx-camera-models  --crop-input-images --threads 1
+parallel_sfs -i ../data/sfs-input-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --use-approx-camera-models  --crop-input-images --threads 1
 
 
