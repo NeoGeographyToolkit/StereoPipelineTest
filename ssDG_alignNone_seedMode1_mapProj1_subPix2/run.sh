e9170ba9656424da0785ba10f4ce2709cc0fe696
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-01-31 12:08:03 -0800

    Adjust some tests given the changed behavior of the --skip-image-normalization option

--- ssDG_alignNone_seedMode1_mapProj1_subPix2/run.sh
+++ ssDG_alignNone_seedMode1_mapProj1_subPix2/run.sh
@@ -3,6 +3,9 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/left.tif ../data/right.tif ../data/left.xml ../data/right.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif  --disable-fill-holes -s stereo.default -t dg --alignment-method none --corr-seed-mode 1 --subpixel-mode 2 --left-image-crop-win 0 0 256 512 --skip-image-normalization
+# Cut a little clip to speed things up
+gdal_translate -srcwin 0 0 256 512 ../data/left.tif left_clip.tif
+
+stereo left_clip.tif ../data/right.tif ../data/left.xml ../data/right.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif  --disable-fill-holes -s stereo.default -t dg --alignment-method none --corr-seed-mode 1 --subpixel-mode 2 --skip-image-normalization
 point2dem -r Earth run/run-PC.tif --nodata-value -32767
 
--- ssRPC_alignNone_seedMode1_mapProj1_subPix1_BasaltHills/run.sh
+++ ssRPC_alignNone_seedMode1_mapProj1_subPix1_BasaltHills/run.sh
@@ -1,7 +1,7 @@
 set -x verbose
 rm -rfv run
 
-stereo --corr-timeout 6000 ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_rpc_5mpp.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_rpc_5mpp.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_rpc.xml ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_rpc.xml run/run ../data/zone10-CA_SanLuisResevoir-9m.tif --corr-max-levels 5 -t rpcmaprpc -s stereo.default --alignment-method none --subpixel-mode 1 --disable-fill-holes --threads 32 --subpixel-h-kernel 19 --subpixel-v-kernel 19 --corr-seed-mode 1 --left-image-crop-win 1024 1024 1024 2048 --skip-image-normalization
+stereo --corr-timeout 6000 ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_rpc_5mpp.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_rpc_5mpp.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_rpc.xml ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_rpc.xml run/run ../data/zone10-CA_SanLuisResevoir-9m.tif --corr-max-levels 5 -t rpcmaprpc -s stereo.default --alignment-method none --subpixel-mode 1 --disable-fill-holes --threads 32 --subpixel-h-kernel 19 --subpixel-v-kernel 19 --corr-seed-mode 1 --left-image-crop-win 1024 1024 1024 2048 
 
 point2dem -r Earth run/run-PC.tif --nodata-value -32767
 
