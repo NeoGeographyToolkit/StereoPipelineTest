0d97f82bd2ae93ebe7a092a819ba5e546fe5c984
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-02-03 09:52:11 -0800

    Tune some bundle adjust testcases, increasing the number of pairwise matches

--- ss_bundle_adjust_terrain_constraint/run.sh
+++ ss_bundle_adjust_terrain_constraint/run.sh
@@ -15,5 +15,5 @@ point2dem run/run-PC.tif
 mv -fv run/run-disp-left_sub16__right_sub16.match run/run-left_sub16__right_sub16.match
 
 # Finally run bundle adjustment
-bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run --intrinsics-to-float other_intrinsics --disparity-list run/run-left_sub16__right_sub16-unaligned-D.tif --max-disp-error 10 --max-iterations 5 --parameter-tolerance 1e-12 --reference-terrain run/run-DEM.tif --max-num-reference-points 1000 --threads 1
+bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run --intrinsics-to-float other_intrinsics --disparity-list run/run-left_sub16__right_sub16-unaligned-D.tif --max-disp-error 10 --max-iterations 5 --parameter-tolerance 1e-12 --reference-terrain run/run-DEM.tif --max-num-reference-points 1000 --threads 1 --max-pairwise-matches 10000
 
