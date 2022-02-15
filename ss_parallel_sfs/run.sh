9adc2ee5752103bfb89ff581cbfba8493973ef8c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-02-15 12:02:33 -0800

    Wipe some spaces in test

--- ss_parallel_sfs/run.sh
+++ ss_parallel_sfs/run.sh
@@ -5,5 +5,3 @@ rm -rfv run
 
 parallel_sfs -i ../data/sfs-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --use-approx-camera-models  --crop-input-images --threads 1 --tile-size 60 --padding 30
 
-
-
