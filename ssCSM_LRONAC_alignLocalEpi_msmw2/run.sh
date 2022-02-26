47c93c491b80c23d1a355ff756e183f3ba5732f4
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-02-25 17:17:28 -0800

    Add more local_epipolar testcases

---
+++ ssCSM_LRONAC_alignLocalEpi_msmw2/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+parallel_stereo --left-image-crop-win 3583 3922 1767 1691 --right-image-crop-win 4095 4348 1498 1668 --threads 16 --corr-seed-mode 1 ../data/M181058717LE.ce.cub ../data/M181073012LE.ce.cub ../data/M181058717LE.ce.json ../data/M181073012LE.ce.json run/run --corr-tile-size 3072 --sgm-collar-size 256 --alignment-method local_epipolar --stereo-algorithm msmw2 --corr-timeout 10000
+point2dem run/run-PC.tif
