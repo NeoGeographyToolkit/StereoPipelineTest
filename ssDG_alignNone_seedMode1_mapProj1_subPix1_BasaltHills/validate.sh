2b5ee03f12e5ed9be9fd73dfca0d04313246b70a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-03-09 16:32:21 -0800

    Add more tests with l-r disp diff

--- ssDG_alignNone_seedMode1_mapProj1_subPix1_BasaltHills/validate.sh
+++ ssDG_alignNone_seedMode1_mapProj1_subPix1_BasaltHills/validate.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-file=run/run-DEM.tif
-gold=gold/run-DEM.tif
+for file in run/run-DEM.tif run/run-L-R-disp-diff.tif; do 
+    
+    gold=${file/run\//gold\/}
 
-if [ ! -e "$file" ]; then
-    echo "ERROR: File $file does not exist."
-    exit 1;
-fi
 
-if [ ! -e "$gold" ]; then
-    echo "ERROR: File $gold does not exist."
-    exit 1;
-fi
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
+
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
 
 # Remove cached xmls
-rm -fv "$file.aux.xml"
-rm -fv "$gold.aux.xml"
+    rm -fv "$file.aux.xml"
+    rm -fv "$gold.aux.xml"
 
-cmp_stats.sh $file $gold
-gdalinfo -stats $file | grep -v Files | grep -v -i tif > run.txt
-gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold.txt
+    cmp_stats.sh $file $gold
+    gdalinfo -stats $file | grep -v Files | grep -v -i tif > run.txt
+    gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold.txt
 
-diff=$(diff run.txt gold.txt)
-cat run.txt
+    diff=$(diff run.txt gold.txt)
+    cat run.txt
 
-rm -f run.txt gold.txt
+    rm -f run.txt gold.txt
 
-echo diff is $diff
-if [ "$diff" != "" ]; then
-    echo Validation failed
-    exit 1
-fi
+    echo diff is $diff
+    if [ "$diff" != "" ]; then
+        echo Validation failed
+        exit 1
+    fi
+done
 
 echo Validation succeded
 exit 0
--- ssISIS_alignHom_seedMode1_mapProj0_subPix1/validate.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1/validate.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-file=run/run-DEM.tif
-gold=gold/run-DEM.tif
+for file in run/run-DEM.tif run/run-L-R-disp-diff.tif; do 
+    
+    gold=${file/run\//gold\/}
 
-if [ ! -e "$file" ]; then
-    echo "ERROR: File $file does not exist."
-    exit 1;
-fi
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
+    
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
+    
+    # Remove cached xmls
+    rm -fv "$file.aux.xml"
+    rm -fv "$gold.aux.xml"
 
-if [ ! -e "$gold" ]; then
-    echo "ERROR: File $gold does not exist."
-    exit 1;
-fi
+    cmp_stats.sh $file $gold
+    gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v size | grep -v Left | grep -v Right > run.txt
+    gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v size | grep -v Left | grep -v Right > gold.txt
 
-# Remove cached xmls
-rm -fv "$file.aux.xml"
-rm -fv "$gold.aux.xml"
+    diff run.txt gold.txt
 
-cmp_stats.sh $file $gold
-gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v size | grep -v Left | grep -v Right > run.txt
-gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v size | grep -v Left | grep -v Right > gold.txt
+    max_err.pl run.txt gold.txt # print the error
+    ans=$(max_err.pl run.txt gold.txt 1e-5) # compare the error
+    if [ "$ans" -eq 0 ]; then
+        echo Validation failed
+        exit 1
+    fi
+    
+    rm -f run.txt gold.txt
+    
+    echo diff is $diff
+    if [ "$diff" != "" ]; then
+        echo Validation failed
+        exit 1
+    fi
 
-diff run.txt gold.txt
-
-max_err.pl run.txt gold.txt # print the error
-ans=$(max_err.pl run.txt gold.txt 1e-5) # compare the error
-if [ "$ans" -eq 0 ]; then
-    echo Validation failed
-    exit 1
-fi
-
-rm -f run.txt gold.txt
-
-echo diff is $diff
-if [ "$diff" != "" ]; then
-    echo Validation failed
-    exit 1
-fi
+done
 
 echo Validation succeded
 exit 0
