0f95eb4fc522c6498f5bdce82fe86832687dd940
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-03-16 16:46:36 -0700

    Wipe spaces

--- ssISIS_alignAffEpp_seedMode2_mapProj0_SGM/run.sh
+++ ssISIS_alignAffEpp_seedMode2_mapProj0_SGM/run.sh
@@ -6,5 +6,3 @@ rm -rfv run
 parallel_stereo ../data/M0100115_crop.cub ../data/E0201461.cub run/run -s stereo.default --disable-fill-holes --left-image-crop-win 0 1024 672 4864 --disparity-estimation-dem ../data/ref-DEM.tif --disparity-estimation-dem-error 5 --alignment-method affineepipolar --corr-seed-mode 2  --stereo-algorithm asp_mgm --corr-tile-size 2000
 point2dem -r mars run/run-PC.tif --nodata-value -32767
 
-
-
