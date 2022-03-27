edc116e0aa58c9ddff6b0110c8209362ac9dd8c9
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-03-27 09:39:51 -0700

    rm --num-threads 1. Normally bundle_adjust will set that correctly for ISIS as it is.

--- ss_bundle_adjust_isis_ceres/run.sh
+++ ss_bundle_adjust_isis_ceres/run.sh
@@ -12,5 +12,5 @@ echo "0      0       1.005 400" >> transform.txt
 echo "0      0       0       0" >> transform.txt
 
 # Testing reading adjustments and applying a transform on top
-bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/out --datum D_MOON --input-adjustments-prefix run/ba/in --initial-transform transform.txt --threads 1 --camera-weight 100
+bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub ../data/gcp1001.gcp --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/ba/out --datum D_MOON --input-adjustments-prefix run/ba/in --initial-transform transform.txt --camera-weight 100
 
