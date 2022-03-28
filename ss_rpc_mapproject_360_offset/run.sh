18659df54912b32446ab9e017e8bf3b2bb8388b1
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-03-28 11:14:01 -0700

    Update test for ss_rpc_mapproject_360_offset

--- ss_rpc_mapproject_360_offset/run.sh
+++ ss_rpc_mapproject_360_offset/run.sh
@@ -3,11 +3,5 @@
 set -x verbose
 rm -rfv run
 
-opt="+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
-
-# mapproject can do multiple output types
-for ot in Byte UInt16 Int16 UInt32 Int32 Float32; do
-	mapproject -t rpc --mpp 25 --t_srs "$opt" ../data/krigged_dem_nsidc_ndv0_fill_crop.tif ../data/WV01_11JAN131652222-P1BS-10200100104A0300.tif ../data/WV01_11JAN131652222-P1BS-10200100104A0300.xml run/run-RPC_${ot}.tif --ot $ot
-done
-
+mapproject ../data/filled_dem.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.xml run/run.tif --tr 0.001
 
