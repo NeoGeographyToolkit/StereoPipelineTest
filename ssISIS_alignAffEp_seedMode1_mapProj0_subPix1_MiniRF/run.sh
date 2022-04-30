56419db609421e9e269513879a384d89c5851b45
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-04-30 12:37:19 -0700

    Update a SAR testcase

--- ssISIS_alignAffEp_seedMode1_mapProj0_subPix1_MiniRF/run.sh
+++ ssISIS_alignAffEp_seedMode1_mapProj0_subPix1_MiniRF/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/LSZ_01636_1CD_XKU_09N120_S1.8bit_crop.cub ../data/LSZ_02331_1CD_XKU_15N120_S1.8bit_crop.cub run/run --ip-per-tile 10000 --min-num-ip 20
+parallel_stereo ../data/LSZ_01636_1CD_XKU_09N120_S1.8bit.cub ../data/LSZ_02330_1CD_XKU_00S120_S1.8bit.cub ../data/LSZ_01636_1CD_XKU_09N120_S1.8bit.state.json ../data/LSZ_02330_1CD_XKU_00S120_S1.8bit.state.json run/run --stereo-algorithm asp_mgm --left-image-crop-win -80 9980 830 943 --right-image-crop-win 1222 48549 1718 1550
 
 point2dem run/run-PC.tif --errorimage
 
