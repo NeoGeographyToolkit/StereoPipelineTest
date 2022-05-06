43b8b356dac8232ef0ce84c429eecacf548dd0ca
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-05-06 10:10:34 -0700

    Update the SAR example

--- ssCSM_SAR/run.sh
+++ ssCSM_SAR/run.sh
@@ -3,8 +3,7 @@
 set -x verbose
 rm -rfv run
 
-# This is not a great testcase but the best available so far 
-parallel_stereo --corr-kernel 45 45 --subpixel-kernel 45 45 --ip-per-tile 5000 --stereo-algorithm asp_bm --left-image-crop-win -201 8218 2123 3509 --right-image-crop-win 765 46111 2236 4471 --corr-seed-mode 1 --threads 16 ../data/LSZ_01636_1CD_XKU_09N120_S1.8bit.cub ../data/LSZ_02330_1CD_XKU_00S120_S1.8bit.cub ../data/LSZ_01636_1CD_XKU_09N120_S1.8bit_fixed.json ../data/LSZ_02330_1CD_XKU_00S120_S1.8bit_fixed.json run/run
+parallel_stereo ../data/lsz_03821_1cd_xku_16n196_v1_clamp_crop.tif ../data/lsz_03822_1cd_xku_23n196_v1_clamp_crop.tif ../data/lsz_03821_1cd_xku_16n196_v1.json ../data/lsz_03822_1cd_xku_23n196_v1.json --stereo-algorithm asp_mgm --skip-rough-homography --no-datum --ip-per-tile 6000 --left-image-crop-win 469 8005 1831 1822 --right-image-crop-win 562 27190 1404 1696 run/run
 
 point2dem run/run-PC.tif
 
