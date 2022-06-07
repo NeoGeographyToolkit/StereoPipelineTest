070d94738d04ff034d90a1c6a43afcc49fdee095
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-06-07 10:36:33 -0700

    Tune-up some tests

--- ssISIS_alignHom_seedMode2_mapProj0_subPix1/run.sh
+++ ssISIS_alignHom_seedMode2_mapProj0_subPix1/run.sh
@@ -3,8 +3,6 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/M0100115_crop.cub ../data/E0201461.cub run/run -s stereo.default --disable-fill-holes --left-image-crop-win 0 1024 672 1200 --disparity-estimation-dem ../data/ref-DEM.tif --disparity-estimation-dem-error 5 --alignment-method homography --corr-seed-mode 2 --subpixel-mode 1
+stereo ../data/M0100115_crop.cub ../data/E0201461.cub run/run -s stereo.default --disable-fill-holes --left-image-crop-win 0 1024 672 1200 --disparity-estimation-dem ../data/seed-mode-2-init-DEM.tif --disparity-estimation-dem-error 5 --alignment-method homography --corr-seed-mode 2 --subpixel-mode 1
 point2dem -r mola run/run-PC.tif --nodata-value -32767
-
-
 
--- ssNadirPinHole_alignAffEpi_seedMode1_mapProj0_subPix1_OIB_dist_bathy/run.sh
+++ ssNadirPinHole_alignAffEpi_seedMode1_mapProj0_subPix1_OIB_dist_bathy/run.sh
@@ -9,20 +9,19 @@ rthresh=141
 image_calc -c "max($lthresh, var_0)" --output-nodata-value $lthresh ../data/img_icebridge2.tif -o run/left_mask.tif
 image_calc -c "max($lthresh, var_0)" --output-nodata-value $lthresh ../data/img_icebridge3.tif -o run/right_mask.tif
 
-bathy_plane_calc --shapefile ../data/bathy_pinhole_shoreline.shp --dem \
-    run/run-DEM.tif --outlier-threshold 0.2                            \
-    --bathy-plane run/bathy-plane-nadirpinhole.txt
-
-parallel_stereo --alignment-method affineepipolar                \
-  --xcorr-threshold -1 --corr-kernel 5 5                         \
-    --cost-mode 4  --stereo-algorithm asp_mgm --threads-single 8 \
-    --corr-seed-mode 1 --session-type nadirpinhole               \
-    ../data/img_icebridge2.tif ../data/img_icebridge3.tif        \
-    ../data/img_icebridge2.tsai ../data/img_icebridge3.tsai      \
-    --left-bathy-mask run/left_mask.tif                          \
-    --right-bathy-mask run/right_mask.tif                        \
-    --refraction-index 1.333                                     \
-    --bathy-plane run/bathy-plane-nadirpinhole.txt               \
+parallel_stereo --alignment-method affineepipolar           \
+    --xcorr-threshold -1 --corr-kernel 5 5                  \
+    --cost-mode 4 --stereo-algorithm asp_mgm                \
+    --session-type nadirpinhole                             \
+    --left-bathy-mask run/left_mask.tif                     \
+    --right-bathy-mask run/right_mask.tif                   \
+    --refraction-index 1.333                                \
+    --bathy-plane ../data/bathy_plane_pinhole.txt           \
+    --left-image-crop-win 4089 386 888 786                  \
+    --right-image-crop-win 3170 489 1011 725                \
+    --corr-seed-mode 1 --threads 8                          \
+    ../data/img_icebridge2.tif ../data/img_icebridge3.tif   \
+    ../data/img_icebridge2.tsai ../data/img_icebridge3.tsai \
     run/run
 
 point2dem  --t_srs '+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs' --tr 0.4 run/run-PC.tif
