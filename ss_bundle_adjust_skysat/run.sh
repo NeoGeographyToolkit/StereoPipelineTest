95656af9de032f87146fabfa3f0e603e1a5833f8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-08-30 16:00:22 -0700

    ss_bundle_adjust_skysat: Spell out image names

--- ss_bundle_adjust_skysat/run.sh
+++ ss_bundle_adjust_skysat/run.sh
@@ -3,7 +3,9 @@
 set -x verbose
 rm -rfv run
 
-parallel_bundle_adjust ../data/skysat/a*tiff --threads 1           \
-    --auto-overlap-params "../data/skysat/uluru_copernicus.tif 10" \
-	-o run/run
+parallel_bundle_adjust                                                            \
+  ../data/skysat/a1000.tiff  ../data/skysat/a1020.tiff  ../data/skysat/a1040.tiff \
+  ../data/skysat/a1010.tiff  ../data/skysat/a1030.tiff  ../data/skysat/a1050.tiff \
+  --threads 1 --auto-overlap-params "../data/skysat/uluru_copernicus.tif 10"      \
+  -o run/run
 
