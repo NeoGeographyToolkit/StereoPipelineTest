5ddf004e97e1a5c6e383642db3b37b600e49e20c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-10-07 10:05:51 -0700

    Format one test

--- ss_point2dem_csv_proj4/run.sh
+++ ss_point2dem_csv_proj4/run.sh
@@ -5,4 +5,3 @@ rm -rfv run
 
 point2dem -r Earth ../data/zone10-CA_SanLuisResevoir-9m_10pct-utm.csv --tr 10 -o run/run --nodata-value -32767 --csv-format '1:easting 2:northing 3:height_above_datum' --csv-proj4 '+proj=utm +zone=10 +datum=WGS84 +units=m +no_defs'
 
-
