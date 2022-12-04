a95696d1d164e60719f77da27316ab6cafd64c95
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-12-03 20:31:33 -0800

    Add another test for gcp;

---
+++ ss_bundle_adjust_gcp_blacksky/validate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+file=run/run-BSG-118-20220730-123737-33360052_georeferenced-pan.tsai 
+gold=gold/run-BSG-118-20220730-123737-33360052_georeferenced-pan.tsai
+
+max_err.pl $file $gold  # print the error
+ans=$(max_err.pl $file $gold 1e-10) # compare the error
+if [ "$ans" -eq 0 ]; then
+    echo Validation failed
+    exit 1
+fi
+
+echo Validation succeded
+exit 0
