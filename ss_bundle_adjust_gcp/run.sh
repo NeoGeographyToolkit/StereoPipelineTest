9ed4b5be769b50e958baabca6cb72b015a431725
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-12-05 08:56:16 -0800

    Update a bundle_adjust testcase given recent code changes

--- ss_bundle_adjust_gcp/run.sh
+++ ss_bundle_adjust_gcp/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-# Bundle adjustment to purely initialize a camera with GCP
-bundle_adjust ../data/DMS_20171029_183706_02501.tif ../data/DMS_20171029_183706_02501.tsai ../data/DMS_20171029_183706_02501.gcp -o run/run --datum WGS84 --inline-adjustments --camera-weight 0 --max-iterations 10 --threads 1
+# Bundle adjustment to purely initialize a camera with GCP, using --init-camera-using-gcp
+bundle_adjust --init-camera-using-gcp ../data/DMS_20171029_183706_02501.tif ../data/DMS_20171029_183706_02501.tsai ../data/DMS_20171029_183706_02501.gcp -o run/run --datum WGS84 --inline-adjustments --camera-weight 0 --max-iterations 10 --threads 1
