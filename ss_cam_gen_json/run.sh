59746dbb305ffe9b2ca06b9fdd9174f62741c10b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-12-20 18:32:33 -0800

    Add ss_cam_gen_json test

---
+++ ss_cam_gen_json/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+# Run cam_gen with a Pinhole file in json format provided by Planet.
+
+cam_gen ../data/1259344352.36984587_sc00104_c2_PAN_i0000000001.tif --input-camera ../data//1259344352.36984587_sc00104_c2_PAN_i0000000001_pinhole.json -o run/run.tsai
