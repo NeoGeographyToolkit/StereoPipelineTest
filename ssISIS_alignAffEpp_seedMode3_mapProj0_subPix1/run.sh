80a32cd142a13e23dcab1ba7186b888929017b30
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-12-29 11:46:44 -0800

    Fix test: lon and lat were swapped

--- ssISIS_alignAffEpp_seedMode3_mapProj0_subPix1/run.sh
+++ ssISIS_alignAffEpp_seedMode3_mapProj0_subPix1/run.sh
@@ -4,6 +4,5 @@ set -x verbose
 rm -rfv run
 
 stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 1200 --alignment-method affineepipolar --subpixel-mode 1 --verbose
-point2dem -r mars --gnomonic --proj-lat 141.5608796 --proj-lon 34.2926059 run/run-PC.tif
-
+point2dem -r mars --gnomonic --proj-lon 141.5608796 --proj-lat 34.2926059 run/run-PC.tif
 
