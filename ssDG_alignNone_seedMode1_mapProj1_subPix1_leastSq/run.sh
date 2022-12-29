c83456967b91fe025c14944fa82072683e57a50a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-12-29 11:54:28 -0800

    Wipe old tests

--- ssDG_alignNone_seedMode1_mapProj1_subPix1_leastSq/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x verbose
-rm -rfv run
-
-stereo ../data/left.tif ../data/right.tif ../data/left.xml ../data/right.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif -s stereo.default -t dg --left-image-crop-win 0 0 150 150 --alignment-method none --corr-seed-mode 1 --subpixel-mode 1 --use-least-squares --corr-search -2 -5 4 4
-point2dem -r Earth run/run-PC.tif --nodata-value -32767
-
