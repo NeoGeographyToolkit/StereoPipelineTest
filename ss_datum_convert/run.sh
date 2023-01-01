59a571b87c865725c4eb0bd6a48c615c3078dbc6
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-12-31 17:24:28 -0800

    Wipe datum_convert; will not be shipped

--- ss_datum_convert/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x verbose
-rm -rfv run
-
-# Generate copy of the DEM in a new datum
-datum_convert ../data/san_luis_dam_75m.dem.tif run/san_luis_dam_75m_wgs84.tif --output-datum wgs84
-
