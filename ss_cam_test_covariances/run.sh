99c556ce6d580609f06ef068f4402911942e63fe
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-01-13 14:05:33 -0800

    Add test for cam_test with covariances

---
+++ ss_cam_test_covariances/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+mkdir run
+
+cam_test --dg-use-csm --session1 dg --session2 dg --test-covariance-computation --image ../data/grand_mesa_WV03_left_crop.tif --cam1 ../data/grand_mesa_WV03_left.xml --cam2 ../data/grand_mesa_WV03_right.xml --bundle-adjust-prefix ../data/ba_grand_mesa/run |tee run/run-out.txt
+
