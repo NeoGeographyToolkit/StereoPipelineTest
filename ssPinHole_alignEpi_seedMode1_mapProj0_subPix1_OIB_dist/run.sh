c41c3cbada70d33546024c1f902626326becb763
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-01-23 13:16:17 -0800

    Fix just added test

--- ssPinHole_alignEpi_seedMode1_mapProj0_subPix1_OIB_dist/run.sh
+++ ssPinHole_alignEpi_seedMode1_mapProj0_subPix1_OIB_dist/run.sh
@@ -7,6 +7,7 @@ parallel_stereo --alignment-method epipolar --xcorr-threshold -1 --corr-kernel 5
     --corr-tile-size 2048 --corr-memory-limit-mb 6096  \
     --cost-mode 4 --stereo-algorithm asp_mgm --threads-single 8 \
     --corr-seed-mode 1 --session-type pinhole \
+	--horizontal-variances 1 1 \
 	--compute-point-cloud-covariances \
     ../data/img_icebridge2.tif ../data/img_icebridge3.tif \
     ../data/img_icebridge2.tsai ../data/img_icebridge3.tsai \
