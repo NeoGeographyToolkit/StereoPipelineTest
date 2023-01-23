a8b41e43637b6ee47dae8ad28262842042149f6b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-01-23 12:08:25 -0800

    Add tests for computing covariances for Pleiades and RPC cameras

--- ssPleiades_Exact_alignAffEp_mapproj0_aspMgm/run.sh
+++ ssPleiades_Exact_alignAffEp_mapproj0_aspMgm/run.sh
@@ -3,9 +3,9 @@
 set -x verbose
 rm -rfv run
 
-parallel_stereo --corr-tile-size 5000 ../data/IMG_PHR1B_P_202204132246314_SEN_6304928201-1_R1C1_crop.TIF ../data/IMG_PHR1B_P_202204132247161_SEN_6304929201-1_R1C1_crop.tif ../data/DIM_PHR1B_P_202204132246314_SEN_6304928201-1.XML ../data/DIM_PHR1B_P_202204132247161_SEN_6304929201-1.XML --stereo-algorithm asp_mgm run/run
+parallel_stereo --corr-tile-size 5000 ../data/IMG_PHR1B_P_202204132246314_SEN_6304928201-1_R1C1_crop.TIF ../data/IMG_PHR1B_P_202204132247161_SEN_6304929201-1_R1C1_crop.tif ../data/DIM_PHR1B_P_202204132246314_SEN_6304928201-1.XML ../data/DIM_PHR1B_P_202204132247161_SEN_6304929201-1.XML --stereo-algorithm asp_mgm run/run --compute-point-cloud-covariances
 
-point2dem --stereographic --proj-lon 0 --proj-lat 90 --errorimage run/run-PC.tif
+point2dem --covariances --stereographic --proj-lon 0 --proj-lat 90 --errorimage run/run-PC.tif
 
 # Run with mapprojected images too
 mapproject run/run-DEM.tif ../data/IMG_PHR1B_P_202204132246314_SEN_6304928201-1_R1C1_crop.TIF ../data/DIM_PHR1B_P_202204132246314_SEN_6304928201-1.XML run/left_proj.tif
