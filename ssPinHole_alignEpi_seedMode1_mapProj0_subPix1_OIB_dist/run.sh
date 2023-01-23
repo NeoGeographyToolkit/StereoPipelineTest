08d763f58e5c5c04363dd1c7a257af705144a6ad
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-01-23 13:13:36 -0800

    Add Pinhole test with ground covariances

--- ssPinHole_alignEpi_seedMode1_mapProj0_subPix1_OIB_dist/run.sh
+++ ssPinHole_alignEpi_seedMode1_mapProj0_subPix1_OIB_dist/run.sh
@@ -7,9 +7,10 @@ parallel_stereo --alignment-method epipolar --xcorr-threshold -1 --corr-kernel 5
     --corr-tile-size 2048 --corr-memory-limit-mb 6096  \
     --cost-mode 4 --stereo-algorithm asp_mgm --threads-single 8 \
     --corr-seed-mode 1 --session-type pinhole \
+	--compute-point-cloud-covariances \
     ../data/img_icebridge2.tif ../data/img_icebridge3.tif \
     ../data/img_icebridge2.tsai ../data/img_icebridge3.tsai \
     run/run
 
-point2dem  --t_srs '+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs' --tr 0.4 run/run-PC.tif --errorimage
+point2dem  --t_srs '+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs' --tr 0.4 run/run-PC.tif --errorimage --covariances
 
