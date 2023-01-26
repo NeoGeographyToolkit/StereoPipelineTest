15ad0517fcbfadb772026ceda0f4cf0f04ef37ba
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-01-26 11:55:08 -0800

    Add test ss_image_align_ecef_trans

---
+++ ss_image_align_ecef_trans/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+# Align two images, then find the best ECEF transform between the corresponding DEM points
+image_align                                                                 \
+    ../data/zone10-CA_SanLuisResevoir-9m_crop_ned_shift_90_135_0.tif        \
+    ../data/zone10-CA_SanLuisResevoir-9m_crop.tif                           \
+    --ecef-transform-type translation                                       \
+    --dem1 ../data/zone10-CA_SanLuisResevoir-9m_crop_ned_shift_90_135_0.tif \
+    --dem2 ../data/zone10-CA_SanLuisResevoir-9m_crop.tif                    \
+    --output-prefix run/run                                                 \
+    -o run/aligned_image2.tif
+
---
+++ ss_image_align_ecef_trans/validate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in run/aligned_image2.tif run/run-transform.txt run/run-ecef-transform.txt; do
+    
+    gold=${file/run\/run/gold\/run}
+
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
+    
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
+    
+    cmp $file $gold
+    ans="$?"
+    
+    if [ "$ans" != "0" ]; then
+        echo Validation failed
+        exit 1
+    fi
+done
+
+echo Validation succeded
+exit 0
+
