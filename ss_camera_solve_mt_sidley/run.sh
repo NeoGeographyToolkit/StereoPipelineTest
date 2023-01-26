63d5995f90f9c2fd1eff14afb157b98896274b9f
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-01-26 11:54:36 -0800

    camera_solve: Reuse theia matches

--- ss_camera_solve_mt_sidley/run.sh
+++ ss_camera_solve_mt_sidley/run.sh
@@ -7,7 +7,7 @@ for ((i=0; i<=10;i++)); do
 
   rm -rfv run
 
-  camera_solve run/ ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif --calib-file camera_info.tsai  --gcp-file ground_control_points.gcp --theia-retries 10
+  camera_solve run/ ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif --calib-file camera_info.tsai  --gcp-file ground_control_points.gcp --theia-retries 10 --reuse-theia-matches
 
   stereo ../data/mt_sidley_47.tif ../data/mt_sidley_48.tif run/mt_sidley_47.tif.final.tsai run/mt_sidley_48.tif.final.tsai run/out --left-image-crop-win 1891 6034 690 345  --right-image-crop-win 4750 5889 708 369
 
