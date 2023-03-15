63a7d32e1fafc7cccffdca17e640a7a43b18eed9
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-03-15 12:56:32 -0700

    Fix recently added test

--- ss_sfm_merge/run.sh
+++ ss_sfm_merge/run.sh
@@ -8,5 +8,5 @@ sfm_submap -input_map ../data/norig.nvm -image_list ../data/norig_nvm_list1.txt 
 sfm_submap -input_map ../data/norig.nvm -image_list ../data/norig_nvm_list2.txt -output_map run/submap2.nvm
 
 # Then merge
-sfm_merge --rig_config ../data/norig_config.txt run/submap1.nvm run/submap2.nvm -num_image_overlaps_at_endpoints 4 -output_map run/merged.map -num_threads 1 
+sfm_merge --rig_config ../data/norig_config.txt run/submap1.nvm run/submap2.nvm -num_image_overlaps_at_endpoints 4 -output_map run/merged.nvm -num_threads 1 
 
