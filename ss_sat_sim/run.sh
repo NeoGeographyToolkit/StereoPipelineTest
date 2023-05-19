24cf7ed6fb4134f50e2255ea3a3e81475dce0106
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-05-19 09:40:36 -0700

    Add sat_sim example

---
+++ ss_sat_sim/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o run/run --first 397 490 -1000 --last 397 480 -1000 --num 2 --focal-length 2000 --optical-center 100 100 --image-size 200 200
+
+# Create orthoimages for inspection
+for i in 10000 10001; do
+  mapproject ../data/sat_sim_DEM.tif run/run${i}.tif run/run${i}.tsai run/run${i}.map.tif
+done
+
---
+++ ss_sat_sim/validate.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in gold/run10000.map.tif gold/run10001.map.tif; do
+    
+    gold=${file/run\//gold\/}
+
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
+    
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
+    
+    gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v size | grep -v Left | grep -v Right > run.txt
+    gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v size | grep -v Left | grep -v Right > gold.txt
+
+    diff run.txt gold.txt
+    rm -f run.txt gold.txt
+    
+    echo diff is $diff
+    if [ "$diff" != "" ]; then
+        echo Validation failed
+        exit 1
+    fi
+
+done
+
+echo Validation succeded
+exit 0
+
