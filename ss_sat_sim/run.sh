74485871689a96c5a7cb585f81c0f88ede903e5a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-05-22 14:08:35 -0700

    Update sat_sim testcase

--- ss_sat_sim/run.sh
+++ ss_sat_sim/run.sh
@@ -3,11 +3,10 @@
 set -x verbose
 rm -rfv run
 
-sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o run/run --first 397 490 -1000 --last 397 480 -1000 --num 2 --focal-length 2000 --optical-center 100 100 --image-size 200 200
+sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o run/run --first 397 490 -1000 --last 397 480 -1000 --num 2 --focal-length 5000 --optical-center 100 100 --image-size 200 200 --first-ground-pos 484 510 --last-ground-pos 332 893
 
 # Create orthoimages for inspection
 for i in 10000 10001; do
   mapproject ../data/sat_sim_DEM.tif run/run-${i}.tif run/run-${i}.tsai run/run-${i}.map.tif
 done
-
 
