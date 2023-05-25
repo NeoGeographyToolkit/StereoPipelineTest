d4a73ac8d4de24f1ecd7df12ed930291e91d49ea
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-05-25 11:46:46 -0700

    Tune sat_sim testcase

--- ss_sat_sim/validate.sh
+++ ss_sat_sim/validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-for file in gold/run-10000.map.tif run/run-10001.map.tif; do
+for file in $(ls run/run-1000[0-4].map.tif); do
     
     gold=${file/run\//gold\/}
 
