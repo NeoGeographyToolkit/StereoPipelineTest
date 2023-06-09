16ec278ab20c71da7fcf00d0bc788eb37cc1d4e7
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-06-09 11:43:01 -0700

    Update recent sat_sim tests

--- ss_sat_sim/run.sh
+++ ss_sat_sim/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o run/run --first 450 0 -1000 --last 450 3000 -1000 --num 5 --focal-length 5000 --optical-center 100 100 --image-size 200 200 --first-ground-pos 450 1500 --last-ground-pos 450 1500
+sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o run/run --first 450 1500 -1000 --last 450 2500 -1000 --num 5 --focal-length 5000 --optical-center 150 100 --image-size 300 200 --first-ground-pos 450 2000 --last-ground-pos 450 2000
 
 # Create orthoimages for inspection
 for ((i = 10000; i < 10005 ; i++)); do
--- ss_sat_sim_ground/run.sh
+++ ss_sat_sim_ground/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o run/run --first 428 688 -2000 --last 428 911 -2000 --num 5 --focal-length 5000 --optical-center 100 100 --image-size 200 200 --first-ground-pos 428 688 --last-ground-pos 428 911 --roll 0 --pitch 25 --yaw 0 --velocity 7500 --jitter-frequency 45 --horizontal-uncertainty 1.0 1.0 1.0
+sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o run/run --first 450 1500 -1000 --last 450 2500 -1000 --num 5 --focal-length 5000 --optical-center 150 100 --image-size 300 200 --first-ground-pos 450 1500 --last-ground-pos 450 2500 --roll 0 --pitch 25 --yaw 0 --velocity 7500 --jitter-frequency 45 --horizontal-uncertainty 1.0 1.0 1.0
 
