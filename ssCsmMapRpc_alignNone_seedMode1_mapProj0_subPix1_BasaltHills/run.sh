bd1a30fc25c589604b627ef70cc339c539d35a22
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-07-23 15:32:41 -0700

    Add testcase for stereo with CSM cameras for triangulation but RPC for mapprojection

---
+++ ssCsmMapRpc_alignNone_seedMode1_mapProj0_subPix1_BasaltHills/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+# Run stereo with CSM cameras using images mapprojected with the RPC model
+
+# First create matches
+bundle_adjust ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.xml ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.xml -o run/ba/run --num-iterations 0
+
+# Then create CSM cameras
+jitter_solve ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.xml ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.xml -o run/jitter/run --num-iterations 0 --match-files-prefix run/ba/run
+
+# Create mapprojected images with RPC cameras
+mapproject --tr 0.000053725423641 -t rpc ../data/stereo-DEM-09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.xml run/left_map.tif 
+
+mapproject --tr 0.000053725423641 -t rpc ../data/stereo-DEM-09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.xml run/right_map.tif
+
+# Run stereo with CSM and RPC-mapprojected images
+stereo run/left_map.tif run/right_map.tif run/jitter/run-09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.adjusted_state.json run/jitter/run-09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.adjusted_state.json run/run ../data/stereo-DEM-09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif --left-image-crop-win 1458 224 748 762 --right-image-crop-win 1495 219 776 849
+
+point2dem run/run-PC.tif
