08d123c951d30fa2bf2b021d3095bbe8f506d48c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-09-01 16:52:34 -0700

    Add bundle adjustment to aster testcase

--- ssASTER_Exact_alignAffEpp/run.sh
+++ ssASTER_Exact_alignAffEpp/run.sh
@@ -3,6 +3,9 @@
 set -x verbose
 rm -rfv run
 
-stereo -t ASTER ../data/aster-Band3N.tif ../data/aster-Band3B.tif ../data/aster-Band3N.xml ../data/aster-Band3B.xml run/run --left-image-crop-win 2523 3311 820 753 --right-image-crop-win 3186 4068 1082 1218
+bundle_adjust -t aster ../data/aster-Band3N.tif ../data/aster-Band3B.tif ../data/aster-Band3N.xml ../data/aster-Band3B.xml -o run/ba/run
+
+stereo -t aster ../data/aster-Band3N.tif ../data/aster-Band3B.tif ../data/aster-Band3N.xml ../data/aster-Band3B.xml run/run --left-image-crop-win 2523 3311 820 753 --right-image-crop-win 3186 4068 1082 1218 --bundle-adjust-prefix run/ba/run
 
 point2dem run/run-PC.tif
+
