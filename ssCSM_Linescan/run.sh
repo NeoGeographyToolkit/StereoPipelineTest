7275d77eb0a68bfcb7333389c5e68cc22c742b62
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-09-25 21:20:29 -0700

    Make some tests shorter

--- ssCSM_Linescan/run.sh
+++ ssCSM_Linescan/run.sh
@@ -3,6 +3,6 @@
 set -x verbose
 rm -rfv run
 
-stereo --left-image-crop-win 1346 9746 1034 1003 --right-image-crop-win 1903 9778 1162 1210 --corr-seed-mode 1 ../data/B17_016219_1978_XN_17N282W.8bit.cub ../data/B18_016575_1978_XN_17N282W.8bit.cub ../data/B17_016219_1978_XN_17N282W.8bit.json ../data/B18_016575_1978_XN_17N282W.8bit.json run/run 
+stereo --left-image-crop-win 2057 10449 364 328 --right-image-crop-win 2695 10691 485 358 --corr-seed-mode 1 ../data/B17_016219_1978_XN_17N282W.8bit.cub ../data/B18_016575_1978_XN_17N282W.8bit.cub ../data/B17_016219_1978_XN_17N282W.8bit.json ../data/B18_016575_1978_XN_17N282W.8bit.json run/run 
 point2dem -r mars run/run-PC.tif --nodata-value -32767
 
