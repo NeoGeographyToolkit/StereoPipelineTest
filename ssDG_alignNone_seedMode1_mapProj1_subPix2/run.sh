58da45b9777596f9ac8a60d9832634c60177a432
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-10-05 14:25:31 -0700

    Minor tweak to test

--- ssDG_alignNone_seedMode1_mapProj1_subPix2/run.sh
+++ ssDG_alignNone_seedMode1_mapProj1_subPix2/run.sh
@@ -4,8 +4,9 @@ set -x verbose
 rm -rfv run
 
 # Cut a little clip to speed things up
-gdal_translate -srcwin 0 0 256 512 ../data/left.tif left_clip.tif
+mkdir -p run
+gdal_translate -srcwin 0 0 256 512 ../data/left.tif run/left_clip.tif
 
-stereo left_clip.tif ../data/right.tif ../data/left.xml ../data/right.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif  --disable-fill-holes -s stereo.default -t dg --alignment-method none --corr-seed-mode 1 --subpixel-mode 2 --skip-image-normalization
+stereo run/left_clip.tif ../data/right.tif ../data/left.xml ../data/right.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif  --disable-fill-holes -s stereo.default -t dg --alignment-method none --corr-seed-mode 1 --subpixel-mode 2 --skip-image-normalization
 point2dem -r Earth run/run-PC.tif --nodata-value -32767
 
