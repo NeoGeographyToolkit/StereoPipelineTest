b1a780b272f7f0faf0f7eaebacdbd52ab86e39df
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-10-13 20:43:42 -0700

    Add testing logic for --auto-proj-center

--- ssISIS_alignHom_seedMode1_mapProj0_subPix1/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1/run.sh
@@ -11,6 +11,6 @@ if [ "$isMac" != "" ]; then
 fi
 
 stereo --enable-fill-holes ../data/M0100115.cub ../data/E0201461.cub run/run -s stereo.default --left-image-crop-win 0 1024 672 4864 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --filter-mode 2 --ip-detect-method 0 --save-left-right-disparity-difference
-point2dem run/run-PC.tif --nodata-value -32767 --errorimage --fsaa 4 --use-surface-sampling
+point2dem run/run-PC.tif --nodata-value -32767 --errorimage --fsaa 4 --use-surface-sampling --auto-proj-center --orthographic
 #point2dem --t_srs http://spatialreference.org/ref/iau2000/49900/  run/run-PC.tif --nodata-value -32767 --errorimage --fsaa 4 --use-surface-sampling
 
