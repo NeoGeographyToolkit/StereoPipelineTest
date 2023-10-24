49db616ee5f11d1177185529891aea298aefb976
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-10-24 10:06:38 -0700

    Tune-up the ASTER regression

--- ssASTER_Exact_alignAffEpp/run.sh
+++ ssASTER_Exact_alignAffEpp/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-bundle_adjust -t aster ../data/aster-Band3N.tif ../data/aster-Band3B.tif ../data/aster-Band3N.xml ../data/aster-Band3B.xml -o run/ba/run
+bundle_adjust -t aster ../data/aster-Band3N.tif ../data/aster-Band3B.tif ../data/aster-Band3N.xml ../data/aster-Band3B.xml -o run/ba/run --threads 1 --num-iterations 10
 
 stereo -t aster ../data/aster-Band3N.tif ../data/aster-Band3B.tif ../data/aster-Band3N.xml ../data/aster-Band3B.xml run/run --left-image-crop-win 2523 3311 820 753 --right-image-crop-win 3186 4068 1082 1218 --bundle-adjust-prefix run/ba/run
 
