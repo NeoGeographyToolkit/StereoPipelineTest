18b1540896bc3a099ee397ba3125ac85b48f77ec
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-11-29 13:08:52 -0800

    Reduce number of digits in diff

--- ss_bundle_adjust_heights_from_dem/validate.sh
+++ ss_bundle_adjust_heights_from_dem/validate.sh
@@ -16,7 +16,7 @@ for file in run/*tsai run/run-mapproj_match_offsets.txt; do
   fi
 
   echo diff $file $gold
-  diff=$(diff $file $gold)
+  diff=$(diff $file $gold | head -n 50)
 
   echo diff is $diff
   if [ "$diff" != "" ]; then
