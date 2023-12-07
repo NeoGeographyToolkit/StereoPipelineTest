09d9b00e7f04e2e38f3bcd0afeb66ee102bffcfb
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-12-07 14:28:20 -0800

    Add test for pdalinfo

--- ss_pc_align_utm/run.sh
+++ ss_pc_align_utm/run.sh
@@ -6,3 +6,7 @@ rm -rfv run
 pc_align ../data/13APR25_WV02_SEVZ_1030010021A8A100_10030010021A64500_DEM3-3m_10pct.tif ../data/Severnaya-Bedrock-UTM47-Ellipsoidal-Height.txt --csv-format "utm:47N 1:easting 2:northing 3:height_above_datum" --max-displacement 100 --save-transformed-source-points --save-inv-transformed-reference-points --max-num-reference-points 1000000 --max-num-source-points 100000
 #Severnaya_UTM47_llh.txt
 
+# Test for point2las
+point2las run/run-trans_reference.tif
+point2las gold/run-trans_reference.tif
+
--- ss_pc_align_utm/validate.sh
+++ ss_pc_align_utm/validate.sh
@@ -53,6 +53,17 @@ if [ "$diff" != "" ]; then
     exit 1
 fi
 
+# Add validation for pdal
+pdal info run/run-trans_reference.las  | grep -v filename | grep -v now > run/pdal_run.txt
+pdal info gold/run-trans_reference.las | grep -v filename |grep -v now > gold/pdal_gold.txt
+
+diff=$(diff run/pdal_run.txt gold/pdal_gold.txt | head -n 50)
+echo "diff is $diff"
+if [ "$diff" != "" ]; then
+    echo Validation failed
+    exit 1
+fi
+
 echo Validation succeded
 exit 0
 
