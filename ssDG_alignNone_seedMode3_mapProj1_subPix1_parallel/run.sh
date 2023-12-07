422dc3dd23ec5891219b0942ec1acf2ff4607192
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-12-07 15:59:42 -0800

    Add ASP_PYTHON_MODULES_PATH, needed for sparse_disp

--- ssDG_alignNone_seedMode3_mapProj1_subPix1_parallel/run.sh
+++ ssDG_alignNone_seedMode3_mapProj1_subPix1_parallel/run.sh
@@ -3,6 +3,9 @@
 set -x verbose
 rm -rfv run
 
+# Needed for sparse_disp
+export ASP_PYTHON_MODULES_PATH=$HOME/miniconda3/envs/sparse_disp/lib/python3.9/site-packages
+
 parallel_stereo --processes 8 --threads-multiprocess 1 --job-size-w 512 --job-size-h 1024 ../data/WV01_11JAN131652222-P1BS-10200100104A0300_ortho1.0m_sub2_crop1.tif ../data/WV01_11JAN131653180-P1BS-1020010011862E00_ortho1.0m_sub2_crop1.tif ../data/WV01_11JAN131652222-P1BS-10200100104A0300.xml ../data/WV01_11JAN131653180-P1BS-1020010011862E00.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif --left-image-crop-win 0 2048 1024 1024  --right-image-crop-win 0 2048 1200 1200 --corr-search -30 -30 30 30 --corr-max-levels 5 -s stereo.default --disable-fill-holes  -t dg --alignment-method none --corr-seed-mode 3 --subpixel-mode 1 --sparse-disp-options '--processes 19 --coarse 512 --fine 256 --output_dec_scale=8' --corr-tile-size 512
 point2dem  run/run-PC.tif --nodata-value -32767
 
