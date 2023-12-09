a94fece5af94f6525a48d883faa37d1244fc35f8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-12-09 13:57:32 -0800

    Make a run shorter

--- ss_bundle_adjust_optical_bar/run.sh
+++ ss_bundle_adjust_optical_bar/run.sh
@@ -4,5 +4,5 @@ set -x verbose
 rm -rfv run
 
 # Bundle adjustment with optical bar cameras. This has fake images and cameras, just enough to go through the motions
-bundle_adjust ../data/DS1105-2248DA079.tif ../data/DS1105-2248DA079_crop.tif ../data/DS1105-2248DA079.tsai ../data/DS1105-2248DA079_crop.tsai -o run/run --solve-intrinsics --num-iterations 3 --num-passes 1 --inline-adjustments --max-pairwise-matches 100 --thread 1
+bundle_adjust ../data/DS1105-2248DA079.tif ../data/DS1105-2248DA079_crop.tif ../data/DS1105-2248DA079.tsai ../data/DS1105-2248DA079_crop.tsai -o run/run --solve-intrinsics --num-iterations 3 --num-passes 1 --inline-adjustments --max-pairwise-matches 100 --threads 1 --ip-per-image 100
 
