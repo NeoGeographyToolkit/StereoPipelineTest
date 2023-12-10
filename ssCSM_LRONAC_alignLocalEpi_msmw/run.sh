4c9112cfb953f42abd16f04a13f94c5ac17b8883
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-12-10 09:23:43 -0800

    Make test shorter

--- ssCSM_LRONAC_alignLocalEpi_msmw/run.sh
+++ ssCSM_LRONAC_alignLocalEpi_msmw/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-parallel_stereo --left-image-crop-win 3583 3922 1767 1691 --right-image-crop-win 4095 4348 1498 1668 --corr-seed-mode 1 ../data/M181058717LE.ce.cub ../data/M181073012LE.ce.cub ../data/M181058717LE.ce.json ../data/M181073012LE.ce.json run/run --corr-tile-size 3072 --sgm-collar-size 256 --alignment-method local_epipolar --stereo-algorithm msmw --corr-timeout 10000
+parallel_stereo  --corr-tile-size 3072 --sgm-collar-size 256 --alignment-method local_epipolar --stereo-algorithm msmw --corr-timeout 10000 --left-image-crop-win 2710 4110 370 354 --right-image-crop-win 3039 4456 417 495 --corr-seed-mode 1 --compute-low-res-disparity-only ../data/M181058717LE.ce.cub ../data/M181073012LE.ce.cub ../data/M181058717LE.ce.json ../data/M181073012LE.ce.json run/run
 point2dem run/run-PC.tif
