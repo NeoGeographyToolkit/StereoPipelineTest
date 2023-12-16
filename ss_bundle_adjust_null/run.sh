c15bf26af0ac4f786aa77ee6e8518dc2e98d2481
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-12-15 17:43:51 -0800

    Add test for reading and writing isis cnet

--- ss_bundle_adjust_null/run.sh
+++ ss_bundle_adjust_null/run.sh
@@ -2,7 +2,15 @@
 
 set -x verbose
 rm -rfv run
+mkdir -p run
 
-echo ../data/left_sub16.tif ../data/right_sub16.tif > list.txt
-bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.null.tsai ../data/right_sub16.null.tsai ../data/gcp_sub16.gcp --inline-adjustments -t pinhole --datum WGS84 -o run/run --intrinsics-to-float "focal_length other_intrinsics" --overlap-list list.txt --threads 1
+# Test --overlap-list
+echo ../data/left_sub16.tif ../data/right_sub16.tif > run/list.txt
+bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.null.tsai ../data/right_sub16.null.tsai ../data/gcp_sub16.gcp --inline-adjustments -t pinhole --datum WGS84 -o run/run --intrinsics-to-float "focal_length other_intrinsics" --overlap-list run/list.txt --threads 1
+
+# Test writing an isis cnet
+bundle_adjust --threads 1 ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.null.tsai ../data/right_sub16.null.tsai -t pinhole --datum WGS84 -o run/run --output-cnet-type isis-cnet
+
+# Test reading it back
+bundle_adjust --threads 1 ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.null.tsai ../data/right_sub16.null.tsai -t pinhole --datum WGS84 -o run/run --output-cnet-type match-files --isis-cnet run/run.net
 
--- ss_bundle_adjust_null/validate.sh
+++ ss_bundle_adjust_null/validate.sh
@@ -26,5 +26,25 @@ for file in run/*tsai; do
 
 done
 
+# Test that the cnet got produced.
+# This file may contain a creation time, so 
+# its actual content will change just because of that.
+for file in run/run.net; do 
+ gold=${file/run\/run/gold\/run}
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+  echo File $file exists
+
+done
+
 echo Validation succeded
 exit 0
