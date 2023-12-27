9b101687aa491c29533694be8d3d2d73ef97efa2
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-12-27 15:55:29 -0800

    Add test for cub with embedded csm

--- ss_cam_test/run.sh
+++ ss_cam_test/run.sh
@@ -5,7 +5,6 @@ rm -rfv run
 
 mkdir run
 
-f=../data/B17_016219_1978_XN_17N282W.8bit.cub
-g=${f/.cub/.json}
-cam_test --sample-rate 500 --image $f --cam1 $f --cam2 $g | grep -E "Min|Max|Median" > run/run-out.txt
+# The second cub file has a CSM camera model state emebedded in it
 
+cam_test --sample-rate 500 --image ../data/B17_016219_1978_XN_17N282W.8bit.cub --cam1 ../data/B17_016219_1978_XN_17N282W.8bit.cub --cam2 ../data/B17_016219_1978_XN_17N282W.csm.cub --session1 isis --session2 csm | grep -E "Min|Max|Median" > run/run-out.txt
