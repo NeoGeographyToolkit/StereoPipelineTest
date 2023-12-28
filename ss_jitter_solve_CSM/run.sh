17ce53964101f2248638e4804a458bfb7edd8767
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-12-27 20:50:24 -0800

    Add ss_jitter_solve_CSM, test jitter and updating cubes

---
+++ ss_jitter_solve_CSM/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+# First find matches with bundle_adjust
+bundle_adjust --threads 1 ../data/B17_016219_1978_XN_17N282W.8bit.cub ../data/B18_016575_1978_XN_17N282W.8bit.cub ../data/B17_016219_1978_XN_17N282W.8bit.json ../data/B18_016575_1978_XN_17N282W.8bit.json -o run/run
+
+# Solve for jitter. Modify the .cub files
+/bin/cp -fv ../data/B17_016219_1978_XN_17N282W.8bit.cub run
+/bin/cp -fv ../data/B18_016575_1978_XN_17N282W.8bit.cub run
+jitter_solve --threads 1 run/B17_016219_1978_XN_17N282W.8bit.cub run/B18_016575_1978_XN_17N282W.8bit.cub run/run-B17_016219_1978_XN_17N282W.8bit.adjusted_state.json run/run-B18_016575_1978_XN_17N282W.8bit.adjusted_state.json -o run/jitter --match-files-prefix run/run --num-iterations 5 --update-isis-cubes-with-csm-state
+
