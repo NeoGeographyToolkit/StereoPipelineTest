da1e5b642a385fc940353e9518274062739dfb7f
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-12-27 20:51:07 -0800

    Split up jitter solving from bundle_adjust testcase

--- ss_bundle_adjust_CSM_Linescan/run.sh
+++ ss_bundle_adjust_CSM_Linescan/run.sh
@@ -11,6 +11,3 @@ bundle_adjust --threads 1 ../data/B17_016219_1978_XN_17N282W.8bit.cub ../data/B1
 # Re-run bundle adjustment with the model state files saved by the previous run
 bundle_adjust --threads 1 ../data/B17_016219_1978_XN_17N282W.8bit.cub ../data/B18_016575_1978_XN_17N282W.8bit.cub run/run-B17_016219_1978_XN_17N282W.8bit.adjusted_state.json run/run-B18_016575_1978_XN_17N282W.8bit.adjusted_state.json -o run/run --force-reuse-match-files
 
-# Solve for jitter. This needs the match files found in the first bundle_adjust call.
-jitter_solve --threads 1 ../data/B17_016219_1978_XN_17N282W.8bit.cub ../data/B18_016575_1978_XN_17N282W.8bit.cub run/run-B17_016219_1978_XN_17N282W.8bit.adjusted_state.json run/run-B18_016575_1978_XN_17N282W.8bit.adjusted_state.json -o run/jitter --match-files-prefix run/run --num-iterations 5
-
