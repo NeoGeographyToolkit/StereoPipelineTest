1d47b1cf812a1c77b8f4dfddeff54797f1d5730b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-01-20 20:11:36 -0800

    Rm empty lines

--- ss_dg_mosaic_rpc_gen/run.sh
+++ ss_dg_mosaic_rpc_gen/run.sh
@@ -5,6 +5,3 @@ rm -rfv run
 
 dg_mosaic --verbose ../data/WV01_11JAN131652222-P1BS-10200100104A0300.tif ../data/WV01_11JAN131652231-P1BS-10200100104A0300.tif --rpc-penalty-weight 0.5 --reduce-percent 33 --skip-tif-gen --output-prefix run/run --verbose
 
-
-
-
