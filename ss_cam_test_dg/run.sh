b79dd95b5e692e9017bd7ae2b49eeadb53e96fe8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-01-22 14:02:38 -0800

    Update tests to remove --dg-use-csm

--- ss_cam_test_dg/run.sh
+++ ss_cam_test_dg/run.sh
@@ -5,5 +5,5 @@ rm -rfv run
 
 mkdir run
 
-cam_test --dg-use-csm --session1 dg --session2 dg --image ../data/WV01_11JAN131652275-P1BS-10200100104A0300.tif --cam1 ../data/WV01_11JAN131652275-P1BS-10200100104A0300.xml --cam2 ../data/WV01_11JAN131652275-P1BS-10200100104A0300.xml | grep -E "Min|Max|Median" > run/run-out.txt
+cam_test --session1 dg --session2 dg --image ../data/WV01_11JAN131652275-P1BS-10200100104A0300.tif --cam1 ../data/WV01_11JAN131652275-P1BS-10200100104A0300.xml --cam2 ../data/WV01_11JAN131652275-P1BS-10200100104A0300.xml | grep -E "Min|Max|Median" > run/run-out.txt
 
