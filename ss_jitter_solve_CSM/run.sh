0e274f8887bcecab68da6806d10d6c01064202d0
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-01-24 19:16:25 -0800

    Format

--- ss_jitter_solve_CSM/run.sh
+++ ss_jitter_solve_CSM/run.sh
@@ -10,14 +10,14 @@ bundle_adjust --threads 1 ../data/B17_016219_1978_XN_17N282W.8bit.cub ../data/B1
 /bin/cp -fv ../data/B17_016219_1978_XN_17N282W.8bit.cub run
 /bin/cp -fv ../data/B18_016575_1978_XN_17N282W.8bit.cub run
 
-jitter_solve --threads 1                        \
-   run/B17_016219_1978_XN_17N282W.8bit.cub      \
-	 run/B18_016575_1978_XN_17N282W.8bit.cub      \
-	 ../data/B17_016219_1978_XN_17N282W.8bit.json \
-	 ../data/B18_016575_1978_XN_17N282W.8bit.json \
-	 --input-adjustments-prefix run/run           \
-	 --match-files-prefix run/run                 \
-	 --num-iterations 5                           \
-	 --update-isis-cubes-with-csm-state           \
-	 -o run/jitter
+jitter_solve --threads 1                       \
+  run/B17_016219_1978_XN_17N282W.8bit.cub      \
+  run/B18_016575_1978_XN_17N282W.8bit.cub      \
+  ../data/B17_016219_1978_XN_17N282W.8bit.json \
+  ../data/B18_016575_1978_XN_17N282W.8bit.json \
+  --input-adjustments-prefix run/run           \
+  --match-files-prefix run/run                 \
+  --num-iterations 5                           \
+  --update-isis-cubes-with-csm-state           \
+  -o run/jitter
 
