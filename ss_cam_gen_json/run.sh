cb1eff90285f9df4d46003de02c070486f7667fb
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-01-31 20:38:38 -0800

    Fix test

--- ss_cam_gen_json/run.sh
+++ ss_cam_gen_json/run.sh
@@ -5,4 +5,4 @@ rm -rfv run
 
 # Run cam_gen with a Pinhole file in json format provided by Planet.
 
-cam_gen ../data/1259344352.36984587_sc00104_c2_PAN_i0000000001.tif --input-camera ../data//1259344352.36984587_sc00104_c2_PAN_i0000000001_pinhole.json -o run/run.tsai
+cam_gen ../data/1259344352.36984587_sc00104_c2_PAN_i0000000001.tif --input-camera ../data/1259344352.36984587_sc00104_c2_PAN_i0000000001_pinhole.json -o run/run.tsai
