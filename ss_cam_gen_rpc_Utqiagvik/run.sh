d4a54d82d2638983bfd508be09f813bd52890608
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-02-01 19:53:22 -0800

    Update ss_cam_gen_rpc_Utqiagvik

--- ss_cam_gen_rpc_Utqiagvik/run.sh
+++ ss_cam_gen_rpc_Utqiagvik/run.sh
@@ -3,6 +3,5 @@
 set -x verbose
 rm -rfv run
 
-# Find the best fit to a given RPC camera and save as CSM
-cam_gen ../data/BSG-101-20220429-230006-23138454_georeferenced-pan.tif --reference-dem ../data/Utqiagvik_dem.tif --focal-length 463763.636364 --optical-center 3300 2200 --pixel-pitch 1 --input-camera ../data/BSG-101-20220429-230006-23138454_georeferenced-pan.tif --refine-camera -o run/run.json
-
+# Find the best fit to a given RPC camera and save as CSM. Refine the distortion and focal length. 
+cam_gen ../data/BSG-101-20220429-230006-23138454_georeferenced-pan.tif --reference-dem ../data/Utqiagvik_dem.tif --focal-length 463763.636364 --optical-center 3300 2200 --pixel-pitch 1 --input-camera ../data/BSG-101-20220429-230006-23138454_georeferenced-pan.tif --refine-camera -o run/run.json --refine-intrinsics focal_length,other_intrinsics --datum WGS84
