79fd18964e01fcbb6666be7d91aa7143b8428ae2
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-02-09 10:16:38 -0800

    Add test for point2dem with utm

--- ssRPC_alignHom_seedMode1_mapProj0_subPix3_BasaltHills/run.sh
+++ ssRPC_alignHom_seedMode1_mapProj0_subPix3_BasaltHills/run.sh
@@ -5,5 +5,7 @@ rm -rfv run
 
 stereo ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.xml ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.xml run/run --left-image-crop-win 1024 1024 2048 2048 --corr-max-levels 5 -t rpc -s stereo.default --alignment-method homography --subpixel-mode 3 --disable-fill-holes --subpixel-h-kernel 19 --subpixel-v-kernel 19 --corr-seed-mode 1 --bundle-adjust-prefix ../data/rpc_ba/run --threads 3 --propagate-errors
 
-point2dem -r Earth run/run-PC.tif --nodata-value -32767 --remove-outliers --propagate-errors
+# Test error propagation and using an UTM projection
+srs='+proj=utm +zone=10 +datum=WGS84 +units=m +no_defs'
+point2dem -r Earth --t_srs "$srs" run/run-PC.tif --nodata-value -32767 --remove-outliers --propagate-errors
 
