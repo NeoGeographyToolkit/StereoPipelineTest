46700b0787bdc604ca80ed459be7ab4281675588
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-02-09 10:18:19 -0800

    Keep aux files in run directory

--- ss_mapproject_dg_unitDegree/run.sh
+++ ss_mapproject_dg_unitDegree/run.sh
@@ -3,8 +3,8 @@
 set -x verbose
 rm -rfv run
 
-# Localhost does not work. Use actual machine name.
-uname -n > machines.txt
+mkdir -p run
+echo localhost > run/machines.txt
 
-mapproject --mpp 10 ../data/ref-DEM-unitDeg.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.xml run/run-DG.tif -t dg --tile-size 512 --nodes-list machines.txt
+mapproject --mpp 10 ../data/ref-DEM-unitDeg.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.xml run/run-DG.tif -t dg --tile-size 512 --nodes-list run/machines.txt
 
