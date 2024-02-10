da59dd57d808e943862673d1514af3f3fd2d6da9
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-02-10 14:02:44 -0800

    Tune up ss_image_align validation script

--- ss_image_align/validate.sh
+++ ss_image_align/validate.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-for file in run/run-align.tif run/run-transform.txt run/run-corr-align.tif; do
-    
-    gold=${file/run\/run/gold\/run}
+for file in run/run-align.tif run/run-corr-align.tif; do
 
-    if [ ! -e "$file" ]; then
-        echo "ERROR: File $file does not exist."
-        exit 1;
-    fi
-    
-    if [ ! -e "$gold" ]; then
-        echo "ERROR: File $gold does not exist."
-        exit 1;
-    fi
-    
-    cmp $file $gold
-    ans="$?"
-    
-    if [ "$ans" != "0" ]; then
-        echo Validation failed
-        exit 1
-    fi
+  echo $file $gold
+  gold=${file/run\/run/gold\/run}
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+  # Remove cached xmls
+  rm -fv "$file.aux.xml"
+  rm -fv "$gold.aux.xml"
+
+  cmp_stats.sh $file $gold
+  gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v xml > run/run.txt
+  gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v xml > gold/gold.txt
+
+  diff run/run.txt gold/gold.txt
+
+  ../bin/max_err.pl run/run.txt gold/gold.txt # print the error
+  ans=$(../bin/max_err.pl run/run.txt gold/gold.txt 1e-9) # compare the error
+  if [ "$ans" -eq 0 ]; then
+      echo Validation failed
+      exit 1
+  fi
+
 done
 
 echo Validation succeded
