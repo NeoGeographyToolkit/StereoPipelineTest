e0cae7b59f331e904c806cbfb56d555d37a49b6c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-02-15 09:54:21 -0800

    Fix two tests by ensuring temporary files go to run dir

--- ssISIS_alignHom_seedMode1_mapProj0_subPix1_parallel/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1_parallel/run.sh
@@ -3,9 +3,10 @@
 set -x verbose
 rm -rfv run
 
-uname -n >  machines.txt
+mkdir -p run
+uname -n >  run/machines.txt
 
-parallel_stereo --enable-fill-holes --processes 2 --threads-multiprocess 8 --job-size-w 512 --job-size-h 512 --nodes-list machines.txt  ../data/M0100115_small.cub ../data/E0201461_small.cub run/run -s stereo.default  --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --corr-tile-size 512 --save-left-right-disparity-difference
+parallel_stereo --enable-fill-holes --processes 2 --threads-multiprocess 8 --job-size-w 512 --job-size-h 512 --nodes-list run/machines.txt  ../data/M0100115_small.cub ../data/E0201461_small.cub run/run -s stereo.default  --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --corr-tile-size 512 --save-left-right-disparity-difference
 
 # Test the situation when the proj4 string is specified as input with the datum name set separately
 point2dem --t_srs '+proj=longlat +a=3396190 +b=3396190 +no_defs '   -r mars run/run-PC.tif --nodata-value -32767
--- ssPinHole_alignHom_seedMode1_mapProj0_subPix1_parallel1/run.sh
+++ ssPinHole_alignHom_seedMode1_mapProj0_subPix1_parallel1/run.sh
@@ -3,7 +3,9 @@
 set -x verbose
 rm -rfv run
 
-uname -n >  machines.txt
+mkdir -p run
+uname -n >  run/machines.txt
 
-parallel_stereo --processes 2 --threads-multiprocess 8 --job-size-w 1536 --job-size-h 1024 --corr-timeout 600 --nodes-list machines.txt ../data/1n270487304eff90cip1952l0m1.tif ../data/1n270487304eff90cip1952r0m1.tif ../data/1n270487304eff90cip1952l0m1.cahvor ../data/1n270487304eff90cip1952r0m1.cahvor run/run -s stereo.default  --left-image-crop-win 10 15 605 450 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1
+parallel_stereo --processes 2 --threads-multiprocess 8 --job-size-w 1536 --job-size-h 1024 --corr-timeout 600 --nodes-list run/machines.txt ../data/1n270487304eff90cip1952l0m1.tif ../data/1n270487304eff90cip1952r0m1.tif ../data/1n270487304eff90cip1952l0m1.cahvor ../data/1n270487304eff90cip1952r0m1.cahvor run/run -s stereo.default  --left-image-crop-win 10 15 605 450 --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1
 point2dem --nodata-value -32767 --semi-major-axis 1 --semi-minor-axis 1 run/run-PC.tif
+
