577a2cb92d1da161cc96aa49470b30788ee30dc7
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-02-14 16:23:22 -0800

    Update a validation file

--- ss_pc_align_point_to_plane/validate.sh
+++ ss_pc_align_point_to_plane/validate.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-for file in run/run-trans_reference.tif run/run-trans_source.tif; do
-	gold=${file/run\/run/gold\/run}
+for file in run/run-trans_reference.tif run/run-trans_source.tif; do 
 
-	if [ ! -e "$file" ]; then
-    	echo "ERROR: File $file does not exist."
-	    exit 1;
-	fi
+    gold=${file/run\/gold\/}
+    echo $file $gold
 
-	if [ ! -e "$gold" ]; then
-	    echo "ERROR: File $gold does not exist."
-	    exit 1;
-	fi
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
 
-	diff=$(cmp $file $gold | head -n 50)
-	ans=$?
-	echo "diff is $diff"
-	echo "return flag is $ans"
-	if [ "$diff" != "" ] || [ "$ans" -ne 0 ]; then
-	    echo Validation failed
-	    exit 1
-	fi
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
+
+    # Remove cached xmls
+    rm -fv "$file.aux.xml"
+    rm -fv "$gold.aux.xml"
+
+    cmp_stats.sh $file $gold
+    gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -v Min= > run/run.txt
+    gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -v Min= > gold/run.txt
+
+    diff=$(diff run/run.txt gold/run.txt | head -n 50)
+
+    rm -f run/run.txt gold/run.txt
+
+    echo diff is $diff
+    if [ "$diff" != "" ]; then
+        echo Validation failed
+        exit 1
+    fi
 done
 
 echo Validation succeded
