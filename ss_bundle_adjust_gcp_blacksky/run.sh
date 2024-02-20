8df444700cec2556ecfbb06ef560bab9c7734803
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-02-20 08:47:45 -0800

    Update ss_bundle_adjust_gcp_blacksky to use a datum

--- ss_bundle_adjust_gcp_blacksky/run.sh
+++ ss_bundle_adjust_gcp_blacksky/run.sh
@@ -5,4 +5,5 @@ rm -rfv run
 
 # Bundle adjustment to purely initialize a camera with GCP
 # This is a testcase for which a bug was fixed
-bundle_adjust --inline-adjustments ../data/BSG-118-20220730-123737-33360052_georeferenced-pan.tif ../data/BSG-118-20220730-123737-33360052_georeferenced-pan.tsai ../data/BSG-118-20220730-123737-33360052_georeferenced-pan.gcp -o run/run
+bundle_adjust --inline-adjustments ../data/BSG-118-20220730-123737-33360052_georeferenced-pan.tif ../data/BSG-118-20220730-123737-33360052_georeferenced-pan.tsai ../data/BSG-118-20220730-123737-33360052_georeferenced-pan.gcp -o run/run --datum WGS84
+
