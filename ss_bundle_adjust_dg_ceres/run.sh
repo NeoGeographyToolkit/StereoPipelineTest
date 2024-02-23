46e9aa1e992c8e15bcc68951f487b969800cf7cd
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-02-23 14:50:57 -0800

    Update testcase

--- ss_bundle_adjust_dg_ceres/run.sh
+++ ss_bundle_adjust_dg_ceres/run.sh
@@ -9,5 +9,5 @@ echo "0 1 0 0" >> run/transform.txt
 echo "0 0 1 0" >> run/transform.txt
 echo "0 0 0 0" >> run/transform.txt
 
-bundle_adjust ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.xml ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.xml -o run/run --ip-detect-method 1 --initial-transform run/transform.txt --camera-weight 0.1 --rotation-weight 0.1 --translation-weight 0.1 --epipolar-threshold 150  --min-matches 15 --ip-per-tile 500 --threads 1
+bundle_adjust ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.tif ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.tif ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.xml ../data/WV01_11JAN131653225-P1BS-1020010011862E00.r12.xml -o run/run --ip-detect-method 1 --initial-transform run/transform.txt --tri-weight 0.1 --epipolar-threshold 150  --min-matches 15 --ip-per-tile 500 --threads 1
 
