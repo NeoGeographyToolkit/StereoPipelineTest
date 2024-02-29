6edeffde42c9e18e24725d8401a1c75899fbb9f9
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-02-29 09:12:31 -0800

    Wipe spaces

--- ss_bundle_adjust_isis_robust_sparse/run.sh
+++ ss_bundle_adjust_isis_robust_sparse/run.sh
@@ -5,4 +5,3 @@ rm -rfv run
 
 bundle_adjust ../data/AS15-M-1134.lev1.cub ../data/AS15-M-1135.lev1.cub --cost-function cauchy --robust-threshold 0.5 --max-iterations 100 -o run/run --threads 1 --camera-weight 100
 
-
