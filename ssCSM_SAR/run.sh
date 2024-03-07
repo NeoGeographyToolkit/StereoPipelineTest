7e75298d7e20f212a3785614b0a8199287aa40d5
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-03-07 09:35:04 -0800

    Tune one test

--- ssCSM_SAR/run.sh
+++ ssCSM_SAR/run.sh
@@ -17,7 +17,7 @@ mapproject run/run-DEM-blur.tif ../data/lsz_03821_1cd_xku_16n196_v1_clamp_crop.t
 
 mapproject run/run-DEM-blur.tif ../data/lsz_03822_1cd_xku_23n196_v1_clamp_crop.tif ../data/lsz_03822_1cd_xku_23n196_v1.json run/lsz_03822_1cd_xku_23n196_v1.map.tif --t_projwin 196.277 22.247 196.611 21.847 --tr 0.00037
 
-bundle_adjust ../data/lsz_03821_1cd_xku_16n196_v1_clamp_crop.tif ../data/lsz_03822_1cd_xku_23n196_v1_clamp_crop.tif ../data/lsz_03821_1cd_xku_16n196_v1.json ../data/lsz_03822_1cd_xku_23n196_v1.json --mapprojected-data "run/lsz_03821_1cd_xku_16n196_v1.map.tif run/lsz_03822_1cd_xku_23n196_v1.map.tif run/run-DEM-blur.tif" -o run/ba/run --ip-per-tile 60000 --matches-per-tile 60000 --threads 1 --min-matches 5 --remove-outliers-params '3 75 100 100' --num-iterations 10
+bundle_adjust ../data/lsz_03821_1cd_xku_16n196_v1_clamp_crop.tif ../data/lsz_03822_1cd_xku_23n196_v1_clamp_crop.tif ../data/lsz_03821_1cd_xku_16n196_v1.json ../data/lsz_03822_1cd_xku_23n196_v1.json --mapprojected-data "run/lsz_03821_1cd_xku_16n196_v1.map.tif run/lsz_03822_1cd_xku_23n196_v1.map.tif run/run-DEM-blur.tif" -o run/ba/run --ip-per-tile 60000 --matches-per-tile 60000 --threads 1 --min-matches 5 --remove-outliers-params '3 75 10000 10000' --num-iterations 10
 
 # Second bundle adjustment, to see if we can load back the state.
 # This run does not like the previously created match points much. Need to figure out why.
