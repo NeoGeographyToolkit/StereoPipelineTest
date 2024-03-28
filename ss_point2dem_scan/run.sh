0da9dc3f917ea532f3cd87e44b2b201f928d3541
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-03-27 19:40:35 -0700

    Add test for reading scanned points

---
+++ ss_point2dem_scan/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+# Grid a dataset having x, y, z values in a small ground-level box, as obtained from a 3D scanner
+proj="+proj=stere +lat_0=0 +lat_ts=0 +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=1737400 +b=1737400 +units=m +no_defs"
+point2dem --t_srs "$proj" --tr 0.01 --input-is-projected ../data/scan.tif -o run/run
+
