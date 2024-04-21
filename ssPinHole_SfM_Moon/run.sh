c22504565f196265d6221c815489ef3233ebb0c0
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-04-20 20:15:09 -0700

    Add SfM example for the moon with gcp and stereo

---
+++ ssPinHole_SfM_Moon/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+camera_solve run/ ../data/AS15-M-0114_MED.png ../data/AS15-M-0115_MED.png --datum D_MOON --calib-file ../data/AS15_calib.tsai --gcp-file ../data/AS15_gcp.gcp
+
+parallel_stereo -t pinhole --stereo-algorithm libelas --alignment-method local_epipolar --left-image-crop-win 1793 1802 801 836 --right-image-crop-win 2220 1708 1058 953 --corr-seed-mode 1 --sgm-collar-size 128 --threads 16 ../data/AS15-M-0114_MED.png ../data/AS15-M-0115_MED.png run/AS15-M-0114_MED.png.final.tsai run/AS15-M-0115_MED.png.final.tsai run/run
+
+point2dem --stereographic --auto-proj-center run/run-PC.tif
+
---
+++ ssPinHole_SfM_Moon/validate.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in run/run-DEM.tif; do
+
+    gold=${file/run\//gold\/};
+    echo $file $gold
+    
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
+    
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
+    
+    # Remove cached xmls
+    rm -fv "$file.aux.xml"
+    rm -fv "$gold.aux.xml"
+    
+    cmp_stats.sh $file $gold
+    gdalinfo -stats $file | grep -v Files | grep -v -i tif > run/run.txt
+    gdalinfo -stats $gold | grep -v Files | grep -v -i tif > run/gold.txt
+
+    # Do not make the error small, because Theia is not deterministic
+    max_err.pl run/run.txt run/gold.txt # print the error
+    ans=$(max_err.pl run.txt gold.txt 1e-2) # compare the error
+    if [ "$ans" != "1" ]; then
+        echo Validation failed
+        exit 1
+    fi
+
+done
+
+echo Validation succeded
+exit 0
