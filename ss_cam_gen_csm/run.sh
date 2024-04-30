bfefeba797eb481b2e2531efc2bc03f9b11bad85
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-04-30 08:35:31 -0700

    Tune up test

--- ss_cam_gen_csm/run.sh
+++ ss_cam_gen_csm/run.sh
@@ -7,5 +7,6 @@ rm -rfv run
 
 cam_gen ../data/1259344352.36984587_sc00104_c2_PAN_i0000000001.tif --input-camera ../data/skysat_radtan_dist.tsai -o run/run.json
 
-cam_test --image ../data/1259344352.36984587_sc00104_c2_PAN_i0000000001.tif --cam1 ../data/skysat_radtan_dist.tsai --cam2 run/run.json > run/run.txt
+cam_test --image ../data/1259344352.36984587_sc00104_c2_PAN_i0000000001.tif --cam1 ../data/skysat_radtan_dist.tsai --cam2 run/run.json \
+ | grep -v -i elapsed > run/run.txt
 
