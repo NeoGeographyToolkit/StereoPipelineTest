f8b8399eeafad2975e5cdbe9ef1151f3339c41a8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-05-30 08:56:02 -0700

    Fix tests

--- ss_sat_sim_linescan/run.sh
+++ ss_sat_sim_linescan/run.sh
@@ -4,7 +4,7 @@ set -x verbose
 rm -rfv run
 
 # sat sim with a linescan rig
-sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o run/run --first 450 1000 10000 --last 450 1300 10000 --num 5 --first-ground-pos 450 1000 --last-ground-pos 450 1300 --roll 0 --pitch 40 --yaw 0 --sensor-type linescan --velocity 7000 --horizontal-uncertainty '0 0 0' --rig ../data/sim_rig_linescan.txt
+sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o run/run --first 450 1000 10000 --last 450 1300 10000 --num 5 --first-ground-pos 450 1000 --last-ground-pos 450 1300 --roll 0 --pitch 40 --yaw 0 --sensor-type linescan --velocity 7000 --horizontal-uncertainty '0 0 0' --rig-config ../data/sim_rig_linescan.txt
 
 # mapproject each image and camera
 for c in c1 c2 c3; do
