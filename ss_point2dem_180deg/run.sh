6a1f4ebd560623347f08cfc1e1557345de33babc
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-06-13 13:36:12 -0700

    Set -r earth in test

--- ss_point2dem_180deg/run.sh
+++ ss_point2dem_180deg/run.sh
@@ -9,7 +9,7 @@ point2dem --t_srs "+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=
 
 point2dem --t_srs "+proj=utm +zone=10 +datum=NAD83 +units=m +no_defs" --csv-format '1:lon 2:lat 3:height_above_datum' --tr 1000 ../data/cloud_180deg.csv -o run/run-utm
 
-point2dem --csv-format 1:lon,2:lat,3:height_above_datum ../data/cloud_180deg.csv -o run/run-lonlat --tr 2
+point2dem -r earth --csv-format 1:lon,2:lat,3:height_above_datum ../data/cloud_180deg.csv -o run/run-lonlat --tr 2
 
 # The EPSG:6318 / "NAD83(2011)" geographic CRS as a GeoJSON string
 srs='
