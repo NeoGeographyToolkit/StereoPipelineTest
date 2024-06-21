21b4191bcde789900e59aac70a3d4f819c561c64
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-06-21 10:48:05 -0700

    Tune up ss_bundle_adjust_brownconrady to run faster

--- ss_bundle_adjust_brownconrady/run.sh
+++ ss_bundle_adjust_brownconrady/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-bundle_adjust --transform-cameras-with-shared-gcp --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai ../data/gcp_sub16.gcp --inline-adjustments -t pinhole --datum WGS84 -o run/run  --intrinsics-to-float "other_intrinsics" --threads 1
+bundle_adjust --transform-cameras-with-shared-gcp --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai ../data/gcp_sub16.gcp --inline-adjustments -t pinhole --datum WGS84 -o run/run  --intrinsics-to-float "other_intrinsics" --num-iterations 10 --num-passes 1 --threads 1
 
