b39e7cfb70ae9f731113fbbc46dc35f547800626
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-06-20 19:24:31 -0700

    Add test for querying pixel

--- ss_mapproject_embed_rpc/run.sh
+++ ss_mapproject_embed_rpc/run.sh
@@ -3,4 +3,9 @@
 set -x verbose
 rm -rfv run
 
+# Test the --query-pixel option
+mkdir -p run 
+mapproject ../data/Copernicus_DSM.tif ../data/basic_panchromatic.tif run/run-RPC.tif --query-pixel 677 333 |tee run/run-query.txt
+
+# Test mapprojection with embeded camera
 mapproject ../data/Copernicus_DSM.tif ../data/basic_panchromatic.tif run/run-RPC.tif
--- ss_mapproject_embed_rpc/validate.sh
+++ ss_mapproject_embed_rpc/validate.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
+# Compare the query results
+file=run/run-query.txt
+gold=gold/run-query.txt
+
+echo Comparing run/run-query.txt gold/run-query.txt
+ans=$(../bin/max_err.pl run/run-query.txt gold/run-query.txt 1e-9)
+if [ "$ans" != "1" ]; then
+     echo Validation failed
+     exit 1
+fi
+echo Files agree
+
+# Compare the tif files
 file=run/run-RPC.tif
 gold=gold/run-RPC.tif
+echo Comparing $file $gold
 
 if [ ! -e "$file" ]; then
     echo "ERROR: File $file does not exist."
@@ -19,21 +33,19 @@ rm -fv "$file.aux.xml"
 rm -fv "$gold.aux.xml"
 
 cmp_stats.sh $file $gold
-gdalinfo -stats $file | grep -v Files | grep -v -i tif > run.txt
-gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold.txt
+gdalinfo -stats $file | grep -v Files | grep -v -i tif > run/run.txt
+gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold/run.txt
 
-diff=$(diff run.txt gold.txt | head -n 50)
-cat run.txt
+diff=$(diff run/run.txt goldrun/.txt | head -n 50)
+cat run/run.txt
 echo Diff is $diff
 
-../bin/max_err.pl run.txt gold.txt
-ans=$(../bin/max_err.pl run.txt gold.txt 1e-9)
+../bin/max_err.pl run/run.txt gold/run.txt
+ans=$(../bin/max_err.pl run/run.txt gold/run.txt 1e-9)
 if [ "$ans" -eq 0 ]; then
      echo Validation failed
      exit 1
 fi
-
-rm -f run.txt gold.txt
 
 echo Validation succeded
 exit 0
