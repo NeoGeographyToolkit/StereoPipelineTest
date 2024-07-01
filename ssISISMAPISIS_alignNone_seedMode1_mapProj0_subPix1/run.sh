db46d0717162cfbcc04b943f3475f37c5460f23e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-07-01 15:35:05 -0700

    Tune-up a test

--- ssISISMAPISIS_alignNone_seedMode1_mapProj0_subPix1/run.sh
+++ ssISISMAPISIS_alignNone_seedMode1_mapProj0_subPix1/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo ../data/M0100115.tif ../data/E0201461.tif ../data/M0100115.cub ../data/E0201461.cub run/run ../data/moc-ref-DEM.tif --left-image-crop-win 141 1881 585 613 --right-image-crop-win 0 2036 578 642
+stereo ../data/M0100115.tif ../data/E0201461.tif ../data/M0100115.cub ../data/E0201461.cub run/run ../data/moc-ref-DEM.tif --left-image-crop-win 598 2990 485 531 --right-image-crop-win 30 3042 478 497
 
 point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage --fsaa 4 --use-surface-sampling
 
