361bc95d680349f39a6ac7872b8d98fb469aeb2c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-07-07 10:35:49 -0700

    Make a test faster

--- ssISIS_alignLocalEpi_seedMode1_mapProj0_subPix1_ASP_MGM/run.sh
+++ ssISIS_alignLocalEpi_seedMode1_mapProj0_subPix1_ASP_MGM/run.sh
@@ -3,7 +3,8 @@
 set -x verbose
 rm -rfv run
 
-parallel_stereo --left-image-crop-win 0 1024 672 4864 --alignment-method local_epipolar  --compute-error-vector --stereo-algorithm asp_mgm --corr-seed-mode 1 ../data/M0100115_crop.cub ../data/E0201461.cub run/run --corr-tile-size 512 --sgm-collar-size 256 --cost-mode 3 --corr-kernel 9 9
+# Run parallel stereo with two small tiles
+parallel_stereo --processes 4 --left-image-crop-win 139 1500 415 793 --right-image-crop-win 335 1239 680 1134 --job-size-h 600 --job-size-w 600 --sgm-collar-size 100 --alignment-method local_epipolar  --compute-error-vector --stereo-algorithm asp_mgm --corr-seed-mode 1 ../data/M0100115_crop.cub ../data/E0201461.cub run/run --cost-mode 3 --corr-kernel 9 9
 
 point2dem -r mars run/run-PC.tif --nodata-value -32767 --errorimage
 
