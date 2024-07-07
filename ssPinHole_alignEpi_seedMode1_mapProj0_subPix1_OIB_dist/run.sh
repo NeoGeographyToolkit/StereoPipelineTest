edbef3fff95b6e049c9fbbc5140e96edea9081e2
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-07-06 21:43:38 -0700

    Make the run faster

--- ssPinHole_alignEpi_seedMode1_mapProj0_subPix1_OIB_dist/run.sh
+++ ssPinHole_alignEpi_seedMode1_mapProj0_subPix1_OIB_dist/run.sh
@@ -3,12 +3,15 @@
 set -x verbose
 rm -rfv run
 
-parallel_stereo --alignment-method epipolar --xcorr-threshold -1 --corr-kernel 5 5 \
+parallel_stereo --alignment-method epipolar \
+    --xcorr-threshold -1 --corr-kernel 5 5  \
     --corr-tile-size 2048 --corr-memory-limit-mb 6096  \
     --cost-mode 4 --stereo-algorithm asp_mgm --threads-single 8 \
     --corr-seed-mode 1 --session-type pinhole \
 	--horizontal-stddev 1 1 \
 	--propagate-errors \
+	--left-image-crop-win 0 0 2000 2000 \
+	--right-image-crop-win 0 0 2000 2000 \
     ../data/img_icebridge2.tif ../data/img_icebridge3.tif \
     ../data/img_icebridge2.tsai ../data/img_icebridge3.tsai \
     run/run
