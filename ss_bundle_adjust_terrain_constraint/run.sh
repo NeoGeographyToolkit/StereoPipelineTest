59d9844f5ec1dfe7b33b4a6c5250daadea3bbaad
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-07-17 15:31:39 -0700

    Tweak ss_bundle_adjust_terrain_constraint to use a match file as is

--- ss_bundle_adjust_terrain_constraint/run.sh
+++ ss_bundle_adjust_terrain_constraint/run.sh
@@ -11,9 +11,6 @@ stereo ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownco
 # Create the reference terrain
 point2dem run/run-PC.tif
 
-# Copy the match file to the new name
-mv -fv run/run-disp-left_sub16__right_sub16.match run/run-left_sub16__right_sub16.match
-
 # Finally run bundle adjustment
-bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run --intrinsics-to-float other_intrinsics --disparity-list run/run-left_sub16__right_sub16-unaligned-D.tif --max-disp-error 10 --max-iterations 5 --parameter-tolerance 1e-12 --reference-terrain run/run-DEM.tif --max-num-reference-points 1000 --threads 1 --max-pairwise-matches 10000
+bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run --intrinsics-to-float other_intrinsics --disparity-list run/run-left_sub16__right_sub16-unaligned-D.tif --max-disp-error 10 --max-iterations 5 --parameter-tolerance 1e-12 --reference-terrain run/run-DEM.tif --max-num-reference-points 1000 --threads 1 --max-pairwise-matches 10000 --match-files-prefix run/run-disp
 
