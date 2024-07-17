25f5c29e4e45b3926b2730191586faa6ea615298
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-07-17 14:58:02 -0700

    Tune run to use --num-matches-from-disparity

--- ss_bundle_adjust_heights_from_dem/run.sh
+++ ss_bundle_adjust_heights_from_dem/run.sh
@@ -6,13 +6,10 @@ rm -rfv run
 # Run bundle_adjust with a terrain constraint
 
 # First get the unaligned disparity and the interest points
-stereo ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai --num-matches-from-disp-triplets 10000 --unalign-disparity run/run
+stereo ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai --num-matches-from-disparity 10000 run/run
 
 # Create the reference terrain
 point2dem run/run-PC.tif
 
-# Copy the match file to the new name
-mv -fv run/run-disp-left_sub16__right_sub16.match run/run-left_sub16__right_sub16.match
-
-bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run --intrinsics-to-float other_intrinsics --parameter-tolerance 1e-12 --heights-from-dem run/run-DEM.tif --max-num-reference-points 1000 --threads 1 --num-passes 1 --num-iterations 4 --mapproj-dem run/run-DEM.tif --heights-from-dem-uncertainty 10
+bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run --intrinsics-to-float other_intrinsics --parameter-tolerance 1e-12 --heights-from-dem run/run-DEM.tif --max-num-reference-points 1000 --threads 1 --num-passes 1 --num-iterations 4 --mapproj-dem run/run-DEM.tif --heights-from-dem-uncertainty 10 --match-files-prefix run/run-disp
 
