e335ef6dcd55a63b44199610155ce87de0852274
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-08-09 09:18:14 -0700

    Update run to handle seed mode 3 with mapproj images

--- ssDG_alignNone_seedMode1_mapProj1_subPix1_no_default/run.sh
+++ ssDG_alignNone_seedMode1_mapProj1_subPix1_no_default/run.sh
@@ -6,7 +6,16 @@ rm -rfv run
 # Test using images mapprojected with the dg session but now running with json cameras.
 # This requires loading the dg cameras to undo the mapprojection.
 # Test running stereo with no default file.
+# Test --corr-seed-mode 2 with mapprojected images.
 
-stereo ../data/grand_mesa_WV03_left.map.dg.tif ../data/grand_mesa_WV03_right.map.dg.tif ../data/grand_mesa_WV03_left.json ../data/grand_mesa_WV03_right.json run/run ../data/grand_mesa_clip.tif
+stereo --corr-seed-mode 2                                \
+  --disparity-estimation-dem ../data/grand_mesa_clip.tif \
+  --disparity-estimation-dem-error 5                     \
+  ../data/grand_mesa_WV03_left.map.dg.tif                \
+  ../data/grand_mesa_WV03_right.map.dg.tif               \
+  ../data/grand_mesa_WV03_left.json                      \
+  ../data/grand_mesa_WV03_right.json                     \
+  run/run                                                \
+  ../data/grand_mesa_clip.tif
 point2dem run/run-PC.tif
 
