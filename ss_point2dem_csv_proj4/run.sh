8c8cc28f9f4cf55944910f8f268fc9d10b50f4d6
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-08-12 10:34:44 -0700

    Replace csv-proj4 with csv-srs

--- ss_point2dem_csv_proj4/run.sh
+++ ss_point2dem_csv_proj4/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-point2dem -r Earth ../data/zone10-CA_SanLuisResevoir-9m_10pct-utm.csv --tr 10 -o run/run --nodata-value -32767 --csv-format '1:easting 2:northing 3:height_above_datum' --csv-proj4 '+proj=utm +zone=10 +datum=WGS84 +units=m +no_defs' --t_projwin 641505.000 4133315.000 651505.000 4123315.000
+point2dem -r Earth ../data/zone10-CA_SanLuisResevoir-9m_10pct-utm.csv --tr 10 -o run/run --nodata-value -32767 --csv-format '1:easting 2:northing 3:height_above_datum' --csv-srs '+proj=utm +zone=10 +datum=WGS84 +units=m +no_defs' --t_projwin 641505.000 4133315.000 651505.000 4123315.000
 
--- ss_point2dem_small_csv/run.sh
+++ ss_point2dem_small_csv/run.sh
@@ -9,7 +9,7 @@ proj="+proj=stere +lat_0=0 +lat_ts=0 +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=1737400 +b=1
 point2dem --stereographic                                \
   --proj-lon 0 --proj-lat 0 -r moon                      \
   --csv-format 1:northing,2:easting,3:height_above_datum \
-  --csv-proj4  "$proj"                                   \
+  --csv-srs  "$proj"                                   \
   --tr 1.0                                               \
   ../data/small.csv -o run/run
 
