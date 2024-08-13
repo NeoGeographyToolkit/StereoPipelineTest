592a16fd51b054ae32ed569e771d917d6b3c8316
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-08-13 09:46:53 -0700

    Fix test

--- ss_pc_align_utm/run.sh
+++ ss_pc_align_utm/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-pc_align ../data/13APR25_WV02_SEVZ_1030010021A8A100_10030010021A64500_DEM3-3m_10pct.tif ../data/Severnaya-Bedrock-UTM47-Ellipsoidal-Height.txt --csv-format "utm:47N 1:easting 2:northing 3:height_above_datum" --max-displacement 100 --save-transformed-source-points --save-inv-transformed-reference-points --max-num-reference-points 1000000 --max-num-source-points 100000
+pc_align ../data/13APR25_WV02_SEVZ_1030010021A8A100_10030010021A64500_DEM3-3m_10pct.tif ../data/Severnaya-Bedrock-UTM47-Ellipsoidal-Height.txt --csv-format "utm:47N 1:easting 2:northing 3:height_above_datum" --max-displacement 100 --save-transformed-source-points --save-inv-transformed-reference-points --max-num-reference-points 1000000 --max-num-source-points 100000 -o run/run
 #Severnaya_UTM47_llh.txt
 
 # Test for point2las
