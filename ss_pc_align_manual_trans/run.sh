4d8fb0d712e9eb2dbb7abf2a08b2258165c9afff
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-08-14 09:19:24 -0700

    Update some scripts

--- ss_pc_align_manual_trans/run.sh
+++ ss_pc_align_manual_trans/run.sh
@@ -3,7 +3,5 @@
 set -x verbose
 rm -rfv run
 
-pc_align ../data/pluto-dem1.tif ../data/pluto-dem2.tif --match-file ../data/pluto1-pluto2.match --max-displacement -1 --num-iterations 0
-
-
+pc_align ../data/pluto-dem1.tif ../data/pluto-dem2.tif --match-file ../data/pluto1-pluto2.match --max-displacement -1 --num-iterations 0 -o run/run
 
