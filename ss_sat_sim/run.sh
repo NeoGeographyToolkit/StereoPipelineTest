53991f039f77745ad5f7a9984e6e41238544c77e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-08-22 16:21:24 -0700

    Update for ref cam

--- ss_sat_sim/run.sh
+++ ss_sat_sim/run.sh
@@ -3,7 +3,7 @@
 set -x verbose
 rm -rfv run
 
-sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o run/run --first 450 1500 -1000 --last 450 2500 -1000 --num 2 --first-ground-pos 450 2000 --last-ground-pos 450 2000  --rig-config ../data/sim_rig.txt --velocity 1000 --blur-sigma 0.5
+sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o run/run --first 450 1500 -1000 --last 450 2500 -1000 --num 2 --first-ground-pos 450 2000 --last-ground-pos 450 2000  --rig-config ../data/sim_rig.txt --velocity 1000 --blur-sigma 0.5 --save-ref-cams
 
 # Create orthoimages for inspection
 for f in run/*c[0-9].tif; do echo $f; g=${f/.tif/.tsai}; h=${f/.tif/.map.tif}; mapproject ../data/sat_sim_DEM.tif $f $g $h; done
