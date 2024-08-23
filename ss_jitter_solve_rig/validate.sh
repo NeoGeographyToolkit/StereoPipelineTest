6fb098f399b8c4f1e8186a5319c98170f588a0be
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-08-22 17:39:16 -0700

    Add jitter solve with rig testcase

---
+++ ss_jitter_solve_rig/validate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in \
+	run/run-linescan-fwd-c1.adjusted_state.json\
+	run/run-linescan-fwd-c2.adjusted_state.json\
+	run/run-linescan-nadir-c1.adjusted_state.json\
+	run/run-linescan-nadir-c2.adjusted_state.json\
+	; do 
+
+    gold=${file/run\//gold\/}
+
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
+    
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
+    
+    diff=$(diff $file $gold | head -n 50)
+    
+	echo diff $file $gold
+    echo diff is $diff
+    if [ "$diff" != "" ]; then
+        echo Validation failed
+        exit 1
+    fi
+
+done
+
+echo Validation succeded
+exit 0
+
