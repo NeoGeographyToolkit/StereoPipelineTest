0ee07f78cd14f9af06b8ae1b43f0189256bc30de
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-09-17 16:32:10 -0700

    Use fewer samples in one test

--- ss_cam2rpc_dem/run.sh
+++ ss_cam2rpc_dem/run.sh
@@ -4,7 +4,7 @@ set -x verbose
 rm -rfv run
 
 # Use a DEM to create an RPC camera
-cam2rpc --dem-file ../data/sat_sim_DEM.tif  ../data/cam2rpc.tif ../data/cam2rpc.tsai run/dem_cam.xml
+cam2rpc --dem-file ../data/sat_sim_DEM.tif  ../data/cam2rpc.tif ../data/cam2rpc.tsai run/dem_cam.xml --num-samples 20
 
 # Use a lon-lat-height range to create an RPC camera
 cam2rpc --datum D_MARS ../data/cam2rpc.tif ../data/cam2rpc.tsai run/llh_cam.xml --lon-lat-range 141.54898000443791 34.22559122255916 141.56529037850123 34.211986717004038 --height-range -10488.166 -9814.194 --num-samples 20
