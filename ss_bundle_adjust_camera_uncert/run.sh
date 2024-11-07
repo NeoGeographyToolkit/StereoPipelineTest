966f623090b16d208c75bea181f9f4b5821ec1e9
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-11-07 10:15:49 -0800

    Tweak new test

--- ss_bundle_adjust_camera_uncert/run.sh
+++ ss_bundle_adjust_camera_uncert/run.sh
@@ -6,9 +6,9 @@ rm -rfv run
 # Form the position uncertainty file
 pos=run/pos.txt
 mkdir -p $(dirname $pos)
-rm -f pos.txt 
-echo ../data/B17_016219_1978_XN_17N282W.8bit.cub 0.5 0.2 >> pos.txt; 
-echo ../data/B18_016575_1978_XN_17N282W.8bit.cub  0.5 0.2 >> pos.txt
+rm -f $pos
+echo ../data/B17_016219_1978_XN_17N282W.8bit.cub 0.5 0.2 >> pos.txt
+echo ../data/B18_016575_1978_XN_17N282W.8bit.cub 0.5 0.2 >> pos.txt
 
 bundle_adjust                                  \
   --threads 1                                  \
