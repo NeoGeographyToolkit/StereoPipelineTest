bc8fb3f3a9ed77456bdb2cb149d376193ecb67ba
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-11-20 13:20:53 -0800

    Clean up wording

--- ss_point2dem_wkt2/run.sh
+++ ss_point2dem_wkt2/run.sh
@@ -3,6 +3,7 @@
 set -x verbose
 rm -rfv run
 
-# This testcase reproduces a bug with dynamic CRS and multipl threads
+# This testcase reproduces a bug with dynamic CRS and multiple threads
 
 point2dem --tr 1.0 --t_srs ../data/proj.wkt --nodata-value -9999.0 ../data/cloud.tif -o run/run
+
