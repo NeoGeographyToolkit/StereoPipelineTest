c4bc148a9d022d5a1fa21195f96da81aa58f7f6b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-12-18 13:03:21 -0800

    Add testcase for no-data

--- ssRPC_alignLocalEpi_Libelas/run.sh
+++ ssRPC_alignLocalEpi_Libelas/run.sh
@@ -3,7 +3,8 @@
 set -x verbose
 rm -rfv run
 
-parallel_stereo --stereo-algorithm libelas --corr-tile-size 650 --sgm-collar-size 200 --alignment-method local_epipolar ../data/img_01_crop.tif ../data/img_02.tif run/run
+# Test libelas with --nodata-value
+parallel_stereo --stereo-algorithm libelas --corr-tile-size 650 --sgm-collar-size 200 --alignment-method local_epipolar ../data/img_01_crop.tif ../data/img_02.tif run/run --nodata-value 104
 
 point2dem run/run-PC.tif
 
