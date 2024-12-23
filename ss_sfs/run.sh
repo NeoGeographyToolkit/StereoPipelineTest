233c49605f084409b5cad38c1aea574a52476962
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-12-22 21:17:26 -0800

    Add --bundle-adjust-prefix to sfs test

--- ss_sfs/run.sh
+++ ss_sfs/run.sh
@@ -3,5 +3,4 @@
 set -x verbose
 rm -rfv run
 
-sfs -i ../data/sfs-input-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --use-approx-camera-models  --crop-input-images --threads 1
-
+sfs -i ../data/sfs-input-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --use-approx-camera-models --crop-input-images --threads 1 --bundle-adjust-prefix ../data/run
