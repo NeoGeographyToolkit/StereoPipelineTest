f0ba54ee9d79b2bcd00aaa1b89f89014094cd7f5
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-01-17 14:21:34 -0800

    Add test for --ref-map

--- ssCsmMapRpc_alignNone_seedMode1_mapProj0_subPix1_BasaltHills/run.sh
+++ ssCsmMapRpc_alignNone_seedMode1_mapProj0_subPix1_BasaltHills/run.sh
@@ -15,7 +15,8 @@ jitter_solve ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../d
 # Create mapprojected images with RPC cameras
 mapproject --tr 0.000053725423641 -t rpc ../data/stereo-DEM-09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.xml run/left_map.tif 
 
-mapproject --tr 0.000053725423641 -t rpc ../data/stereo-DEM-09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.xml run/right_map.tif
+# Borrow the projection string and grid size from the previous image
+mapproject -t rpc ../data/stereo-DEM-09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.tif ../data/09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.xml run/right_map.tif --ref-map run/left_map.tif
 
 # Run stereo with CSM and RPC-mapprojected images
 stereo run/left_map.tif run/right_map.tif run/jitter/run-09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.adjusted_state.json run/jitter/run-09OCT11191555-P1BS_R1C1-052783426010_01_P001_sub10.adjusted_state.json run/run ../data/stereo-DEM-09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif --left-image-crop-win 1458 224 748 762 --right-image-crop-win 1495 219 776 849
