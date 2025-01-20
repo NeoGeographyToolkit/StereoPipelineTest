ab55697c4870b9138d4987df82df726c4cfc0250
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-01-20 10:06:38 -0800

    Tune-up the optical bar test

--- ss_cam_gen_optical_bar/run.sh
+++ ss_cam_gen_optical_bar/run.sh
@@ -2,11 +2,14 @@
 
 set -x verbose
 rm -rfv run
-
-cam_gen --camera-type opticalbar                                              \
-    --lon-lat-values '99.55 31.266 101.866 31.55 101.916 31.416 99.55 31.133' \
-    --sample-file ../data/DS1105-2248DA079_optical_bar.tsai                   \
-    ../data/DS1105-2248DA079.tif                                              \
-    --reference-dem ../data/optical_bar_dem.tif                               \
-    -o run/run.tsai
     
+ll_vals='99.55 31.266 101.866 31.55 101.916 31.416 99.55 31.133' 
+sampFile=../data/DS1105-2248DA079_optical_bar.tsai
+cam_gen                                       \
+  --camera-type opticalbar                    \
+  --lon-lat-values "$ll_vals"                 \
+  --sample-file $sampFile                     \
+  ../data/DS1105-2248DA079.tif                \
+  --reference-dem ../data/optical_bar_dem.tif \
+  --refine-camera                             \
+  -o run/run.tsai
