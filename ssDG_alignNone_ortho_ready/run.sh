5f37048a15f17cdae46c69aa9ae4169a08325c0e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-01-22 13:47:17 -0800

    Update DG ortho ready run

--- ssDG_alignNone_ortho_ready/run.sh
+++ ssDG_alignNone_ortho_ready/run.sh
@@ -3,13 +3,6 @@
 set -x verbose
 rm -rfv run
 
-parallel_stereo -t dg           \
-  --stereo-algorithm asp_mgm    \
-  --ortho-heights -23.1 -23.3   \
-  ../data/left_bathy_ortho.tif  \
-  ../data/right_bathy_ortho.tif \
-  ../data/left_bathy.xml        \
-  ../data/right_bathy.xml       \
-  run/run
-
+parallel_stereo --stereo-algorithm asp_mgm ../data/dg_ortho/left_regrid.tif ../data/dg_ortho/right_regrid.tif ../data/dg_ortho/25JAN15185222-P2AS-200007010928_01_P001.XML ../data/dg_ortho/25JAN16185415-P2AS-200007018742_01_P001.XML --left-image-crop-win 962 1493 1000 1000 --right-image-crop-win 1879 2489 1694 1570 run/run
 point2dem run/run-PC.tif
+
