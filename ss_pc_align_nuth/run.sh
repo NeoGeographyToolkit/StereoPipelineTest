3d6ac085a29d46a0b8fd739bdc526143f905f707
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-02-17 13:49:14 -0800

    Update nuth test

--- ss_pc_align_nuth/run.sh
+++ ss_pc_align_nuth/run.sh
@@ -3,4 +3,5 @@
 set -x verbose
 rm -rfv run
 
-pc_align ../data/rock_dem_50pct_crop1.tif ../data/rock_dem_50pct_shift_10_20_30.tif -o run/run --max-displacement 5002 --compute-translation-only --threads 1 --alignment-method nuth
+pc_align ../data/rock_dem_50pct_crop1.tif ../data/rock_dem_50pct_shift_10_20_30.tif -o run/run --max-displacement 5002 --compute-translation-only --threads 1 --alignment-method nuth --nuth-options "--slope-lim 0.1025 40.4 --tol 0.0013 --num-inner-iter 12 --max-vertical-offset 6005 --max-horizontal-offset 6066"
+
