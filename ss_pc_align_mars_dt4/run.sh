e6c0a2cc32665e6f20a11673c62234130ac41bae
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-02-17 19:03:46 -0800

    Tune up ss_pc_align_mars_dt4

--- ss_pc_align_mars_dt4/run.sh
+++ ss_pc_align_mars_dt4/run.sh
@@ -3,6 +3,5 @@
 set -x verbose
 rm -rfv run
 
-pc_align --max-displacement 100 ../data/H2145_0000_DT4_crop.cub ../data/H2145_0000_DT4_crop.cub --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --datum D_MARS  --max-num-source-points 10000 --max-num-reference-points 1000
-
+pc_align --max-displacement 100 ../data/H2145_0000_DT4_crop.cub ../data/H2145_0000_DT4_crop.cub --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --datum D_MARS --max-num-source-points 100000 --max-num-reference-points 100000
 
