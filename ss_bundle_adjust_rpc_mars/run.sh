a037c4402b128bd6f873e6ebcc7897c46b5103c6
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-02-25 13:45:00 -0800

    Add test for --image-list in ba

--- ss_bundle_adjust_rpc_mars/run.sh
+++ ss_bundle_adjust_rpc_mars/run.sh
@@ -3,5 +3,9 @@
 set -x verbose
 rm -rfv run
 
-bundle_adjust ../data/left_mars_rpc.tif ../data/right_mars_rpc.tif ../data/left_mars_rpc.xml ../data/right_mars_rpc.xml -o run/run -t rpc --datum mars --threads 1
+# Test --image-list
+mkdir -p run
+ls ../data/left_mars_rpc.tif ../data/right_mars_rpc.tif > run/image_list.txt
+ls ../data/left_mars_rpc.xml ../data/right_mars_rpc.xml > run/camera_list.txt
+bundle_adjust --image-list run/image_list.txt --camera-list run/camera_list.txt -o run/run -t rpc --datum mars --threads 1
 
