0d77348dc449f66439b7fc0a37f20793ecb6a589
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-02-26 09:49:48 -0800

    Cleanup test

--- ss_bundle_adjust_fix_one_cam/run.sh
+++ ss_bundle_adjust_fix_one_cam/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-
 bundle_adjust ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.photometrix.tsai ../data/right_sub16.photometrix.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run --ip-per-tile 1000 --fixed-camera-indices "1" --threads 1
+
