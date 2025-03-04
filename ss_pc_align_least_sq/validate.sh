82a8771db3585b3f46e30f9122b2ca6dfcd29ee5
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-03-04 08:56:03 -0800

    Fix a test

--- ss_pc_align_least_sq/validate.sh
+++ ss_pc_align_least_sq/validate.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-for file in run/run-trans_reference.tif run/run-trans_source.tif; do
-	gold=${file/run\/run/gold\/run}
+for file in run/run-trans_reference-DEM.tif; do 
+    
+    gold=${file/run\//gold\/}
 
-	if [ ! -e "$file" ]; then
-    	echo "ERROR: File $file does not exist."
-	    exit 1;
-	fi
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
+    
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
+    
+    # Remove cached xmls
+    rm -fv "$file.aux.xml"
+    rm -fv "$gold.aux.xml"
 
-	if [ ! -e "$gold" ]; then
-	    echo "ERROR: File $gold does not exist."
-	    exit 1;
-	fi
+    cmp_stats.sh $file $gold
+    gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v size | grep -v Left | grep -v Right > run.txt
+    gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v size | grep -v Left | grep -v Right > gold.txt
 
-	diff=$(cmp $file $gold | head -n 50)
-	ans=$?
-	echo "diff is $diff"
-	echo "return flag is $ans"
-	if [ "$diff" != "" ] || [ "$ans" -ne 0 ]; then
-	    echo Validation failed
-	    exit 1
-	fi
+    diff run.txt gold.txt
+
+    max_err.pl run.txt gold.txt # print the error
+    ans=$(max_err.pl run.txt gold.txt 1e-7) # compare the error
+    if [ "$ans" -eq 0 ]; then
+        echo Validation failed
+        exit 1
+    fi
+    
+    rm -f run.txt gold.txt
+    
+    echo diff is $diff
+    if [ "$diff" != "" ]; then
+        echo Validation failed
+        exit 1
+    fi
+
 done
 
 echo Validation succeeded
 exit 0
-
