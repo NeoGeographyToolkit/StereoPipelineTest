489b7c2a574dcac0d808723c5f0ce9776cd817b4
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-03-04 19:24:37 -0800

    Fix test

--- ss_pc_align_least_sq/run.sh
+++ ss_pc_align_least_sq/run.sh
@@ -11,5 +11,4 @@ pc_align ../data/ref_scale_dem.tif ../data/src_scale_dem.tif -o run/run --max-di
 
 pc_align ../data/ref_scale_dem.tif ../data/src_scale_dem.tif -o run/run --max-displacement 10000 --alignment-method least-squares --max-num-reference-points 10000 --max-num-source-points 1000 --num-iterations 10 --save-transformed-source-points --save-inv-transformed-reference-points --initial-transform run/run-transform.txt
 
-
-
+point2dem run/run-trans_reference.tif
