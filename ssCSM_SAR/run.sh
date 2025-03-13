5493c59d53d2db95198c9060b1a679ed2a862810
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-03-13 08:48:50 -0700

    Tweak run

--- ssCSM_SAR/run.sh
+++ ssCSM_SAR/run.sh
@@ -5,7 +5,7 @@ rm -rfv run
 
 parallel_stereo ../data/lsz_03821_1cd_xku_16n196_v1_clamp_crop.tif ../data/lsz_03822_1cd_xku_23n196_v1_clamp_crop.tif ../data/lsz_03821_1cd_xku_16n196_v1.json ../data/lsz_03822_1cd_xku_23n196_v1.json --stereo-algorithm asp_mgm --skip-rough-homography --no-datum --ip-per-tile 6000 --left-image-crop-win 469 8005 1831 1822 --right-image-crop-win 562 27190 1404 1696 run/run
 
-point2dem run/run-PC.tif
+point2dem --geographic run/run-PC.tif
 
 # Fill holes. There are a lot of them.
 dem_mosaic --dem-blur-sigma 10 run/run-DEM.tif -o run/run-DEM-blur.tif
