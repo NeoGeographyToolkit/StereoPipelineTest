d3e28ddd69b1233440a0d8acb81395cf4b1178c4
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-03-13 09:00:33 -0700

    Fix more the tests; now must set explicitly the geographic projection

--- ssCSM_SAR/run.sh
+++ ssCSM_SAR/run.sh
@@ -10,13 +10,13 @@ point2dem --geographic run/run-PC.tif
 # Fill holes. There are a lot of them.
 dem_mosaic --dem-blur-sigma 10 run/run-DEM.tif -o run/run-DEM-blur.tif
 
+# A geographic projection must be explicitly set
+proj="+proj=longlat +R=1737400 +no_defs"
+mapproject run/run-DEM-blur.tif ../data/lsz_03821_1cd_xku_16n196_v1_clamp_crop.tif ../data/lsz_03821_1cd_xku_16n196_v1.json run/lsz_03821_1cd_xku_16n196_v1.map.tif --t_projwin 196.277 22.247 196.611 21.847 --tr 0.00037 --t_srs "$proj"
+mapproject run/run-DEM-blur.tif ../data/lsz_03822_1cd_xku_23n196_v1_clamp_crop.tif ../data/lsz_03822_1cd_xku_23n196_v1.json run/lsz_03822_1cd_xku_23n196_v1.map.tif --t_projwin 196.277 22.247 196.611 21.847 --tr 0.00037 --t_srs "$proj"
+
 # Now test bundle adjustment. It is tricky to find match points, that is why 
 # use mapprojection after stereo
-
-mapproject run/run-DEM-blur.tif ../data/lsz_03821_1cd_xku_16n196_v1_clamp_crop.tif ../data/lsz_03821_1cd_xku_16n196_v1.json run/lsz_03821_1cd_xku_16n196_v1.map.tif --t_projwin 196.277 22.247 196.611 21.847 --tr 0.00037
-
-mapproject run/run-DEM-blur.tif ../data/lsz_03822_1cd_xku_23n196_v1_clamp_crop.tif ../data/lsz_03822_1cd_xku_23n196_v1.json run/lsz_03822_1cd_xku_23n196_v1.map.tif --t_projwin 196.277 22.247 196.611 21.847 --tr 0.00037
-
 bundle_adjust ../data/lsz_03821_1cd_xku_16n196_v1_clamp_crop.tif ../data/lsz_03822_1cd_xku_23n196_v1_clamp_crop.tif ../data/lsz_03821_1cd_xku_16n196_v1.json ../data/lsz_03822_1cd_xku_23n196_v1.json --mapprojected-data "run/lsz_03821_1cd_xku_16n196_v1.map.tif run/lsz_03822_1cd_xku_23n196_v1.map.tif run/run-DEM-blur.tif" -o run/ba/run --ip-per-tile 60000 --matches-per-tile 60000 --threads 1 --min-matches 5 --remove-outliers-params '3 75 10000 10000' --num-iterations 10
 
 # Second bundle adjustment, to see if we can load back the state.
