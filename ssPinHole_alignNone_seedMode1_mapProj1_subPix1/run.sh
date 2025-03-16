cbe179716efc661bf737cb08ec947562bde31a83
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-03-15 18:42:15 -0700

    Tune test

--- ssPinHole_alignNone_seedMode1_mapProj1_subPix1/run.sh
+++ ssPinHole_alignNone_seedMode1_mapProj1_subPix1/run.sh
@@ -3,8 +3,7 @@
 set -x verbose
 rm -rfv run
 
-stereo -t pinholemappinhole ../data/pinhole_proj_47.tif ../data/pinhole_proj_48.tif ../data/pinhole_proj_47.tif.tsai ../data/pinhole_proj_48.tif.tsai   run/run ../data/krigged_dem_nsidc_ndv0_fill.tif -s stereo.default --corr-seed-mode 1 --subpixel-mode 1
+stereo -t pinholemappinhole ../data/pinhole_proj_47.tif ../data/pinhole_proj_48.tif ../data/pinhole_proj_47.tif.tsai ../data/pinhole_proj_48.tif.tsai run/run ../data/krigged_dem_nsidc_ndv0_fill.tif -s stereo.default --corr-seed-mode 1 --subpixel-mode 1
 
 point2dem --nodata-value -32767 run/run-PC.tif
-
 
