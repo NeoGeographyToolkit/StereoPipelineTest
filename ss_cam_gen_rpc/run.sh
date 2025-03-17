19164954f00dd7cb18a635051b50244369f198ba
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-03-17 16:46:55 -0700

    Tweak test

--- ss_cam_gen_rpc/run.sh
+++ ss_cam_gen_rpc/run.sh
@@ -5,6 +5,10 @@ rm -rfv run
 
 # Export RPC stored separately
 cam_gen ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12_crop.tif --input-camera ../data/WV01_11JAN131652275-P1BS-10200100104A0300.r12.xml --camera-type rpc -o run/run_rpc1.xml
+
 # Export RPC embedded in the image
 cam_gen --camera-type rpc ../data/2024-08-08-02-33-49_UMBRA-05_GEC.tif -o run/run_rpc2.xml
+
+# Export RPC embedded in rpb
+cam_gen ../data/09OCT11191503-P1BS_R1C1-052783426010_01_P001_sub10.tif --input-camera ../data/camera.rpb --camera-type rpc -o run/run_rpc3.xml
 
