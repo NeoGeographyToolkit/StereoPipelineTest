5e60ec016e996caa872e9d8dcf1ba922b0dcc5ce
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-03-25 08:46:14 -0700

    Fix test to use local convert

--- ss_historical_helper/run.sh
+++ ss_historical_helper/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-historical_helper.py --interest-points '4523 1506  114956 1450  114956 9355  4453 9408' rotate-crop --input-path ../data/left.tif --output-path run/run.tif --convert-path $HOME/miniconda3/envs/imagemagick/bin/convert
+historical_helper.py --interest-points '4523 1506  114956 1450  114956 9355  4453 9408' rotate-crop --input-path ../data/left.tif --output-path run/run.tif --convert-path /usr/bin/convert
 
