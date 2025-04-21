bd4881e89548a50d5e3930ab3c8245b47eb68303
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-04-21 12:00:59 -0700

    Tuneup max_err.pl

--- bin/max_err.pl
+++ bin/max_err.pl
@@ -1,18 +1,20 @@
 #!/usr/bin/perl
-use strict;	   # insist that all variables be declared
-use diagnostics;   # expand the cryptic warnings
+use strict;
+use diagnostics;
 use Scalar::Util qw(looks_like_number);
 use List::Util   qw(max min);
 
-undef $/;          # read one whole file in one scalar
+undef $/;
 
 MAIN:{
 
-  # Look at the maximum discrepancy between corresponding numbers in
-  # file1 and file2.  If a third argument is given, return the
-  # comparision of whether the maximum relative error between the two
-  # files is less than the third argument.
-  if (scalar(@ARGV) < 2){
+  # Given two files and a tolerance, return 1 if the maximum relative error
+  # between any corresponding numerical values in the two files is less than the
+  # tolerance. Return 0 otherwise, or if the files are not 1-to-1.
+  
+  # Without the tolerance, print the lines with discrepancy.
+  
+  if (scalar(@ARGV) < 2) {
     print "Usage: $0 file1 file2\n";
     exit(1);
   }
@@ -20,18 +22,29 @@ MAIN:{
   my $file1 = shift @ARGV;
   my $file2 = shift @ARGV;
 
-  my $thresh = -1;
-  if (scalar(@ARGV)){
-    $thresh = shift @ARGV;
+  my $tol = -1;
+  if (scalar(@ARGV)) {
+    $tol = shift @ARGV;
   }
   
-  open(FILE, "<$file1");  my @val1 = split("\n", <FILE>);  close(FILE);
-  open(FILE, "<$file2");  my @val2 = split("\n", <FILE>);  close(FILE);
-  if ( scalar(@val1) != scalar(@val2) ){
+  # Read the lines from the files
+  open(FILE, "<$file1"); my @val1 = split("\n", <FILE>);  close(FILE);
+  open(FILE, "<$file2"); my @val2 = split("\n", <FILE>);  close(FILE);
+  
+  if (scalar(@val1) != scalar(@val2)) {
+
+    if ($tol > 0) {
+      # The comparison with tolerance failed
+      my $ans = 1;
+      print "$ans\n";
+      exit(0);
+    }
+      
     print "Error: Files $file1 and $file2 do not have the same number of rows.\n";
-	exit(1);
+	  exit(1);
   }
-  my $numRows = min( scalar(@val1), scalar(@val2) );
+  
+  my $numRows = min(scalar(@val1), scalar(@val2));
 
   my ($max_abs_err, $max_rel_err, $max_row, $max_col) = (0, 0, 0, 0);
 
@@ -57,26 +70,36 @@ MAIN:{
 
     my $len1 = scalar(@l1);
     my $len2 = scalar(@l2);
-    if ( $len1 != $len2 ){
-      print "Warning: Row " . ($row_num + 1) . " does not have the same number "
+    if ($len1 != $len2) {
+
+      if ($tol > 0) {
+        # The comparison with tolerance failed
+        my $ans = 1;
+        print "$ans\n";
+        exit(0);
+      }
+      
+      print "Error: Row " . ($row_num + 1) . " does not have the same number "
          . "of elements in $file1 and $file2 ($len1 vs $len2).\n";
+      exit(1);
     }
-    my $numlen = min( $len1, $len2 );
+    
+    my $len = min($len1, $len2);
 
     my $col_num = -1;
-    for (my $col = 0; $col < $numlen; $col++){
+    for (my $col = 0; $col < $len; $col++) {
 
-      if ( looks_like_number( $l1[$col] ) && looks_like_number( $l2[$col] ) ){
+      if (looks_like_number($l1[$col]) && looks_like_number( $l2[$col])) {
 
         $col_num++;
 
-        my $err = abs( $l1[$col] - $l2[$col] );
+        my $err = abs($l1[$col] - $l2[$col]);
         my $den = min(abs($l1[$col]), abs($l2[$col]));
         $den = 1 if ($den == 0);
         my $rel_err = $err/$den;
         #print "rel_err abs_err data $rel_err $err $l1[$col] $l2[$col] "
         # . "$row_num $col_num\n";
-        if ($err > $max_abs_err){
+        if ($err > $max_abs_err) {
           $max_abs_err = $err;
           $max_rel_err = $rel_err;
           $max_col = $col_num;
@@ -94,11 +117,10 @@ MAIN:{
 
   }
 
-  if ($thresh > 0){
-    # Print if the value is below threshold
+  if ($tol > 0) {
     my $ans = 0;
-    if ($max_rel_err < $thresh ){
-      $ans = 1;
+    if ($max_rel_err < $tol) {
+      $ans = 1; # success comparing with tolerance
     }
     print "$ans\n";
     exit(0);
