9acfd32668d5f86a82912d88c2f63d97bd23df1a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-04-29 19:42:11 -0700

    Relax validation

--- ss_camera_solve_mt_sidley/validate.sh
+++ ss_camera_solve_mt_sidley/validate.sh
@@ -1,42 +1,45 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-for file in run/run-DEM.tif; do
+file=run/run-DEM.tif
+gold=gold/run-DEM.tif
 
-  gold=${file/run\//gold\/}
-  echo file=$file
-  echo gold=$gold
+if [ ! -e "$file" ]; then
+    echo "ERROR: File $file does not exist."
+    exit 1;
+fi
 
-  if [ ! -e "$file" ]; then
-      echo "ERROR: File $file does not exist."
-      exit 1;
-  fi
+if [ ! -e "$gold" ]; then
+    echo "ERROR: File $gold does not exist."
+    exit 1;
+fi
 
-  if [ ! -e "$gold" ]; then
-      echo "ERROR: File $gold does not exist."
-      exit 1;
-  fi
+# Remove cached xmls
+rm -fv "$file.aux.xml"
+rm -fv "$gold.aux.xml"
 
-  # Remove cached xmls
-  rm -fv "$file.aux.xml"
-  rm -fv "$gold.aux.xml"
+cmp_stats.sh $file $gold
+gdalinfo -stats $file | grep -v Files | grep -v -i tif > run.txt
+gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold.txt
 
-  cmp_stats.sh $file $gold
-  gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v Min= > run.txt
-  gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v Min= > gold.txt
+diff=$(diff run.txt gold.txt)
+cat run.txt
 
-  diff=$(diff run.txt gold.txt)
-  cat run.txt
+# Print the error and check the status
+../bin/max_err.pl run.txt gold.txt
+ans=$?
+if [ "$ans" -ne 0 ]; then
+	echo Validation failed
+	exit 1
+fi
 
-  rm -f run.txt gold.txt
+ans=$(../bin/max_err.pl run.txt gold.txt 0.01) # compare the error
+if [ "$ans" != "1" ]; then
+    echo Validation failed
+    exit 1
+fi
 
-  echo diff is $diff
-  if [ "$diff" != "" ]; then
-      echo Validation failed
-      exit 1
-  fi
-
-done
+rm -f run.txt gold.txt
 
 echo Validation succeeded
 exit 0
