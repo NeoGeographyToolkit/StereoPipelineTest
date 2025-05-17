0005ee82f0b0d80d404347837eff357177486174
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-05-16 19:51:16 -0700

    Update test for copc

--- ss_pc_align_copc/run.sh
+++ ss_pc_align_copc/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-pc_align ../data/autzen-classified.copc.laz ../data/autzen-classified.copc.laz --max-displacement 100 --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --csv-format utm:10N,1:easting,2:northing,3:height_above_datum --ref-copc-win 636400 852260 638180 849990 --src-copc-win 636400 852260 638180 849990
+pc_align ../data/autzen-classified.crop.laz ../data/autzen-classified.copc.laz --max-displacement 100 --save-transformed-source-points --save-inv-transformed-reference-points --output-prefix run/run --csv-format utm:10N,1:easting,2:northing,3:height_above_datum
 
--- ss_pc_align_copc/validate.sh
+++ ss_pc_align_copc/validate.sh
@@ -1,65 +1,46 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-file=run/run-trans_reference.laz
-gold=gold/run-trans_reference.laz
+for file in run/run-trans_reference.laz run/run-trans_source.laz; do 
+  
+  gold=gold/$(basename $file)
+  
+  echo "Comparing: $file with $gold"
 
-if [ ! -e "$file" ]; then
-    echo "ERROR: File $file does not exist."
-    exit 1;
-fi
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
 
-if [ ! -e "$gold" ]; then
-    echo "ERROR: File $gold does not exist."
-    exit 1;
-fi
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
 
-pdal info --all $file | grep -v filename | grep -v date | grep -i -v software | grep -v now | grep -v creation | grep -v href > run/run.txt
-ans=$?
-if [ $ans -ne 0 ]; then
-    echo Validation failed
-    exit 1
-fi
+    pdal info --all $file | grep -v filename | grep -v date | grep -i -v software | grep -v now | grep -v creation | grep -v href > run/run.txt
+    ans=$?
+    if [ $ans -ne 0 ]; then
+        echo Validation failed
+        exit 1
+    fi
 
-pdal info --all $gold |grep -v filename | grep -v date | grep -i -v software | grep -v now | grep -v creation | grep -v href > gold/run.txt
-ans=$?
-if [ $ans -ne 0 ]; then
-    echo Validation failed
-    exit 1
-fi
+    pdal info --all $gold |grep -v filename | grep -v date | grep -i -v software | grep -v now | grep -v creation | grep -v href > gold/run.txt
+    ans=$?
+    if [ $ans -ne 0 ]; then
+        echo Validation failed
+        exit 1
+    fi
 
-diff=$(diff run/run.txt gold/run.txt | head -n 50)
+    diff=$(diff run/run.txt gold/run.txt | head -n 50)
 
-rm -f run/run.txt gold/run.txt
+    rm -f run/run.txt gold/run.txt
 
-echo diff is $diff
-if [ "$diff" != "" ]; then
-    echo Validation failed
-    exit 1
-fi
-
-file=run/run-trans_source.csv
-gold=gold/run-trans_source.csv
-
-if [ ! -e "$file" ]; then
-    echo "ERROR: File $file does not exist."
-    exit 1;
-fi
-
-if [ ! -e "$gold" ]; then
-    echo "ERROR: File $gold does not exist."
-    exit 1;
-fi
-
-diff=$(diff $file $gold| head -n 100)
-echo "diff is $diff"
-
-../bin/max_err.pl $file $gold
-ans=$(../bin/max_err.pl $file $gold 1e-10 | grep -v Warn) # compare the error
-if [ "$ans" -eq 0 ]; then
-    echo Validation failed
-    exit 1
-fi
+    echo diff is $diff
+    if [ "$diff" != "" ]; then
+        echo Validation failed
+        exit 1
+    fi
+done
 
 echo Validation succeeded
 exit 0
