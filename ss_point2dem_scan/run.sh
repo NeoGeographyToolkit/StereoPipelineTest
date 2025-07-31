a7d51e93cd2c0b56e552f06636c555c1fb9b7097
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-07-31 09:56:22 -0700

    Fix a test

--- ss_point2dem_scan/run.sh
+++ ss_point2dem_scan/run.sh
@@ -6,7 +6,7 @@ rm -rfv run
 # Grid a dataset having x, y, z values in a small ground-level box, as obtained from a 3D scanner
 proj="+proj=stere +lat_0=0 +lat_ts=0 +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=1737400 +b=1737400 +units=m +no_defs"
 
-point2dem --t_srs "$proj" --tr 0.01 --input-is-projected ../data/moon-PC.tif -o run/run
+point2dem --t_srs "$proj" --tr 1 --input-is-projected ../data/moon-PC.tif -o run/run
 
 # TODO(oalexan1): Why does this fail after upgrading GDAP for ASP 3.5.0? There's no good reason.
 # point2dem --t_srs "$proj" --tr 0.01 --input-is-projected ../data/scan.tif -o run/run
