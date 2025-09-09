3f5122354fa80ec6e7d4b90ab4e508bd29bcf350
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-09-09 13:51:26 -0700

    Add testcase for mapproject --tap

---
+++ ss_mapproject_tap/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+mapproject ../data/srtm_59_08.tif ../data/mapprojtest.tif ../data/mapprojtest.xml run/run-RPC.tif --t_srs "+proj=eqc +units=m +datum=WGS84" --tr 0.5 -t rpc --gdal-tap --t_projwin 12610941.000 2579715.000 12611485.000 2580214.500
+
