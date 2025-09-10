d20261374fbdd172a874b05882f9428243aac5c0
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-09-10 09:42:35 -0700

    Update test ss_point2dem_tap

--- ss_point2dem_tap/run.sh
+++ ss_point2dem_tap/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-point2dem --tr 10 --gdal-tap ../data/alaska-PC.tif -o run/run
+point2dem --gdal-tap --tr 10 --t_projwin -191775 -2265255.2 -190865 -2266205.3 ../data/alaska-PC.tif -o run/run
 
