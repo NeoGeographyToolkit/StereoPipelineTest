8d667133fc9ef6a2beaf9894e93fb309e90365cc
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-09-23 20:23:44 -0700

    Update a test

--- ss_sfs/run.sh
+++ ss_sfs/run.sh
@@ -3,8 +3,8 @@
 set -x verbose
 rm -rfv run
 
-# SfS without approximating the camera model or input adjustments
-sfs -i ../data/sfs-input-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --crop-input-images --threads 1
+# SfS without approximating the camera model or input adjustments. Test the option --allow-borderline-data
+sfs -i ../data/sfs-input-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --crop-input-images --threads 1 --allow-borderline-data --blending-dist 5 --shadow-threshold 0.026
 
 # Estimate height errors
 sfs -i ../data/sfs-input-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --crop-input-images --threads 1 --estimate-height-errors
