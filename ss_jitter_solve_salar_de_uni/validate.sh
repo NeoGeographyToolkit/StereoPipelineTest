8496f945f43f2ff2f5e5dacaabc1c12a698c196a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-09-29 14:21:04 -0700

    Update validate test

--- ss_jitter_solve_salar_de_uni/validate.sh
+++ ss_jitter_solve_salar_de_uni/validate.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
+# Validate the produced cameras
+for file in \
+    run/run-csm-104001001427B900.r100.adjusted_state.json \
+    run/run-csm-1040010014761800.r100.adjusted_state.json; do
+    
+    gold=gold/$(basename $file)
+    
+    # file must exist
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
+    
+    # gold must exist
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
+
+    # Diff must be empty
+    diff=$(diff $file $gold)
+    if [ "$diff" != "" ]; then
+        echo Validation failed for $file
+        exit 1
+    else
+        echo Validation succeeded for $file
+    fi
+done
+
+# Validate the orbit_plot.py results
 file=run/run.png
 gold=gold/run.png
 
@@ -31,5 +61,5 @@ if [ "$diff" != "" ]; then
     exit 1
 fi
 
-echo Validation succeeded
+echo Validation succeeded for $file
 exit 0
