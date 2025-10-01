3e0e77ae6d162216419e959ad9488d6a92ccc3d1
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-09-30 19:54:48 -0700

    Expand test for camera footprint

--- ss_camera_footprint/run.sh
+++ ss_camera_footprint/run.sh
@@ -3,6 +3,9 @@
 set -x verbose
 rm -rfv run
 
-# This tests camera_footprint in a borderline situation where there was a bug 
-camera_footprint --datum WGS_1984 ../data/camera_footprint.tif ../data/camera_footprint.tsai --output-kml run/run.kml
+# This tests camera_footprint in a borderline situation where there was a bug. Find footprint onto datum. 
+camera_footprint --datum WGS_1984 ../data/camera_footprint.tif ../data/camera_footprint.tsai --output-kml run/run_datum.kml
+
+# Find footprint onto DEM
+camera_footprint --dem-file ../data/camera_footprint_dem.tif ../data/camera_footprint.tif ../data/camera_footprint.tsai --output-kml run/run_dem.kml
 
--- ss_camera_footprint/validate.sh
+++ ss_camera_footprint/validate.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-file=run/run.kml
-gold=gold/run.kml
+for file in run/run_datum.kml run/run_dem.kml; do 
 
-if [ ! -e "$file" ]; then
-    echo "ERROR: File $file does not exist."
-    exit 1;
-fi
+    gold=gold/$(basename $file)
 
-if [ ! -e "$gold" ]; then
-    echo "ERROR: File $gold does not exist."
-    exit 1;
-fi
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
 
-diff=$(diff $file $gold)
-echo diff is $diff
-if [ "$diff" != "" ]; then
-    echo Validation failed
-    exit 1
-fi
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
+
+    diff=$(diff $file $gold)
+    echo diff is $diff
+    if [ "$diff" != "" ]; then
+        echo Validation failed
+        exit 1
+    fi
+done
 
 echo Validation succeeded
 exit 0
