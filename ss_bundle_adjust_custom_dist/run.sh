014d43daa08e33e43f43898d613982cc6bcee9ff
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-10-10 14:21:34 -0700

    Update test  ss_bundle_adjust_custom_dist

--- ss_bundle_adjust_custom_dist/run.sh
+++ ss_bundle_adjust_custom_dist/run.sh
@@ -14,9 +14,11 @@ ls ../data/right_sub16.v2.tsai > run/sensor2_cameras.txt
 bundle_adjust --solve-intrinsics                                  \
     --inline-adjustments                                          \
     --intrinsics-to-float "distortion"                            \
+    --fixed-distortion-indices 4                                  \
     -t pinhole                                                    \
     --threads 1                                                   \
     --num-iterations 10                                           \
     --image-list run/sensor1_images.txt,run/sensor2_images.txt    \
     --camera-list run/sensor1_cameras.txt,run/sensor2_cameras.txt \
     -o run/run
+
