fbcc979102c67d6733d24472102886c2357d4ff1
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-10-10 10:29:37 -0700

    Add ss_bundle_adjust_custom_dist

---
+++ ss_bundle_adjust_custom_dist/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+mkdir -p run
+
+# Bundle adjustment (per sensor) while floating only certain distortion
+# coefficients. This also influences --min-distortion.
+
+ls ../data/left_sub16.tif > run/sensor1_images.txt
+ls ../data/right_sub16.tif > run/sensor2_images.txt
+ls ../data/left_sub16.v2.tsai > run/sensor1_cameras.txt
+ls ../data/right_sub16.v2.tsai > run/sensor2_cameras.txt
+bundle_adjust --solve-intrinsics                                  \
+    --inline-adjustments                                          \
+    --intrinsics-to-float "distortion"                            \
+    -t pinhole                                                    \
+    --threads 1                                                   \
+    --image-list run/sensor1_images.txt,run/sensor2_images.txt    \
+    --camera-list run/sensor1_cameras.txt,run/sensor2_cameras.txt \
+    -o run/run
---
+++ ss_bundle_adjust_custom_dist/validate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in \
+    run/run-left_sub16.v2.tsai \
+    run/run-right_sub16.v2.tsai; do
+
+  gold=gold/$(basename $file)
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+  echo diff $file $gold
+  diff=$(diff $file $gold | head -n 50)
+
+  echo diff is $diff
+  if [ "$diff" != "" ]; then
+      echo Validation failed
+      exit 1
+  fi
+
+done
+
+echo Validation succeeded
+exit 0
