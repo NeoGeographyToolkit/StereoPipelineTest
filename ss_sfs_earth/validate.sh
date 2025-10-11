d1d86bc18f7d3cadcb1529451439746d967a455a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-10-10 21:57:58 -0700

    Fix test

--- ss_sfs_earth/validate.sh
+++ ss_sfs_earth/validate.sh
@@ -3,6 +3,11 @@ export PATH=../bin:$PATH
 
 # Print any diff
 ../bin/max_err.pl run/run.txt gold/run.txt
+ans=$?
+if [ "$ans" -ne 0 ]; then
+  echo Validation failed
+  exit 1
+fi
 
 # Evaluate the diff with tolerance
 ans=$(../bin/max_err.pl run/run.txt gold/run.txt 1e-9)
