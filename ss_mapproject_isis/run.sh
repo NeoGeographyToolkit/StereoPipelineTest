c9dec37414cd6eafa0fd7ecf6f240e654ea95552
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-11-12 13:21:49 -0800

    Update test for new corner case

--- ss_mapproject_isis/run.sh
+++ ss_mapproject_isis/run.sh
@@ -6,5 +6,7 @@ rm -rfv run
 mkdir -p run
 uname -n > run/machines.txt
 
-mapproject ../data/sfs-DEM.tif ../data/sfs-cam.cub run/run.tif --tile-size 50 --nodes-list run/machines.txt
+# Test for when the DEM grid and output image grid are the same. This demonstrates a bug fix
+# for erision in the output mapprojected image at DEM boundary.
+mapproject --tr 10 --tile-size 50 ../data/dem_tr10.tif ../data/sfs-cam.cub run/run.tif --nodes-list run/machines.txt
 
