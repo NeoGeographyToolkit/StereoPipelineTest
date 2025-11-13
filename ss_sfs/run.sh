0e38c1db21f2b6d914e0a0ae9981d4d7331ed380
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-11-13 10:19:11 -0800

    sfs: Test curvature in shadow

--- ss_sfs/run.sh
+++ ss_sfs/run.sh
@@ -3,9 +3,30 @@
 set -x verbose
 rm -rfv run
 
-# SfS without approximating the camera model or input adjustments. Test the option --allow-borderline-data
-sfs -i ../data/sfs-input-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --crop-input-images --threads 1 --allow-borderline-data --blending-dist 5 --shadow-threshold 0.026
+# SfS without approximating the camera model or input adjustments. Test the options
+# --allow-borderline-data and --curvature-in-shadow.
+sfs                                \
+  -i ../data/sfs-input-DEM.tif     \
+  ../data/sfs-cam.cub              \
+  --max-iterations 2               \
+  --crop-input-images              \
+  --threads 1                      \
+  --allow-borderline-data          \
+  --blending-dist 5                \
+  --shadow-threshold 0.026         \
+  --curvature-in-shadow 0.15       \
+  --curvature-in-shadow-weight 0.1 \
+  --lit-curvature-dist 10          \
+  --shadow-curvature-dist 5        \
+  -o run/run
 
 # Estimate height errors
-sfs -i ../data/sfs-input-DEM.tif ../data/sfs-cam.cub -o run/run --max-iterations 2 --crop-input-images --threads 1 --estimate-height-errors
+sfs                            \
+  -i ../data/sfs-input-DEM.tif \
+  ../data/sfs-cam.cub          \
+  --max-iterations 2           \
+  --crop-input-images          \
+  --threads 1                  \
+  --estimate-height-errors     \
+  -o run/run
 
