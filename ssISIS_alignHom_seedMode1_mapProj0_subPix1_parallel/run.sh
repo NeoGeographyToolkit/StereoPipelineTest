d77c10a4189f73e91fdc50cb71430546759a7b8a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-11-14 09:55:38 -0800

    Test nodata with ISIS

--- ssISIS_alignHom_seedMode1_mapProj0_subPix1_parallel/run.sh
+++ ssISIS_alignHom_seedMode1_mapProj0_subPix1_parallel/run.sh
@@ -6,7 +6,8 @@ rm -rfv run
 mkdir -p run
 uname -n >  run/machines.txt
 
-parallel_stereo --enable-fill-holes --processes 2 --threads-multiprocess 8 --job-size-w 512 --job-size-h 512 --nodes-list run/machines.txt  ../data/M0100115_small.cub ../data/E0201461_small.cub run/run -s stereo.default  --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --corr-tile-size 512 --save-left-right-disparity-difference
+# Test --nodata-value
+parallel_stereo --enable-fill-holes --processes 2 --threads-multiprocess 8 --job-size-w 512 --job-size-h 512 --nodes-list run/machines.txt  ../data/M0100115_small.cub ../data/E0201461_small.cub run/run -s stereo.default  --alignment-method homography --corr-seed-mode 1 --subpixel-mode 1 --corr-tile-size 512 --save-left-right-disparity-difference --nodata-value 0.12
 
 # Test the situation when the proj4 string is specified as input with the datum name set separately
 point2dem --t_srs '+proj=longlat +a=3396190 +b=3396190 +no_defs '   -r mars run/run-PC.tif --nodata-value -32767
