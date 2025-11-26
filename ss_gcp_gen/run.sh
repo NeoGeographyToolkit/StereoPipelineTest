aa10dc2a15647bbf3f8375fbb57e185e73a5e4c8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-11-26 10:28:44 -0800

    Update gcp_gen test comments

--- ss_gcp_gen/run.sh
+++ ss_gcp_gen/run.sh
@@ -3,14 +3,10 @@
 set -x verbose
 rm -rfv run
 
-# Create a synthetic image and camera with pitch of 40
+# Create a synthetic image and camera with pitch of 40. The command included for reference.
 # sat_sim --dem ../data/sat_sim_DEM.tif --ortho ../data/sat_sim_ortho.tif -o runp40/run --first 400 600 80000 --last 400 1000 80000 --num 1 --focal-length 30000 --optical-center 250 250 --image-size 500 500 --first-ground-pos 400 600 --last-ground-pos 400 1000 --roll 0 --pitch 40 --yaw 0
-# and similarily for the other one
 
-# ../data/img_pitch_minus40.tif ../data/img_pitch_minus40.tsai 
-# ../data/img_pitch_plus40.tsai ../data/img_pitch_plus40.tif 
-# ../data/sat_sim_DEM.tif ../data/sat_sim_ortho.tif
-
+# Test gcp_gen on this synthetic data
 gcp_gen --camera-image ../data/img_pitch_minus40.tif --ortho-image ../data/sat_sim_ortho.tif --dem ../data/sat_sim_DEM.tif -o run/run.gcp --output-prefix run/run
 
 # bundle_adjust should be able to use this GCP file
