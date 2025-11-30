2ea6601b3505d135da92806b067dfbda5970c85b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-11-30 13:30:46 -0800

    Tune up ss_parallel_bundle_adjust_isis

--- ss_parallel_bundle_adjust_isis/run.sh
+++ ss_parallel_bundle_adjust_isis/run.sh
@@ -4,7 +4,7 @@ set -x verbose
 rm -rfv run
 mkdir -p run
 
-uname -n > run/machines.txt
+echo localhost > run/machines.txt
 
 parallel_bundle_adjust --processes 1 --threads 1 --nodes-list run/machines.txt  ../data/M0100115_small.cub ../data/E0201461_small.cub -o run/run --num-iterations 5 --ip-per-tile 1000
 
