5ea1dd8b9e2896967a9b036cc61d3f619ddadc1a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-12-09 18:39:12 -0800

    Sfs earth variance

---
+++ ss_sfs_earth_var/validate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in run/run-DEM-final.tif run/run-albedo-final.tif; do 
+
+  echo $file $gold
+  gold=${file/run\/run/gold\/run}
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+  # Remove cached xmls
+  rm -fv "$file.aux.xml"
+  rm -fv "$gold.aux.xml"
+
+  cmp_stats.sh $file $gold
+  gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v xml > run/run.txt
+  gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v xml > gold/gold.txt
+
+  diff run/run.txt gold/gold.txt
+
+  ../bin/max_err.pl run/run.txt gold/gold.txt # print the error
+  ans=$(../bin/max_err.pl run/run.txt gold/gold.txt 1e-12) # compare the error
+  if [ "$ans" -eq 0 ]; then
+      echo Validation failed
+      exit 1
+  fi
+
+done
+
+echo Validation succeeded
+exit 0
