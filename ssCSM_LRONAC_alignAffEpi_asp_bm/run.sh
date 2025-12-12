bca54383c229d68b09d1a61b751905a8583df0c6
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-12-11 21:56:27 -0800

    Add a note on a test

--- ssCSM_LRONAC_alignAffEpi_asp_bm/run.sh
+++ ssCSM_LRONAC_alignAffEpi_asp_bm/run.sh
@@ -3,6 +3,9 @@
 set -x verbose
 rm -rfv run
 
+# Logic to test stereo_gui with GCP creation. This cannot be run automatically as it needs the gui. Run manually if something changes.
+#stereo_gui ../data/M181058717LE.ce.cub ../data/M181073012LE.ce.cub run/run-DEM.tif --dem-file run/run-DEM.tif --gcp-file run/gcp3.gcp --gcp-sigma 1 run/run
+
 parallel_stereo --left-image-crop-win 3583 3922 1767 1691 --right-image-crop-win 4095 4348 1498 1668 --threads 16 --corr-seed-mode 1 ../data/M181058717LE.ce.cub ../data/M181073012LE.ce.cub ../data/M181058717LE.ce.json ../data/M181073012LE.ce.json run/run --corr-tile-size 3072 --sgm-collar-size 256 --alignment-method affineepipolar --stereo-algorithm asp_bm --save-left-right-disparity-difference
 
 point2dem run/run-PC.tif
