ec94cbd9c887b06602c271ae83a218597e594cb1
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-01-01 10:06:14 -0800

    Tweak test for mapproject

--- ss_mapproject_isis/run.sh
+++ ss_mapproject_isis/run.sh
@@ -8,5 +8,5 @@ uname -n > run/machines.txt
 
 # Test for when the DEM grid and output image grid are the same. This demonstrates a bug fix
 # for erision in the output mapprojected image at DEM boundary.
-mapproject --tr 10 --tile-size 50 ../data/dem_tr10.tif ../data/sfs-cam.cub run/run.tif --nodes-list run/machines.txt
+mapproject --tr 10 --tile-size 50 ../data/dem_tr10.tif ../data/sfs-cam.cub run/run.tif --nodes-list run/machines.txt --threads 1
 
