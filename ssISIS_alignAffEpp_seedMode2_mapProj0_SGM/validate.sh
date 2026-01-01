e6e3e67dec3d64d551b9a2ea8f17b5e38b023ae2
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-01-01 12:02:08 -0800

    Add test for disparitydebug

--- ssISIS_alignAffEpp_seedMode2_mapProj0_SGM/validate.sh
+++ ssISIS_alignAffEpp_seedMode2_mapProj0_SGM/validate.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 export PATH=../bin:$PATH
 
-file=run/run-DEM.tif
-gold=gold/run-DEM.tif
+for file in run/run-DEM.tif run/run-F-H.tif run/run-F-V.tif; do
 
-if [ ! -e "$file" ]; then
-    echo "ERROR: File $file does not exist."
-    exit 1;
-fi
+  echo $file $gold
+  gold=${file/run\/run/gold\/run}
 
-if [ ! -e "$gold" ]; then
-    echo "ERROR: File $gold does not exist."
-    exit 1;
-fi
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
 
-# Remove cached xmls
-rm -fv "$file.aux.xml"
-rm -fv "$gold.aux.xml"
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
 
-cmp_stats.sh $file $gold
-gdalinfo -stats $file | grep -v Files | grep -v -i tif > run.txt
-gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold.txt
+  # Remove cached xmls
+  rm -fv "$file.aux.xml"
+  rm -fv "$gold.aux.xml"
 
-diff=$(diff run.txt gold.txt)
-cat run.txt
+  cmp_stats.sh $file $gold
+  gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v xml > run/run.txt
+  gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v xml > gold/gold.txt
 
-rm -f run.txt gold.txt
+  diff run/run.txt gold/gold.txt
 
-echo diff is $diff
-if [ "$diff" != "" ]; then
-    echo Validation failed
-    exit 1
-fi
+  ../bin/max_err.pl run/run.txt gold/gold.txt # print the error
+  ans=$(../bin/max_err.pl run/run.txt gold/gold.txt 1e-9) # compare the error
+  if [ "$ans" -eq 0 ]; then
+      echo Validation failed
+      exit 1
+  fi
+
+done
 
 echo Validation succeeded
 exit 0
