3502da25b51a6cf0cde990495cb847535f679b16
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-12-31 20:05:04 -0800

    Add more operations for image_calc

--- ss_image_calc_multi/run.sh
+++ ss_image_calc_multi/run.sh
@@ -3,14 +3,17 @@
 set -x verbose
 rm -rfv run
 
-# Test operations that take in multiple arguments, such as min()
+# Test image_calc operators
 image_calc -c \
- "2.0 * min(var_0, var_1, 7.0) +
-  3.0 * max(var_0, 100, var_1) +
-  4.0 * lt(var_1, 50, 1, 0) + 
-  5.0 * gt(var_0, 100, 2, 0) + 
-  6.0 * lte(var_1, 200, 4, 1) + 
-  7.0 * gte(var_0, 50, 10, 0) + 
-  8.0 * eq(var_1, 128, 1, 0)" \
-  ../data/dem1_10pct.tif ../data/dem1_10pct.tif -o run/output.tif
-
+  "2.0 * min(var_0, var_1, 7.0) +
+   3.0 * max(var_0, 100, var_1) +
+   4.0 * lt(var_1, 50, 1, 0) + 
+   5.0 * gt(var_0, 100, 2, 0) + 
+   6.0 * lte(var_1, 200, 4, 1) + 
+   7.0 * gte(var_0, 50, 10, 0) + 
+   8.0 * eq(var_1, 128, 1, 0) + 
+   9.0 * abs(var_0 - 50) + 
+   10.0 * sign(var_1 - 100) + 
+   11.0 * rand(var_0)" \
+   --threads 1 \
+   ../data/dem1_10pct.tif ../data/dem1_10pct.tif -o run/output.tif
