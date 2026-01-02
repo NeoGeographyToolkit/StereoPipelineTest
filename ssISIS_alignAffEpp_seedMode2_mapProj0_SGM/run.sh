64ad508d46fa6a6db0428634ec88d6ec9277032e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-01-01 20:27:31 -0800

    Test the disparitydebug --raw option

--- ssISIS_alignAffEpp_seedMode2_mapProj0_SGM/run.sh
+++ ssISIS_alignAffEpp_seedMode2_mapProj0_SGM/run.sh
@@ -6,5 +6,6 @@ rm -rfv run
 parallel_stereo ../data/M0100115_crop.cub ../data/E0201461.cub run/run -s stereo.default --disable-fill-holes --left-image-crop-win 0 1024 672 4864 --disparity-estimation-dem ../data/seed-mode-2-init-DEM.tif --disparity-estimation-dem-error 5 --alignment-method affineepipolar --corr-seed-mode 2 --stereo-algorithm asp_mgm --corr-tile-size 2000 --cost-mode 4 --corr-kernel 9 9
 point2dem -r mars run/run-PC.tif --nodata-value -32767
 
-disparitydebug run/run-F.tif
+# Test the disparitydebug --raw option
+disparitydebug --raw run/run-F.tif
 
