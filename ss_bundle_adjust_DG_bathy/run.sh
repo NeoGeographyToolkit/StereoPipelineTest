4b93dc290cbb4f0cf8067494d36bf33646ba9e7e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-01-13 12:05:33 -0800

    Add testcase for bundle_adjust with bathy

---
+++ ss_bundle_adjust_DG_bathy/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+mkdir -p run
+
+# Create the mask list file
+echo ../data/left_bathy_b7_mask2.tif ../data/right_bathy_b7_mask2.tif > run/mask_list.txt  
+
+# Bundle adjustment with bathy masks
+bundle_adjust                           \
+  ../data/left_bathy_b3_corr.tif        \
+  ../data/right_bathy_b3_corr.tif       \
+  ../data/left_bathy.xml                \
+  ../data/right_bathy.xml               \
+  --bathy-mask-list run/mask_list.txt   \
+  --refraction-index 1.333              \
+  --bathy-plane ../data/bathy-plane.txt \
+  -o run/run
---
+++ ss_bundle_adjust_DG_bathy/validate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+ for file in run/run-left_bathy.adjusted_state.json \
+             run/run-right_bathy.adjusted_state.json; do \
+                
+    gold=gold/$(basename $file)
+
+    if [ ! -e "$file" ]; then
+        echo "ERROR: File $file does not exist."
+        exit 1;
+    fi
+
+    if [ ! -e "$gold" ]; then
+        echo "ERROR: File $gold does not exist."
+        exit 1;
+    fi
+
+    # Print the error and check the status
+    ../bin/max_err.pl $file $gold 
+    ans=$?
+    if [ "$ans" -ne 0 ]; then
+        echo Validation failed
+        exit 1
+    fi
+
+    ans=$(../bin/max_err.pl $file $gold 5e-6)
+    if [ "$ans" != "1" ]; then
+        echo Validation failed
+        exit 1
+    fi
+
+done
+
+echo Validation succeeded
+exit 0
