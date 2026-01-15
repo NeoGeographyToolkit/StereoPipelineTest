aa6c28a267461740bc7b4b3a2f060c7db6b21c61
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-01-14 19:39:41 -0800

    Tweak test

--- ss_refr_index/run.sh
+++ ss_refr_index/run.sh
@@ -4,5 +4,5 @@ set -x verbose
 rm -rfv run
 
 mkdir -p run
-refr_index --salinity 35 --temperature 20 --spectral-response ../data/spectral_response.txt > run/run.txt
+refr_index --salinity 35 --temperature 20 --spectral-response ../data/WV02_Green_spectral_response.txt > run/run.txt
 
