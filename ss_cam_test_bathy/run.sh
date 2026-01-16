58f23e5e0c60707ba5b767cc88cd4b28c029df14
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-01-16 10:33:45 -0800

    Add test ss_cam_test_bathy

---
+++ ss_cam_test_bathy/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x verbose
+rm -rfv run
+
+mkdir run
+
+# cam_test with bathy plane
+cam_test --image ../data/left_bathy_b3_corr.tif --cam1 ../data/left_bathy.xml --cam2 ../data/left_bathy.xml --bathy-plane ../data/bathy-plane.txt --refraction-index 1.333 --height-above-datum -16 > run/run.txt
+
---
+++ ss_cam_test_bathy/validate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for file in run/run.txt; do
+
+  gold=${file/run\/run/gold\/run}
+  
+  echo $file $gold
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+  diff=$(diff $file $gold | head -n 50)
+
+  echo diff is $diff
+  if [ "$diff" != "" ]; then
+      echo Validation failed
+      exit 1
+  fi
+
+done
+
+echo Validation succeeded
+exit 0
