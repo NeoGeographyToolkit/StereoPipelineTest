5356bb96706874780c7faf1137c43f8bcf09738a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-01-19 11:44:11 -0800

    Put both Quan-Fry and Parrish mode

--- ss_refr_index/run.sh
+++ ss_refr_index/run.sh
@@ -4,5 +4,12 @@ set -x verbose
 rm -rfv run
 
 mkdir -p run
-refr_index --salinity 35 --temperature 20 --spectral-response ../data/WV02_Green_spectral_response.txt > run/run.txt
+
+# Test with Quan-Fry mode (default, but set explicitly)
+refr_index --salinity 35 --temperature 20 --mode Quan-Fry \
+  --spectral-response ../data/WV02_Green_spectral_response.txt > run/run.txt
+
+# Test with Parrish mode (append to same file)
+refr_index --salinity 35 --temperature 20 --mode Parrish \
+  --spectral-response ../data/WV02_Green_spectral_response.txt >> run/run.txt
 
