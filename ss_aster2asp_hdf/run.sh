ab7c1235ccbec4c7f42bf803d828bc3333c23448
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-01-26 17:57:26 -0800

    asster2asp: Add hdf support

--- ss_aster2asp_hdf/run.sh
+++ ss_aster2asp_hdf/run.sh
@@ -5,5 +5,5 @@
 set -x verbose
 rm -rfv run
 
-aster2asp ../data/aster -o run/run
+aster2asp data/AST_L1A_00404012022185436_20250920182851.hdf -o run/run
 
---
+++ ss_aster2asp_hdf/validate.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+for gold in gold/run-Band3N.xml gold/run-Band3B.xml; do
+
+  file=${gold/gold/run}
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $f does not exist."
+      exit 1
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1
+  fi
+
+  echo Comparing $file and $gold
+  max_err.pl $file $gold # print the error
+  ans=$(max_err.pl $file $gold 1e-10) # comapre the error with threshold
+  if [ "$ans" -eq 0 ]; then
+      echo Validation failed
+      exit 1
+  fi
+
+done
+
+for gold in gold/run-Band3N.tif gold/run-Band3B.tif; do
+
+    file=${gold/gold/run}
+
+    if [ ! -e "$file" ]; then
+	echo "ERROR: File $file does not exist."
+	exit 1;
+    fi
+
+    if [ ! -e "$gold" ]; then
+	echo "ERROR: File $gold does not exist."
+	exit 1;
+    fi
+
+    # Remove cached xmls
+    rm -fv "$file.aux.xml"
+    rm -fv "$gold.aux.xml"
+
+    echo Comparing $file and $gold
+    cmp_stats.sh $file $gold
+    gdalinfo -stats $file | grep -v Files | grep -v -i tif | grep -i -v xml > run/run.txt
+    gdalinfo -stats $gold | grep -v Files | grep -v -i tif | grep -i -v xml > gold/run.txt
+
+    diff=$(diff run/run.txt gold/run.txt)
+    cat run/run.txt
+
+    rm -f run/run.txt gold/run.txt
+
+    echo diff is $diff
+    if [ "$diff" != "" ]; then
+	echo Validation failed
+	exit 1
+    fi
+done
+
+echo Validation succeeded
+exit 0
