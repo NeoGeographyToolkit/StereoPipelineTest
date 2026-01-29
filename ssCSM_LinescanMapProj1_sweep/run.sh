6fc0f078a4441a1dc410ac8247f6afea609f2d7c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-01-28 20:00:25 -0800

    Tweak test

--- ssCSM_LinescanMapProj1_sweep/run.sh
+++ ssCSM_LinescanMapProj1_sweep/run.sh
@@ -15,5 +15,8 @@ stereo_sweep                                      \
   ../data/B18_016575_1978_XN_17N282W.8bit.json    \
   --dem ../data/linescanDEM.tif                   \
   --output-dir run                                \
-  --point2dem-params '--tr 10 --orthoimage --errorimage' \
+  --point2dem-params "
+    --tr 10
+    --orthoimage
+    --errorimage"                                 \
   --dry-run 2>&1 | tee run/run.txt
