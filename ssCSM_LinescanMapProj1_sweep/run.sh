6a8ac1e3a05301aa988a266119974ec39dcd7bc6
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-01-30 10:23:02 -0800

    Tweak run

--- ssCSM_LinescanMapProj1_sweep/run.sh
+++ ssCSM_LinescanMapProj1_sweep/run.sh
@@ -8,7 +8,13 @@ stereo_sweep                                      \
   --param-sweep                                   \
     "--stereo-algorithm asp_mgm asp_sgm
     --subpixel-mode 1 9
-    --corr-kernel 7,7 9,9"                        \
+    --corr-kernel 7,7 9,9
+    --proj-win -180,3873,3287,180
+               -947,1418,3022,-2752"              \
+  --param-sweep                                   \
+    "--stereo-algorithm asp_bm
+    --subpixel-mode 1 3
+    --corr-kernel 19,19 21,21"                    \
   ../data/B17_016219_1978_XN_17N282W.8bit.map.tif \
   ../data/B18_016575_1978_XN_17N282W.8bit.map.tif \
   ../data/B17_016219_1978_XN_17N282W.8bit.json    \
