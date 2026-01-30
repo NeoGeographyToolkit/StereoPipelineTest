2f01fd648bcd35495285d27e94976a56311284a8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-01-29 18:50:58 -0800

    Add option stereo --proj-win

--- ssPinHole_alignNone_seedMode1_mapProj1_subPix1/run.sh
+++ ssPinHole_alignNone_seedMode1_mapProj1_subPix1/run.sh
@@ -3,7 +3,8 @@
 set -x verbose
 rm -rfv run
 
-stereo -t pinholemappinhole ../data/pinhole_proj_47.tif ../data/pinhole_proj_48.tif ../data/pinhole_proj_47.tif.tsai ../data/pinhole_proj_48.tif.tsai run/run ../data/krigged_dem_nsidc_ndv0_fill.tif -s stereo.default --corr-seed-mode 1 --subpixel-mode 1
+# Test stereo with --proj-win
+stereo --proj-win -1134640 -827580 -1130080 -832860 -t pinholemappinhole ../data/pinhole_proj_47.tif ../data/pinhole_proj_48.tif ../data/pinhole_proj_47.tif.tsai ../data/pinhole_proj_48.tif.tsai run/run ../data/krigged_dem_nsidc_ndv0_fill.tif -s stereo.default --corr-seed-mode 1 --subpixel-mode 1
 
 point2dem --nodata-value -32767 run/run-PC.tif
 
