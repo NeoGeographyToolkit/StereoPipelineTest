85f2e592b9d9153960830b21720db76463240f47
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-02-10 14:12:58 -0800

    Format a test

--- ss_bundle_adjust_tsai/run.sh
+++ ss_bundle_adjust_tsai/run.sh
@@ -14,17 +14,20 @@ ls ../data/left_sub16.tif > run/sensor1_images.txt
 ls ../data/right_sub16.tif > run/sensor2_images.txt
 ls ../data/left_sub16.v2.tsai > run/sensor1_cameras.txt
 ls ../data/right_sub16.v2.tsai > run/sensor2_cameras.txt
-bundle_adjust --solve-intrinsics                                  \
-    --inline-adjustments                                          \
-    --intrinsics-to-float "other_intrinsics"                      \
-    -t pinhole                                                    \
-    --threads 1                                                   \
-    --num-iterations 10                                           \
-    --image-list run/sensor1_images.txt,run/sensor2_images.txt    \
-    --camera-list run/sensor1_cameras.txt,run/sensor2_cameras.txt \
-    ../data/gcp_sub16.gcp                                         \
-    --datum WGS84                                                 \
-    -o run/ba1/run
+bundle_adjust                                       \
+  --solve-intrinsics                                \
+  --inline-adjustments                              \
+  --intrinsics-to-float "other_intrinsics"          \
+  -t pinhole                                        \
+  --threads 1                                       \
+  --num-iterations 10                               \
+  --image-list                                      \
+    run/sensor1_images.txt,run/sensor2_images.txt   \
+  --camera-list                                     \
+    run/sensor1_cameras.txt,run/sensor2_cameras.txt \
+  ../data/gcp_sub16.gcp                             \
+  --datum WGS84                                     \
+  -o run/ba1/run
 
 # Then just apply a transform
 bundle_adjust ../data/left_sub16.tif ../data/right_sub16.tif  run/ba1/run-left_sub16.v2.tsai run/ba1/run-right_sub16.v2.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run --initial-transform run/transform.txt --apply-initial-transform-only --threads 1
