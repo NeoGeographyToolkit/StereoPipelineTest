4c4fbb469f9e9aee1aad59d65d5c21a1300c59ab
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-02-13 08:38:35 -0800

    Add test for stereo_dist

--- ssCSM_LinescanMapProj1/run.sh
+++ ssCSM_LinescanMapProj1/run.sh
@@ -2,6 +2,9 @@
 
 # Test parallel_stereo with mapprojected images and --num-matches-from-disp-triplets
 
+set -x verbose
+rm -rfv run
+
 parallel_stereo --matches-as-txt ../data/B17_016219_1978_XN_17N282W.8bit.map.tif ../data/B18_016575_1978_XN_17N282W.8bit.map.tif ../data/B17_016219_1978_XN_17N282W.8bit.json ../data/B18_016575_1978_XN_17N282W.8bit.json run/run ../data/linescanDEM.tif --num-matches-from-disp-triplets 1000 --threads 1
 
 point2dem run/run-PC.tif
---
+++ ssCSM_LinescanMapProj1_dist/validate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+export PATH=../bin:$PATH
+
+file=run/run-DEM.tif
+gold=gold/$(basename $file)
+
+if [ ! -e "$file" ]; then
+    echo "ERROR: File $file does not exist."
+    exit 1;
+fi
+
+if [ ! -e "$gold" ]; then
+    echo "ERROR: File $gold does not exist."
+    exit 1;
+fi
+
+# Remove cached xmls
+rm -fv "$file.aux.xml"
+rm -fv "$gold.aux.xml"
+
+cmp_stats.sh $file $gold
+gdalinfo -stats $file | grep -v Files | grep -v -i tif > run/run.txt
+gdalinfo -stats $gold | grep -v Files | grep -v -i tif > gold/run.txt
+
+diff=$(diff run/run.txt gold/run.txt)
+cat run/run.txt
+
+rm -f run/run.txt gold/run.txt
+
+echo diff is $diff
+if [ "$diff" != "" ]; then
+    echo Validation failed
+    exit 1
+fi
+
+echo Validation succeeded
+exit 0
