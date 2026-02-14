57d782b5ceb5199c7c77e23cf73e92a4db4f1b53
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-02-14 11:03:44 -0800

    Use parallel_stereo in ssDG_alignNone_seedMode1_mapProj1_subPix2

--- ssDG_alignNone_seedMode1_mapProj1_subPix2/run.sh
+++ ssDG_alignNone_seedMode1_mapProj1_subPix2/run.sh
@@ -7,6 +7,6 @@ rm -rfv run
 mkdir -p run
 gdal_translate -srcwin 0 0 256 512 ../data/left.tif run/left_clip.tif
 
-stereo --allow-different-mapproject-gsd run/left_clip.tif ../data/right.tif ../data/left.xml ../data/right.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif  --disable-fill-holes -s stereo.default -t dg --alignment-method none --corr-seed-mode 1 --subpixel-mode 2 --skip-image-normalization
+parallel_stereo --allow-different-mapproject-gsd run/left_clip.tif ../data/right.tif ../data/left.xml ../data/right.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif  --disable-fill-holes -s stereo.default -t dg --alignment-method none --corr-seed-mode 1 --subpixel-mode 2 --skip-image-normalization
 point2dem -r Earth run/run-PC.tif --nodata-value -32767
 
