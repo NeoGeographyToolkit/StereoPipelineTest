60961405e33b87b6c8d4822a30710669e11d3cff
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-02-17 13:23:47 -0800

    Add +stereo_dist --corr-seed-mode 3 test

--- ssDG_alignNone_seedMode3_mapProj1_subPix1_parallel/run.sh
+++ ssDG_alignNone_seedMode3_mapProj1_subPix1_parallel/run.sh
@@ -3,6 +3,29 @@
 set -x verbose
 rm -rfv run
 
-parallel_stereo --processes 8 --threads-multiprocess 1 --job-size-w 512 --job-size-h 1024 ../data/WV01_11JAN131652222-P1BS-10200100104A0300_ortho1.0m_sub2_crop1.tif ../data/WV01_11JAN131653180-P1BS-1020010011862E00_ortho1.0m_sub2_crop1.tif ../data/WV01_11JAN131652222-P1BS-10200100104A0300.xml ../data/WV01_11JAN131653180-P1BS-1020010011862E00.xml run/run ../data/krigged_dem_nsidc_ndv0_fill.tif --left-image-crop-win 0 2048 1024 1024  --right-image-crop-win 0 2048 1200 1200 --corr-search -30 -30 30 30 --corr-max-levels 5 -s stereo.default --disable-fill-holes  -t dg --alignment-method none --corr-seed-mode 3 --subpixel-mode 1 --sparse-disp-options '--processes 19 --coarse 512 --fine 256 --output_dec_scale=8' --corr-tile-size 512 --allow-different-mapproject-gsd
-point2dem  run/run-PC.tif --nodata-value -32767
+sparseOpts='--processes 19 --coarse 512 --fine 256 --output_dec_scale=8'
+
+stereo_dist                                                                  \
+  --tile-size 512                                                            \
+  --tile-padding 128                                                         \
+  --processes 8                                                              \
+  --threads-multiprocess 1                                                   \
+  -s stereo.default                                                          \
+  -t dg                                                                      \
+  --alignment-method none                                                    \
+  --corr-seed-mode 3                                                         \
+  --corr-search -30 -30 30 30                                                \
+  --corr-max-levels 5                                                        \
+  --corr-tile-size 512                                                       \
+  --subpixel-mode 1                                                          \
+  --disable-fill-holes                                                       \
+  --allow-different-mapproject-gsd                                           \
+  --sparse-disp-options "$sparseOpts"                                        \
+  ../data/WV01_11JAN131652222-P1BS-10200100104A0300_ortho1.0m_sub2_crop2.tif \
+  ../data/WV01_11JAN131653180-P1BS-1020010011862E00_ortho1.0m_sub2_crop2.tif \
+  ../data/WV01_11JAN131652222-P1BS-10200100104A0300.xml                      \
+  ../data/WV01_11JAN131653180-P1BS-1020010011862E00.xml                      \
+  run/run                                                                    \
+  --dem ../data/krigged_dem_nsidc_ndv0_fill.tif                              \
+  --point2dem-options '--tr 2'
 
