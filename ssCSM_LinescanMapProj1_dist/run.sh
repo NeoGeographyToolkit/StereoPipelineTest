63abdcdc8e861ce0339a1f3edc2d1605669e3e7f
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-02-17 13:08:21 -0800

    Add stereo_dist tests

--- ssCSM_LinescanMapProj1_dist/run.sh
+++ ssCSM_LinescanMapProj1_dist/run.sh
@@ -5,5 +5,5 @@
 set -x verbose
 rm -rfv run
 
-stereo_dist ../data/B17_016219_1978_XN_17N282W.8bit.crop.map.tif ../data/B18_016575_1978_XN_17N282W.8bit.crop.map.tif ../data/B17_016219_1978_XN_17N282W.8bit.json ../data/B18_016575_1978_XN_17N282W.8bit.json run/run ../data/linescanDEM.tif --tile-size 512 --tile-padding 128 --point2dem-options '--tr 10'
+stereo_dist ../data/B17_016219_1978_XN_17N282W.8bit.crop.map.tif ../data/B18_016575_1978_XN_17N282W.8bit.crop.map.tif ../data/B17_016219_1978_XN_17N282W.8bit.json ../data/B18_016575_1978_XN_17N282W.8bit.json run/run --dem ../data/linescanDEM.tif --tile-size 512 --tile-padding 128 --point2dem-options '--tr 10'
 
--- ssDG_alignNone_seedMode2_mapProj1_subPix1_no_default/run.sh
+++ ssDG_alignNone_seedMode2_mapProj1_subPix1_no_default/run.sh
@@ -7,8 +7,12 @@ rm -rfv run
 # This requires loading the dg cameras to undo the mapprojection.
 # Test running stereo with no default file.
 # Test --corr-seed-mode 2 with mapprojected images.
+# Test stereo_dist workflow.
 
-stereo --corr-seed-mode 2                                \
+stereo_dist                                              \
+  --tile-size 500                                        \
+  --tile-padding 100                                     \
+  --corr-seed-mode 2                                     \
   --disparity-estimation-dem ../data/grand_mesa_clip.tif \
   --disparity-estimation-dem-error 5                     \
   ../data/grand_mesa_WV03_left.map.dg.tif                \
@@ -16,6 +20,6 @@ stereo --corr-seed-mode 2                                \
   ../data/grand_mesa_WV03_left.json                      \
   ../data/grand_mesa_WV03_right.json                     \
   run/run                                                \
-  ../data/grand_mesa_clip.tif
-point2dem run/run-PC.tif
+  --dem ../data/grand_mesa_clip.tif                      \
+  --point2dem-options '--tr 0.5'
 
