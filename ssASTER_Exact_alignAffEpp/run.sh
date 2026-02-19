100be2fdb25fa3a00fd56ab502963921b8ccf2fc
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-02-19 14:13:07 -0800

    Speed up test

--- ssASTER_Exact_alignAffEpp/run.sh
+++ ssASTER_Exact_alignAffEpp/run.sh
@@ -3,9 +3,9 @@
 set -x verbose
 rm -rfv run
 
-bundle_adjust -t aster ../data/aster-Band3N.tif ../data/aster-Band3B.tif ../data/aster-Band3N.xml ../data/aster-Band3B.xml -o run/ba/run --threads 1 --num-iterations 10
+bundle_adjust -t aster --aster-use-csm ../data/aster-Band3N.tif ../data/aster-Band3B.tif ../data/aster-Band3N.xml ../data/aster-Band3B.xml -o run/ba/run --threads 1 --num-iterations 3
 
-stereo -t aster ../data/aster-Band3N.tif ../data/aster-Band3B.tif ../data/aster-Band3N.xml ../data/aster-Band3B.xml run/run --left-image-crop-win 2523 3311 820 753 --right-image-crop-win 3186 4068 1082 1218 --bundle-adjust-prefix run/ba/run
+stereo -t aster --aster-use-csm ../data/aster-Band3N.tif ../data/aster-Band3B.tif ../data/aster-Band3N.xml ../data/aster-Band3B.xml run/run --left-image-crop-win 2846 3290 491 509 --right-image-crop-win 3507 4000 751 729 --bundle-adjust-prefix run/ba/run
 
 point2dem run/run-PC.tif
 
