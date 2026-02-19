fe0785f952342e7893d08dc3ba2edc41fb18542a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-02-18 19:52:54 -0800

    Tweak test

--- ssPleiades_stereoDistMapproj/run.sh
+++ ssPleiades_stereoDistMapproj/run.sh
@@ -13,8 +13,8 @@ cam1="../data/RPC_PHR1B_P_202204132246314_SEN_6304928201-1.XML"
 cam2="../data/RPC_PHR1B_P_202204132247161_SEN_6304929201-1.XML"
 
 srs='+proj=stere +lat_0=90 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs'
-projwin='-827973 1927946 -827848 1927796'
-pdopt='--stereographic --proj-lon 0 --proj-lat 90 --tr 1'
+projwin="-827855 1928110 -827631 1927887"
+pdopt='--stereographic --proj-lon 0 --proj-lat 90 --tr 0.5'
 
 stereo_dist                      \
   --mapproject                   \
@@ -30,5 +30,5 @@ stereo_dist                      \
   --subpixel-mode 1              \
   --tile-size 600                \
   --tile-padding 128             \
-  --stereo-algorithm asp_bm      \
+  --stereo-algorithm asp_mgm     \
   --point2dem-options "$pdopt"
