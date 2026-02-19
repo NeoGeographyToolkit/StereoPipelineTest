7d0cebacb64451a5c0e129067a06d8b4b4af3024
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-02-19 14:18:47 -0800

    Speedup test

--- ssCSM_LRONAC_alignLocalEpi_asp_mgm/run.sh
+++ ssCSM_LRONAC_alignLocalEpi_asp_mgm/run.sh
@@ -3,5 +3,5 @@
 set -x verbose
 rm -rfv run
 
-parallel_stereo --left-image-crop-win 3583 3922 1767 1691 --right-image-crop-win 4095 4348 1498 1668 --corr-seed-mode 1 ../data/M181058717LE.ce.cub ../data/M181073012LE.ce.cub ../data/M181058717LE.ce.json ../data/M181073012LE.ce.json run/run --corr-tile-size 3072 --sgm-collar-size 256 --alignment-method local_epipolar --stereo-algorithm asp_mgm
+parallel_stereo --left-image-crop-win 4096 4755 521 535 --right-image-crop-win 4451 4995 542 596 --corr-seed-mode 1 ../data/M181058717LE.ce.cub ../data/M181073012LE.ce.cub ../data/M181058717LE.ce.json ../data/M181073012LE.ce.json run/run --corr-tile-size 3072 --sgm-collar-size 256 --alignment-method local_epipolar --stereo-algorithm asp_mgm
 point2dem run/run-PC.tif
