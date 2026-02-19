c0ab82404f03a28252f48c07c6b76442f68615a2
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-02-19 15:03:50 -0800

    Spedup tests

--- ssDG_alignHom_seedMode1_mapProj0_subPix1_Cov/run.sh
+++ ssDG_alignHom_seedMode1_mapProj0_subPix1_Cov/run.sh
@@ -3,7 +3,23 @@
 set -x verbose
 rm -rfv run
 
-parallel_stereo --bundle-adjust-prefix ../data/ba_grand_mesa/run --alignment-method affineepipolar --stereo-algorithm asp_mgm --subpixel-mode 9 -t dg --propagate-errors ../data/grand_mesa_WV03_left_crop.tif ../data/grand_mesa_WV03_right_crop.tif ../data/grand_mesa_WV03_left.xml ../data/grand_mesa_WV03_right.xml run/run --left-image-crop-win 17660 5330 579 610 --right-image-crop-win 5116 15520 720 688 --corr-tile-size 5000
+img1=../data/grand_mesa_WV03_left_crop.tif
+img2=../data/grand_mesa_WV03_right_crop.tif
+cam1=../data/grand_mesa_WV03_left.xml
+cam2=../data/grand_mesa_WV03_right.xml
 
-point2dem --propagate-errors run/run-PC.tif 
+parallel_stereo                                    \
+  $img1 $img2                                      \
+  $cam1 $cam2                                      \
+  run/run                                          \
+  --bundle-adjust-prefix ../data/ba_grand_mesa/run \
+  --alignment-method affineepipolar                \
+  --stereo-algorithm asp_mgm                       \
+  --subpixel-mode 9                                \
+  -t dg                                            \
+  --propagate-errors                               \
+  --left-image-crop-win 17664 5325 435 500         \
+  --right-image-crop-win 5279 15622 414 500        \
+  --corr-tile-size 5000
 
+point2dem --propagate-errors run/run-PC.tif
