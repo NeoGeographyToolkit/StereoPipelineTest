3f041140e5172b5d03f1c4e302c24c5e544d71fe
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-02-18 19:04:34 -0800

    Test ssPleiades_stereoDistMapproj

---
+++ ssPleiades_stereoDistMapproj/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Test stereo_dist with --mapproject flag.
+# Uses Pleiades images with RPC XML cameras.
+
+set -x verbose
+rm -rfv run
+
+dem="../data/pleiades_dem.tif"
+img1="../data/IMG_PHR1B_P_202204132246314_SEN_6304928201-1_R1C1_crop.TIF"
+img2="../data/IMG_PHR1B_P_202204132247161_SEN_6304929201-1_R1C1_crop.tif"
+cam1="../data/RPC_PHR1B_P_202204132246314_SEN_6304928201-1.XML"
+cam2="../data/RPC_PHR1B_P_202204132247161_SEN_6304929201-1.XML"
+
+srs='+proj=stere +lat_0=90 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs'
+projwin='-827973 1927946 -827848 1927796'
+pdopt='--stereographic --proj-lon 0 --proj-lat 90 --tr 1'
+
+stereo_dist                      \
+  --mapproject                   \
+  --dem $dem                     \
+  --t_srs "$srs"                 \
+  --tr 0.5                       \
+  --t_projwin $projwin           \
+  $img1 $img2                    \
+  $cam1 $cam2                    \
+  run/run                        \
+  --alignment-method none        \
+  --corr-seed-mode 1             \
+  --subpixel-mode 1              \
+  --tile-size 600                \
+  --tile-padding 128             \
+  --stereo-algorithm asp_bm      \
+  --point2dem-options "$pdopt"
