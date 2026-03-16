23bcd9c7f454f69b9482499ce636832ea5622c9e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-03-16 08:48:07 -0700

    Tweak tes

--- ss_point2mesh/run.sh
+++ ss_point2mesh/run.sh
@@ -3,4 +3,4 @@
 set -x verbose
 rm -rfv run
 
-point2mesh ../data/run-PC.tif ../data/run-L.tif --output-prefix run/run
+point2mesh ../data/point2mesh-PC.tif ../data/point2mesh-L.tif --output-prefix run/run
