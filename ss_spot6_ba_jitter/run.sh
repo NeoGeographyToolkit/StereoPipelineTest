b4a1edbcf0663c87ed39db04368bfaeee9461083
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-03-26 08:17:56 -0700

    Tweak test

--- ss_spot6_ba_jitter/run.sh
+++ ss_spot6_ba_jitter/run.sh
@@ -13,21 +13,23 @@ left_cam=../data/DIM_SYNTH_SPOT6_LEFT.XML
 right_cam=../data/DIM_SYNTH_SPOT6_RIGHT.XML
 
 # Bundle adjust with 1 iteration. Produces .adjust and .adjusted_state.json.
-bundle_adjust -t spot               \
-  --num-iterations 1                \
-  --camera-weight 0                 \
-  --tri-weight 0.1                  \
-  $left $right $left_cam $right_cam \
+bundle_adjust -t spot                \
+  --num-iterations 1                 \
+  --camera-weight 0                  \
+  --tri-weight 0.1                   \
+  --threads 1                        \
+  $left $right $left_cam $right_cam  \
   -o run/ba/run
 
 # Jitter solve with 1 iteration. Uses BA matches and adjusted cameras.
 # With 2000-line images, 200 lines per knot gives ~10 knots.
-jitter_solve                                \
-  --image-list run/ba/run-image_list.txt    \
-  --camera-list run/ba/run-camera_list.txt  \
-  --match-files-prefix run/ba/run           \
-  --num-iterations 1                        \
-  --num-lines-per-position 200              \
-  --num-lines-per-orientation 200           \
+jitter_solve                                 \
+  --image-list run/ba/run-image_list.txt     \
+  --camera-list run/ba/run-camera_list.txt   \
+  --match-files-prefix run/ba/run            \
+  --num-iterations 1                         \
+  --num-lines-per-position 200               \
+  --num-lines-per-orientation 200            \
+  --threads 1                                \
   -o run/jitter/run
 
