69cd28524d01e5b769180840d0c35a7c30f78069
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-03-27 14:21:15 -0700

    PeruSat cam_test: remove --perusat-vs-csm flag

--- ss_cam_test_perusat/run.sh
+++ ss_cam_test_perusat/run.sh
@@ -5,13 +5,12 @@ rm -rfv run
 
 mkdir run
 
-# PeruSat exact linescan model: old VW-based vs new CSM-based.
-# Same camera compared against itself. Uses a small crop.
+# PeruSat exact linescan model. Same camera compared against itself.
+# Uses a small crop.
 
 cam_test --session1 perusat --session2 perusat \
   --image ../data/IMG_PER1_20171207153639_SEN_P_000041_crop.tif \
   --cam1 ../data/DIM_PER1_20171207153639_SEN_P_000041.XML \
   --cam2 ../data/DIM_PER1_20171207153639_SEN_P_000041.XML \
-  --perusat-vs-csm \
   --sample-rate 100 \
   | grep -E "Min|Max|Median" > run/run-out.txt
