68d73a3714c2200ecba334d9055e8a1fd15c492e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-03-28 16:00:44 -0700

    Remove --aster-use-csm from ASTER test run.sh files
    ASTER now always uses CSM (migration commit 5108f2ba9 in ASP).
    The --aster-use-csm flag was removed from all tools.

--- ss_cam_test_aster_csm/run.sh
+++ ss_cam_test_aster_csm/run.sh
@@ -5,5 +5,5 @@ rm -rfv run
 
 mkdir run
 
-cam_test --image ../data/aster-Band3N.tif --cam1 ../data/aster-Band3N.xml --cam2 ../data/aster-Band3N.xml --aster-use-csm | grep -E "Min|Max|Median" > run/run-out.txt
+cam_test --image ../data/aster-Band3N.tif --cam1 ../data/aster-Band3N.xml --cam2 ../data/aster-Band3N.xml | grep -E "Min|Max|Median" > run/run-out.txt
 
