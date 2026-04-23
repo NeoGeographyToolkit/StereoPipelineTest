cf901588895c61ff4a9ec8c52710ab1196f5381c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-04-23 15:25:41 -0700

    lonlat surface bathy test

---
+++ ss_cam_test_lonlat_surface/validate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+source ../bin/setup_env.sh
+
+for file in run/run.txt; do
+
+  gold=gold/$(basename $file)
+
+  echo $file $gold
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$gold" ]; then
+      echo "ERROR: File $gold does not exist."
+      exit 1;
+  fi
+
+  diff=$(diff $file $gold | head -n 50)
+
+  echo diff is $diff
+  if [ "$diff" != "" ]; then
+      echo Validation failed
+      exit 1
+  fi
+
+done
+
+echo Validation succeeded
+exit 0
