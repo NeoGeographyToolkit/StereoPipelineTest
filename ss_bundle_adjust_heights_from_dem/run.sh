df8b5cc8e7222f5e8585f7140fb3113b81f69bf3
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-07-07 18:47:25 -0700

    Test for ht dem list

--- ss_bundle_adjust_heights_from_dem/run.sh
+++ ss_bundle_adjust_heights_from_dem/run.sh
@@ -11,5 +11,11 @@ stereo ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownco
 # Create the reference terrain
 point2dem run/run-PC.tif
 
+# Test --heights-from-dem
 bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run --intrinsics-to-float other_intrinsics --parameter-tolerance 1e-12 --heights-from-dem run/run-DEM.tif --max-num-reference-points 1000 --threads 1 --num-passes 2 --num-iterations 2 --mapproj-dem run/run-DEM.tif --heights-from-dem-uncertainty 10 --match-files-prefix run/run-disp
+
+# Test --heights-from-dem-list
+echo run/run-DEM.tif  > run/dem_list.txt
+echo run/run-DEM.tif >> run/dem_list.txt
+bundle_adjust --solve-intrinsics ../data/left_sub16.tif ../data/right_sub16.tif ../data/left_sub16.brownconrady.tsai ../data/right_sub16.brownconrady.tsai --inline-adjustments -t pinhole --datum WGS84 -o run/run2 --intrinsics-to-float other_intrinsics --parameter-tolerance 1e-12 --heights-from-dem-list run/dem_list.txt --max-num-reference-points 1000 --threads 1 --num-passes 2 --num-iterations 2 --mapproj-dem run/run-DEM.tif --heights-from-dem-uncertainty 10 --match-files-prefix run/run-disp
 
--- ss_bundle_adjust_heights_from_dem/validate.sh
+++ ss_bundle_adjust_heights_from_dem/validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ../bin/setup_env.sh
 
-for file in run/*tsai run/run-mapproj_match_offsets.txt run/run-mapproj_match_offset_stats.txt; do
+for file in run/run-*tsai run/run-mapproj_match_offsets.txt run/run-mapproj_match_offset_stats.txt; do
 
   gold=gold/$(basename $file)
 
@@ -21,6 +21,32 @@ for file in run/*tsai run/run-mapproj_match_offsets.txt run/run-mapproj_match_of
   echo diff is $diff
   if [ "$diff" != "" ]; then
       echo Validation failed
+      exit 1
+  fi
+
+done
+
+# Validate --heights-from-dem-list
+for file in run/run2*tsai; do
+
+  ref=$(echo $file | sed 's|run/run2|run/run|')
+
+  if [ ! -e "$file" ]; then
+      echo "ERROR: File $file does not exist."
+      exit 1;
+  fi
+
+  if [ ! -e "$ref" ]; then
+      echo "ERROR: File $ref does not exist."
+      exit 1;
+  fi
+
+  echo diff $file $ref
+  diff=$(diff $file $ref | head -n 50)
+
+  echo diff is $diff
+  if [ "$diff" != "" ]; then
+      echo "Validation failed: --heights-from-dem-list differs from --heights-from-dem"
       exit 1
   fi
 
